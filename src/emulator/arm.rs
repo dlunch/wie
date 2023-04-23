5c2daa8012580fe04b06178e363867a10f91c349
Inseok Lee <git@dlun.ch>
2023-04-23 09:49:14 +0900

    Add debug logs

--- src/emulator/arm.rs
+++ src/emulator/arm.rs
@@ -74,8 +74,12 @@ impl ArmEmulator {
             }
         }
 
+        log::debug!("Run function start {:#x}, params {:?}", address, params);
+
         self.uc.reg_write(RegisterARM::LR, RUN_FUNCTION_LR).unwrap();
         self.uc.emu_start(address as u64, RUN_FUNCTION_LR, 0, 0).unwrap();
+
+        log::debug!("Run function end");
 
         self.uc.reg_read(RegisterARM::R0).unwrap() as u32
     }
@@ -91,6 +95,12 @@ impl ArmEmulator {
 
         self.uc
             .add_code_hook(address, address + 2, move |uc, _, _| {
+                log::debug!(
+                    "Registerd function called at {:#x}, LR: {:#x}",
+                    address,
+                    uc.reg_read(RegisterARM::LR).unwrap()
+                );
+
                 let mut new_self = Self::from_uc(Unicorn::try_from(uc.get_handle()).unwrap());
 
                 let ret = function(&mut new_self);
@@ -99,10 +109,14 @@ impl ArmEmulator {
             })
             .unwrap();
 
+        log::debug!("Register function at {:#x}", address);
+
         address as u32 + 1
     }
 
     pub fn alloc(&mut self, address: u32, size: u32) {
+        log::debug!("Alloc address: {:#x}, size: {:#x}", address, size);
+
         self.uc
             .mem_map(address as u64, size as usize, Permission::READ | Permission::WRITE)
             .unwrap();
@@ -114,51 +128,60 @@ impl ArmEmulator {
     {
         let data = self.uc.mem_read_as_vec(address as u64, std::mem::size_of::<T>()).unwrap();
 
+        log::debug!("Read address: {:#x}, data: {:02x?}", address, data);
+
         unsafe { *(data.as_ptr() as *const T) }
     }
 
     pub fn write<T>(&mut self, address: u32, data: T) {
         let data_slice = unsafe { slice::from_raw_parts(&data as *const T as *const u8, std::mem::size_of::<T>()) };
 
+        log::debug!("Write address: {:#x}, data: {:02x?}", address, data_slice);
+
         self.uc.mem_write(address as u64, data_slice).unwrap();
     }
 
     pub fn free(&mut self, address: u32, size: u32) {
+        log::debug!("Free address: {:#x}, size: {:#x}", address, size);
+
         self.uc.mem_unmap(address as u64, size as usize).unwrap()
     }
 
-    pub fn dump_regs(&self) {
+    pub fn dump_regs(&self) -> String {
         Self::dump_regs_inner(&self.uc)
     }
 
-    fn dump_regs_inner(uc: &Unicorn<'_, ()>) {
-        println!(
-            "R0: {:x} R1: {:x} R2: {:x} R3: {:x} R4: {:x} R5: {:x} R6: {:x} R7: {:x} R8: {:x}",
-            uc.reg_read(RegisterARM::R0).unwrap(),
-            uc.reg_read(RegisterARM::R1).unwrap(),
-            uc.reg_read(RegisterARM::R2).unwrap(),
-            uc.reg_read(RegisterARM::R3).unwrap(),
-            uc.reg_read(RegisterARM::R4).unwrap(),
-            uc.reg_read(RegisterARM::R5).unwrap(),
-            uc.reg_read(RegisterARM::R6).unwrap(),
-            uc.reg_read(RegisterARM::R7).unwrap(),
-            uc.reg_read(RegisterARM::R8).unwrap()
-        );
-        println!(
-            "SB: {:x} SL: {:x} FP: {:x} IP: {:x} SP: {:x} LR: {:x} PC: {:x}",
-            uc.reg_read(RegisterARM::SB).unwrap(),
-            uc.reg_read(RegisterARM::SL).unwrap(),
-            uc.reg_read(RegisterARM::FP).unwrap(),
-            uc.reg_read(RegisterARM::IP).unwrap(),
-            uc.reg_read(RegisterARM::SP).unwrap(),
-            uc.reg_read(RegisterARM::LR).unwrap(),
-            uc.reg_read(RegisterARM::PC).unwrap()
-        );
-        println!("APSR: {:032b}", uc.reg_read(RegisterARM::APSR).unwrap());
+    fn dump_regs_inner(uc: &Unicorn<'_, ()>) -> String {
+        [
+            format!(
+                "R0: {:x} R1: {:x} R2: {:x} R3: {:x} R4: {:x} R5: {:x} R6: {:x} R7: {:x} R8: {:x}",
+                uc.reg_read(RegisterARM::R0).unwrap(),
+                uc.reg_read(RegisterARM::R1).unwrap(),
+                uc.reg_read(RegisterARM::R2).unwrap(),
+                uc.reg_read(RegisterARM::R3).unwrap(),
+                uc.reg_read(RegisterARM::R4).unwrap(),
+                uc.reg_read(RegisterARM::R5).unwrap(),
+                uc.reg_read(RegisterARM::R6).unwrap(),
+                uc.reg_read(RegisterARM::R7).unwrap(),
+                uc.reg_read(RegisterARM::R8).unwrap()
+            ),
+            format!(
+                "SB: {:x} SL: {:x} FP: {:x} IP: {:x} SP: {:x} LR: {:x} PC: {:x}",
+                uc.reg_read(RegisterARM::SB).unwrap(),
+                uc.reg_read(RegisterARM::SL).unwrap(),
+                uc.reg_read(RegisterARM::FP).unwrap(),
+                uc.reg_read(RegisterARM::IP).unwrap(),
+                uc.reg_read(RegisterARM::SP).unwrap(),
+                uc.reg_read(RegisterARM::LR).unwrap(),
+                uc.reg_read(RegisterARM::PC).unwrap()
+            ),
+            format!("APSR: {:032b}\n", uc.reg_read(RegisterARM::APSR).unwrap()),
+        ]
+        .join("\n")
     }
 
     fn block_hook(uc: &mut Unicorn<'_, ()>, address: u64, size: u32) {
-        println!("-- address: {:x}, size: {:x}", address, size);
+        log::trace!("-- address: {:x}, size: {:x}", address, size);
         let insn = uc.mem_read_as_vec(address, size as usize).unwrap();
 
         let cs = Capstone::new()
@@ -170,13 +193,13 @@ impl ArmEmulator {
 
         let insns = cs.disasm_all(&insn, address).unwrap();
         for insn in insns.iter() {
-            println!("{} {}", insn.mnemonic().unwrap(), insn.op_str().unwrap());
+            log::trace!("{} {}", insn.mnemonic().unwrap(), insn.op_str().unwrap());
         }
-        println!("-- reg");
+        log::trace!("-- reg");
 
-        Self::dump_regs_inner(uc);
+        log::trace!("\n{}", Self::dump_regs_inner(uc));
 
-        println!("--");
+        log::trace!("--");
     }
 
     fn mem_hook(uc: &mut Unicorn<'_, ()>, mem_type: MemType, address: u64, size: usize, value: i64) -> bool {
@@ -186,20 +209,32 @@ impl ArmEmulator {
             let value = uc.mem_read_as_vec(address, size).unwrap();
             if size == 4 {
                 let value = u32::from_le_bytes(value.try_into().unwrap());
-                println!(
+                log::trace!(
                     "pc: {:x} mem_type: {:?} address: {:x} size: {:x} value: {:x}",
-                    pc, mem_type, address, size, value
+                    pc,
+                    mem_type,
+                    address,
+                    size,
+                    value
                 );
             } else {
-                println!(
+                log::trace!(
                     "pc: {:x} mem_type: {:?} address: {:x} size: {:x} value: {:?}",
-                    pc, mem_type, address, size, value
+                    pc,
+                    mem_type,
+                    address,
+                    size,
+                    value
                 );
             }
         } else {
-            println!(
+            log::trace!(
                 "pc: {:x} mem_type: {:?} address: {:x} size: {:x} value: {:x}",
-                pc, mem_type, address, size, value
+                pc,
+                mem_type,
+                address,
+                size,
+                value
             );
         }
 
