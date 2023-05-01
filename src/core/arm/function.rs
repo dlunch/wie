f7fb73f96304e79fdfc70b9b339270afcb76bb05
Inseok Lee <git@dlun.ch>
2023-05-01 15:25:46 +0900

    Support more param

--- src/core/arm/function.rs
+++ src/core/arm/function.rs
@@ -41,6 +41,22 @@ where
     }
 }
 
+impl<Func, C, Param1, Param2, Param3> EmulatedFunction<(Param1, Param2, Param3), C> for Func
+where
+    Func: Fn(&mut ArmCore, C, Param1, Param2, Param3) -> u32,
+    Param1: EmulatedFunctionParam<Param1>,
+    Param2: EmulatedFunctionParam<Param2>,
+    Param3: EmulatedFunctionParam<Param3>,
+{
+    fn call(&self, core: &mut ArmCore, context: C) -> u32 {
+        let param1 = Param1::get(core, 0);
+        let param2 = Param2::get(core, 1);
+        let param3 = Param3::get(core, 2);
+
+        self(core, context, param1, param2, param3)
+    }
+}
+
 pub trait EmulatedFunctionParam<T> {
     fn get(core: &mut ArmCore, pos: usize) -> T;
 
