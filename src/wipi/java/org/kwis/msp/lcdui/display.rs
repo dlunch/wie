e95528ebba256a626324facb5b164484f33daf6b
Inseok Lee <git@dlun.ch>
2023-05-03 16:33:12 +0900

    Add getDisplay skeleton

--- src/wipi/java/org/kwis/msp/lcdui/display.rs
+++ src/wipi/java/org/kwis/msp/lcdui/display.rs
@@ -9,16 +9,29 @@ impl Display {
     pub fn as_java_impl() -> JavaClassImpl {
         JavaClassImpl {
             name: "org/kwis/msp/lcdui/Display".to_owned(),
-            methods: vec![JavaMethodImpl {
-                name: "<init>".into(),
-                signature: "()V".into(),
-                body: Box::new(Self::init),
-            }],
+            methods: vec![
+                JavaMethodImpl {
+                    name: "<init>".into(),
+                    signature: "()V".into(),
+                    body: Box::new(Self::init),
+                },
+                JavaMethodImpl {
+                    name: "getDisplay".into(),
+                    signature: "(Ljava/lang/String;)Lorg/kwis/msp/lcdui/Display;".into(),
+                    body: Box::new(Self::get_display),
+                },
+            ],
         }
     }
 
     fn init(_: Vec<Box<dyn Any>>) -> Box<dyn Any> {
         log::debug!("Display::init");
+
+        Box::new(())
+    }
+
+    fn get_display(_: Vec<Box<dyn Any>>) -> Box<dyn Any> {
+        log::debug!("Display::getDisplay");
 
         Box::new(())
     }
