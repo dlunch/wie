eee404a53c7da65c0e53307e677d9983a94270b2
Inseok Lee <git@dlun.ch>
2023-05-04 21:47:31 +0900

    Rename ProgramInfo to ModuleInfo

--- src/wipi/module/ktf.rs
+++ src/wipi/module/ktf.rs
@@ -47,10 +47,10 @@ impl KtfWipiModule {
     }
 
     fn init(core: &mut ArmCore, context: &Context, base_address: u32, bss_size: u32, main_class: &str) -> anyhow::Result<u32> {
-        let program = runtime::init(core, context, base_address, bss_size)?;
+        let module = runtime::init(core, context, base_address, bss_size)?;
 
-        log::info!("Call wipi init at {:#x}", program.fn_init);
-        let result = core.run_function(program.fn_init, &[])?;
+        log::info!("Call wipi init at {:#x}", module.fn_init);
+        let result = core.run_function(module.fn_init, &[])?;
         if result != 0 {
             return Err(anyhow::anyhow!("wipi init failed with code {:#x}", result));
         }
@@ -58,8 +58,8 @@ impl KtfWipiModule {
         let main_class_name = context.alloc(20)?; // TODO size fix
         core.write_raw(main_class_name, main_class.as_bytes())?;
 
-        log::info!("Call class getter at {:#x}", program.fn_get_class);
-        let main_class = core.run_function(program.fn_get_class, &[main_class_name])?;
+        log::info!("Call class getter at {:#x}", module.fn_get_class);
+        let main_class = core.run_function(module.fn_get_class, &[main_class_name])?;
         if main_class == 0 {
             return Err(anyhow::anyhow!("Failed to get main class"));
         }
