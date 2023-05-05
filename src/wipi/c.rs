add1b6e20b50740264c2cc2ec50a2586faefe1d9
Inseok Lee <git@dlun.ch>
2023-05-05 22:31:26 +0900

    Add kernel interface

--- src/wipi/c.rs
+++ src/wipi/c.rs
@@ -1,14 +1,20 @@
 mod graphics;
+mod kernel;
 mod method;
 
 pub use graphics::get_graphics_method_table;
+pub use kernel::get_kernel_method_table;
 
-use self::method::{CMethodBody, CMethodImpl};
+pub use self::method::{CMethodBody, CMethodImpl};
 
 pub type CError = anyhow::Error;
 pub type CResult<T> = anyhow::Result<T>;
 
-pub trait Bridge {}
+pub trait Bridge {
+    fn alloc(&mut self, size: u32) -> CResult<u32>;
+    fn write_raw(&mut self, address: u32, data: &[u8]) -> CResult<()>;
+    fn register_function(&mut self, method: Box<dyn Fn(&mut dyn Bridge) -> CResult<u32>>) -> CResult<u32>;
+}
 
 fn into_body<M, F, R, P>(method: M) -> Box<dyn CMethodBody<CError>>
 where
