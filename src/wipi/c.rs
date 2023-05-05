2f8dfdb67968c4d5b7e5ee35b417e5374bf5d8fd
Inseok Lee <git@dlun.ch>
2023-05-05 22:33:39 +0900

    Clippy

--- src/wipi/c.rs
+++ src/wipi/c.rs
@@ -10,10 +10,11 @@ pub use self::method::{CMethodBody, CMethodImpl};
 pub type CError = anyhow::Error;
 pub type CResult<T> = anyhow::Result<T>;
 
+pub type BridgeMethod = Box<dyn Fn(&mut dyn Bridge) -> CResult<u32>>;
 pub trait Bridge {
     fn alloc(&mut self, size: u32) -> CResult<u32>;
     fn write_raw(&mut self, address: u32, data: &[u8]) -> CResult<()>;
-    fn register_function(&mut self, method: Box<dyn Fn(&mut dyn Bridge) -> CResult<u32>>) -> CResult<u32>;
+    fn register_function(&mut self, method: BridgeMethod) -> CResult<u32>;
 }
 
 fn into_body<M, F, R, P>(method: M) -> Box<dyn CMethodBody<CError>>
