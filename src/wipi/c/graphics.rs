77188625071ca60423d2e65d603cdf5105a8b214
Inseok Lee <git@dlun.ch>
2023-05-06 11:58:37 +0900

    Add graphics method dummy

--- src/wipi/c/graphics.rs
+++ src/wipi/c/graphics.rs
@@ -6,11 +6,17 @@ fn dummy(_: &mut CContext) -> CResult<u32> {
     Ok(0)
 }
 
+fn get_screen_frame_buffer(_: &mut CContext, a0: u32) -> CResult<u32> {
+    log::debug!("get_screen_frame_buffer({:#x})", a0);
+
+    Ok(1234)
+}
+
 pub fn get_graphics_method_table() -> Vec<CMethodBody> {
     vec![
         into_body(dummy),
         into_body(dummy),
-        into_body(dummy),
+        into_body(get_screen_frame_buffer),
         into_body(dummy),
         into_body(dummy),
         into_body(dummy),
