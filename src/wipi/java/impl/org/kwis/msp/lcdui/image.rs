2c8cdf08fc26bb82769ad4a188e54333f231e23b
Inseok Lee <git@dlun.ch>
2023-05-06 13:18:13 +0900

    Add more methods

--- src/wipi/java/impl/org/kwis/msp/lcdui/image.rs
+++ src/wipi/java/impl/org/kwis/msp/lcdui/image.rs
@@ -1,4 +1,4 @@
-use crate::wipi::java::{JavaClassProto, JavaContext, JavaMethodProto, JavaResult};
+use crate::wipi::java::{JavaClassProto, JavaContext, JavaMethodProto, JavaObjectProxy, JavaResult};
 
 // class org.kwis.msp.lcdui.Image
 pub struct Image {}
@@ -6,7 +6,11 @@ pub struct Image {}
 impl Image {
     pub fn as_proto() -> JavaClassProto {
         JavaClassProto {
-            methods: vec![JavaMethodProto::new("<init>", "(I)V", Self::init)],
+            methods: vec![
+                JavaMethodProto::new("<init>", "(I)V", Self::init),
+                JavaMethodProto::new("createImage", "(II)Lorg/kwis/msp/lcdui/Image;", Self::create_image),
+                JavaMethodProto::new("getGraphics", "()Lorg/kwis/msp/lcdui/Graphics;", Self::get_graphics),
+            ],
         }
     }
 
@@ -14,5 +18,17 @@ impl Image {
         log::debug!("Image::<init>");
 
         Ok(())
+    }
+
+    fn create_image(context: &mut JavaContext, a0: u32, a1: u32) -> JavaResult<JavaObjectProxy> {
+        log::debug!("Image::createImage({}, {})", a0, a1);
+
+        context.instantiate("Lorg/kwis/msp/lcdui/Image;")
+    }
+
+    fn get_graphics(context: &mut JavaContext) -> JavaResult<JavaObjectProxy> {
+        log::debug!("Image::get_graphics");
+
+        context.instantiate("Lorg/kwis/msp/lcdui/Graphics;")
     }
 }
