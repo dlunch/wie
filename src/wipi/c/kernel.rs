1df1cbaeac4d70b0ebfaf620070411e7353b0638
Inseok Lee <git@dlun.ch>
2023-05-07 15:14:45 +0900

    Add kernel stub

--- src/wipi/c/kernel.rs
+++ src/wipi/c/kernel.rs
@@ -6,6 +6,18 @@ fn stub(_: &mut CContext) -> CResult<u32> {
     Ok(0)
 }
 
+fn current_time(_: &mut CContext) -> CResult<u32> {
+    log::debug!("current_time()");
+
+    Ok(0)
+}
+
+fn alloc(context: &mut CContext, size: u32) -> CResult<u32> {
+    log::debug!("alloc({:#x})", size);
+
+    context.alloc(size)
+}
+
 pub fn get_kernel_method_table<M, F, R, P>(reserved1: M) -> Vec<CMethodBody>
 where
     M: MethodImpl<F, R, CError, CContext, P>,
@@ -31,6 +43,7 @@ where
         into_body(stub),
         into_body(stub),
         into_body(stub),
+        into_body(alloc),
         into_body(stub),
         into_body(stub),
         into_body(stub),
@@ -38,8 +51,7 @@ where
         into_body(stub),
         into_body(stub),
         into_body(stub),
-        into_body(stub),
-        into_body(stub),
+        into_body(current_time),
         into_body(stub),
         into_body(stub),
         into_body(stub),
