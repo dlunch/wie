695b21998d0cad2db6b4bb4f1b864d4146d2e310
Inseok Lee <git@dlun.ch>
2023-05-16 21:52:56 +0900

    Implement currentTimeMillis

--- crates/wie_wipi_java/src/impl/java/lang/system.rs
+++ crates/wie_wipi_java/src/impl/java/lang/system.rs
@@ -1,6 +1,6 @@
 use alloc::vec;
 
-use crate::base::JavaClassProto;
+use crate::base::{JavaClassProto, JavaContext, JavaMethodProto, JavaResult};
 
 // class java.lang.System
 pub struct System {}
@@ -8,8 +8,14 @@ pub struct System {}
 impl System {
     pub fn as_proto() -> JavaClassProto {
         JavaClassProto {
-            methods: vec![],
+            methods: vec![JavaMethodProto::new("currentTimeMillis", "()J", Self::current_time_millis)],
             fields: vec![],
         }
+    }
+
+    fn current_time_millis(_: &mut JavaContext) -> JavaResult<u32> {
+        log::debug!("System::current_time_millis()");
+
+        Ok(0)
     }
 }
