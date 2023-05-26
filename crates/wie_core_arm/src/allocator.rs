b4155fa4d9f0ead1ff805836a3206e42b41cd253
Inseok Lee <git@dlun.ch>
2023-05-26 20:51:44 +0900

    Change allocationheader to 4byte

--- crates/wie_core_arm/src/allocator.rs
+++ crates/wie_core_arm/src/allocator.rs
@@ -8,8 +8,23 @@ const HEAP_SIZE: u32 = 0x100000;
 
 #[derive(Clone, Copy)]
 struct AllocationHeader {
-    size: u32,
-    in_use: u32,
+    data: u32,
+}
+
+impl AllocationHeader {
+    pub fn new(size: u32, in_use: bool) -> Self {
+        Self {
+            data: size | (in_use as u32) << 31,
+        }
+    }
+
+    pub fn size(&self) -> u32 {
+        self.data & 0x7FFFFFFF
+    }
+
+    pub fn in_use(&self) -> bool {
+        self.data & 0x80000000 != 0
+    }
 }
 
 pub struct Allocator {}
@@ -18,7 +33,7 @@ impl Allocator {
     pub fn init(core: &mut ArmCore) -> anyhow::Result<(u32, u32)> {
         core.alloc(HEAP_BASE, HEAP_SIZE)?;
 
-        let header = AllocationHeader { size: HEAP_SIZE, in_use: 0 };
+        let header = AllocationHeader::new(HEAP_SIZE, false);
 
         write_generic(core, HEAP_BASE, header)?;
 
@@ -32,35 +47,29 @@ impl Allocator {
 
         let previous_header = read_generic::<AllocationHeader>(core, address)?;
 
-        let header = AllocationHeader { size: alloc_size, in_use: 1 };
+        let header = AllocationHeader::new(alloc_size, true);
         write_generic(core, address, header)?;
 
         // write next
-        if previous_header.size > alloc_size {
-            let next_header = AllocationHeader {
-                size: previous_header.size - alloc_size,
-                in_use: 0,
-            };
+        if previous_header.size() > alloc_size {
+            let next_header = AllocationHeader::new(previous_header.size() - alloc_size, false);
             write_generic(core, address + alloc_size, next_header)?;
         }
 
-        log::trace!("Allocated {:#x} bytes at {:#x}", size, address + 8);
+        log::trace!("Allocated {:#x} bytes at {:#x}", size, address + size_of::<AllocationHeader>() as u32);
 
-        Ok(address + 8)
+        Ok(address + size_of::<AllocationHeader>() as u32)
     }
 
     pub fn free(core: &mut ArmCore, address: u32) -> anyhow::Result<()> {
-        let base_address = address - 8;
+        let base_address = address - size_of::<AllocationHeader>() as u32;
 
         log::trace!("Freeing {:#x}", base_address);
 
         let header = read_generic::<AllocationHeader>(core, base_address)?;
-        assert!(header.in_use == 1);
+        assert!(header.in_use());
 
-        let header = AllocationHeader {
-            size: header.size,
-            in_use: 0,
-        };
+        let header = AllocationHeader::new(header.size(), false);
         write_generic(core, base_address, header)?;
 
         Ok(())
@@ -70,10 +79,10 @@ impl Allocator {
         let mut cursor = HEAP_BASE;
         loop {
             let header = read_generic::<AllocationHeader>(core, cursor).ok()?;
-            if header.in_use == 0 && header.size >= request_size {
+            if !header.in_use() && header.size() >= request_size {
                 return Some(cursor);
             } else {
-                cursor += header.size;
+                cursor += header.size();
             }
 
             if cursor >= HEAP_BASE + HEAP_SIZE {
