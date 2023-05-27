8e7f01b9015d426196b1d3b1f072ee38df65e4fd
Inseok Lee <git@dlun.ch>
2023-05-27 16:59:04 +0900

    Add database api stub

---
+++ crates/wie_wipi_c/src/database.rs
@@ -0,0 +1,35 @@
+use alloc::{vec, vec::Vec};
+
+use crate::{
+    base::{CContext, CMethodBody},
+    method::MethodImpl,
+};
+
+fn gen_stub(id: u32) -> CMethodBody {
+    let body = move |_: &mut dyn CContext| async move {
+        log::warn!("database stub{} called", id);
+
+        Ok(0)
+    };
+
+    body.into_body()
+}
+
+pub fn get_database_method_table() -> Vec<CMethodBody> {
+    vec![
+        gen_stub(0),
+        gen_stub(1),
+        gen_stub(2),
+        gen_stub(3),
+        gen_stub(4),
+        gen_stub(5),
+        gen_stub(6),
+        gen_stub(7),
+        gen_stub(8),
+        gen_stub(9),
+        gen_stub(10),
+        gen_stub(11),
+        gen_stub(12),
+        gen_stub(13),
+    ]
+}
