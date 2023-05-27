835ab65cc24b51458aed6fbbc70f32f0268135fd
Inseok Lee <git@dlun.ch>
2023-05-27 19:13:04 +0900

    Add HandsetProperty

--- crates/wie_wipi_java/src/impl/org/kwis/msp/handset.rs
+++ crates/wie_wipi_java/src/impl/org/kwis/msp/handset.rs
@@ -1,3 +1,4 @@
 mod backlight;
+mod handset_property;
 
-pub use self::backlight::BackLight;
+pub use self::{backlight::BackLight, handset_property::HandsetProperty};
--- crates/wie_wipi_java/src/impl/org/kwis/msp/handset/backlight.rs
+++ crates/wie_wipi_java/src/impl/org/kwis/msp/handset/backlight.rs
@@ -1,9 +1,6 @@
 use alloc::vec;
 
-use crate::{
-    base::{JavaClassProto, JavaContext, JavaMethodProto, JavaResult},
-    proxy::JavaObjectProxy,
-};
+use crate::base::{JavaClassProto, JavaContext, JavaMethodProto, JavaResult};
 
 // class org.kwis.msp.handset.Backlight
 pub struct BackLight {}
@@ -11,18 +8,9 @@ pub struct BackLight {}
 impl BackLight {
     pub fn as_proto() -> JavaClassProto {
         JavaClassProto {
-            methods: vec![
-                JavaMethodProto::new("<init>", "()V", Self::init),
-                JavaMethodProto::new("alwaysOn", "()V", Self::always_on),
-            ],
+            methods: vec![JavaMethodProto::new("alwaysOn", "()V", Self::always_on)],
             fields: vec![],
         }
-    }
-
-    async fn init(_: &mut dyn JavaContext, instance: JavaObjectProxy) -> JavaResult<()> {
-        log::warn!("stub Backlight::<init>({:#x})", instance.ptr_instance);
-
-        Ok(())
     }
 
     async fn always_on(_: &mut dyn JavaContext) -> JavaResult<()> {
