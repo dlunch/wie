aef8ed4957edbef6ff38745144cc7001bec4f371
Inseok Lee <git@dlun.ch>
2023-05-27 18:44:10 +0900

    Take care of offseted memory

--- crates/wie_wipi_c/src/graphics.rs
+++ crates/wie_wipi_c/src/graphics.rs
@@ -5,7 +5,7 @@ use core::mem::size_of;
 use wie_base::util::write_generic;
 
 use crate::{
-    base::{CContext, CMethodBody, CResult},
+    base::{CContext, CMemoryId, CMethodBody, CResult},
     method::MethodImpl,
 };
 
@@ -15,9 +15,7 @@ struct Framebuffer {
     height: u32,
     bpl: u32,
     bpp: u32,
-    id: u32,
-    unk1: u32,
-    ptr_unk2: u32,
+    id: CMemoryId,
 }
 
 fn gen_stub(id: u32) -> CMethodBody {
@@ -30,31 +28,24 @@ fn gen_stub(id: u32) -> CMethodBody {
     body.into_body()
 }
 
-async fn get_screen_frame_buffer(context: &mut dyn CContext, a0: u32) -> CResult<u32> {
+async fn get_screen_frame_buffer(context: &mut dyn CContext, a0: u32) -> CResult<CMemoryId> {
     log::debug!("get_screen_frame_buffer({:#x})", a0);
 
-    let ptr_unk3 = context.alloc(12)?;
-    let ptr_unk2 = context.alloc(12)?;
-    write_generic(context, ptr_unk2, ptr_unk3)?;
+    let franebuffer_data = context.alloc(320 * 480)?;
 
-    let ptr_framebuffer = context.alloc(size_of::<Framebuffer>() as u32)?;
     let framebuffer = Framebuffer {
         // TODO: hardcoded
         width: 320,
         height: 480,
         bpl: 1280,
         bpp: 32,
-        id: 0,
-        unk1: 0,
-        ptr_unk2,
+        id: franebuffer_data,
     };
 
-    write_generic(context, ptr_framebuffer, framebuffer)?;
+    let memory = context.alloc(size_of::<Framebuffer>() as u32)?;
+    write_generic(context, context.data_ptr(memory)?, framebuffer)?;
 
-    let address = context.alloc(4)?;
-    write_generic(context, address, ptr_framebuffer)?;
-
-    Ok(address)
+    Ok(memory)
 }
 
 async fn get_display_info(_: &mut dyn CContext, a0: u32, a1: u32) -> CResult<u32> {
