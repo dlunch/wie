99e386e6b1eb99fab2ab5ea79ef39f91bd8aec6d
Inseok Lee <git@dlun.ch>
2023-06-11 14:07:06 +0900

    Remove unused methods

--- crates/wie_wipi_java/src/impl/java/lang/string.rs
+++ crates/wie_wipi_java/src/impl/java/lang/string.rs
@@ -1,9 +1,6 @@
 use alloc::vec;
 
-use crate::{
-    base::{JavaClassProto, JavaContext, JavaMethodProto, JavaResult},
-    proxy::JavaObjectProxy,
-};
+use crate::base::JavaClassProto;
 
 // class java.lang.String
 pub struct String {}
@@ -11,14 +8,8 @@ pub struct String {}
 impl String {
     pub fn as_proto() -> JavaClassProto {
         JavaClassProto {
-            methods: vec![JavaMethodProto::new("<init>", "()V", Self::init)],
+            methods: vec![],
             fields: vec![],
         }
-    }
-
-    async fn init(_: &mut dyn JavaContext, instance: JavaObjectProxy) -> JavaResult<()> {
-        log::warn!("stub String::<init>({:#x})", instance.ptr_instance);
-
-        Ok(())
     }
 }
