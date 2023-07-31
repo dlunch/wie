43a822a462f6e993d3f53d63b188238784624dd8
Inseok Lee <git@dlun.ch>
2023-07-31 20:00:04 +0900

    Fix initparam0 invalid unk

--- crates/wie_vendor_ktf/src/runtime/init.rs
+++ crates/wie_vendor_ktf/src/runtime/init.rs
@@ -14,12 +14,6 @@ use crate::runtime::{
 #[repr(C)]
 #[derive(Clone, Copy)]
 struct InitParam0 {
-    ptr_unk_struct: u32,
-}
-
-#[repr(C)]
-#[derive(Clone, Copy)]
-struct InitParam0Unk {
     unk: u32,
 }
 
@@ -142,11 +136,8 @@ pub async fn init(core: &mut ArmCore, backend: &Backend, base_address: u32, bss_
 
     log::info!("Got wipi_exe {:#x}", wipi_exe);
 
-    let ptr_unk_struct = Allocator::alloc(core, size_of::<InitParam0Unk>() as u32)?;
-    write_generic(core, ptr_unk_struct, InitParam0Unk { unk: 0 })?;
-
     let ptr_param_0 = Allocator::alloc(core, size_of::<InitParam0>() as u32)?;
-    write_generic(core, ptr_param_0, InitParam0 { ptr_unk_struct })?;
+    write_generic(core, ptr_param_0, InitParam0 { unk: 0 })?;
 
     let ptr_unk_struct = Allocator::alloc(core, size_of::<InitParam1UnkUnk>() as u32)?;
     write_generic(core, ptr_unk_struct, InitParam1UnkUnk { unk: [0; 8] })?;
