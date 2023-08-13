fb3255a93c82826f7b201ea5fd9bdebe52d11660
Inseok Lee <git@dlun.ch>
2023-08-13 15:29:06 +0900

    Add StringBuffer stub

--- crates/wie_wipi_java/src/impl/java/lang.rs
+++ crates/wie_wipi_java/src/impl/java/lang.rs
@@ -4,10 +4,11 @@ mod object;
 mod runnable;
 mod runtime;
 mod string;
+mod string_buffer;
 mod system;
 mod thread;
 
 pub use self::{
     exception::Exception, interrupted_exception::InterruptedException, object::Object, runnable::Runnable, runtime::Runtime, string::String,
-    system::System, thread::Thread,
+    string_buffer::StringBuffer, system::System, thread::Thread,
 };
