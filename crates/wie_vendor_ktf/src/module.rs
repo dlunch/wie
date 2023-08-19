a1da8b7f48d11648c87457a5d31300e58f21e590
Inseok Lee <git@dlun.ch>
2023-08-19 18:22:21 +0900

    Change log level

--- crates/wie_vendor_ktf/src/module.rs
+++ crates/wie_vendor_ktf/src/module.rs
@@ -54,7 +54,7 @@ impl KtfWipiModule {
         let instance = java_context.instantiate_from_ptr_class(ptr_main_class)?;
         java_context.call_method(&instance, "<init>", "()V", &[]).await?;
 
-        log::info!("Main class instance: {:#x}", instance.ptr_instance);
+        log::debug!("Main class instance: {:#x}", instance.ptr_instance);
 
         let arg = java_context.instantiate_array("Ljava/lang/String;", 0)?;
         java_context
@@ -79,7 +79,7 @@ impl KtfWipiModule {
     async fn init(core: &mut ArmCore, backend: &Backend, base_address: u32, bss_size: u32, main_class_name: &str) -> anyhow::Result<u32> {
         let module = crate::runtime::init(core, backend, base_address, bss_size).await?;
 
-        log::info!("Call wipi init at {:#x}", module.fn_init);
+        log::debug!("Call wipi init at {:#x}", module.fn_init);
         let result = core.run_function::<u32>(module.fn_init, &[]).await?;
         if result != 0 {
             return Err(anyhow::anyhow!("wipi init failed with code {:#x}", result));
@@ -88,14 +88,14 @@ impl KtfWipiModule {
         let ptr_main_class_name = Allocator::alloc(core, 20)?; // TODO size fix
         core.write_bytes(ptr_main_class_name, main_class_name.as_bytes())?;
 
-        log::info!("Call class getter at {:#x}", module.fn_get_class);
+        log::debug!("Call class getter at {:#x}", module.fn_get_class);
         let ptr_main_class = core.run_function(module.fn_get_class, &[ptr_main_class_name]).await?;
         if ptr_main_class == 0 {
             return Err(anyhow::anyhow!("Failed to get main class"));
         }
         Allocator::free(core, ptr_main_class_name)?;
 
-        log::info!("Got main class: {:#x}", ptr_main_class);
+        log::debug!("Got main class: {:#x}", ptr_main_class);
 
         Ok(ptr_main_class)
     }
@@ -106,7 +106,7 @@ impl KtfWipiModule {
 
         let base_address = core.load(data, data.len() + bss_size as usize)?;
 
-        log::info!("Loaded at {:#x}, size {:#x}, bss {:#x}", base_address, data.len(), bss_size);
+        log::debug!("Loaded at {:#x}, size {:#x}, bss {:#x}", base_address, data.len(), bss_size);
 
         Ok((base_address, bss_size))
     }
