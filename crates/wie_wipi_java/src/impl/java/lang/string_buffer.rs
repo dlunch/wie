5420d9c4536271214566d318cec98cdf546da022
Inseok Lee <git@dlun.ch>
2023-08-20 15:57:30 +0900

    Implement stringbuffer append string

--- crates/wie_wipi_java/src/impl/java/lang/string_buffer.rs
+++ crates/wie_wipi_java/src/impl/java/lang/string_buffer.rs
@@ -1,7 +1,7 @@
 use alloc::vec;
 
 use crate::{
-    base::{JavaClassProto, JavaMethodAccessFlag, JavaMethodProto},
+    base::{JavaClassProto, JavaFieldProto, JavaMethodAccessFlag, JavaMethodProto},
     JavaContext, JavaObjectProxy, JavaResult,
 };
 
@@ -16,23 +16,61 @@ impl StringBuffer {
                 JavaMethodProto::new(
                     "append",
                     "(Ljava/lang/String;)Ljava/lang/StringBuffer;",
-                    Self::append,
+                    Self::append_string,
                     JavaMethodAccessFlag::NONE,
                 ),
             ],
-            fields: vec![],
+            fields: vec![
+                JavaFieldProto::new("value", "[C", crate::JavaFieldAccessFlag::NONE),
+                JavaFieldProto::new("count", "I", crate::JavaFieldAccessFlag::NONE),
+            ],
         }
     }
 
-    async fn init(_: &mut dyn JavaContext, _instance: JavaObjectProxy) -> JavaResult<()> {
-        log::warn!("stub java.lang.StringBuffer::<init>()");
+    async fn init(context: &mut dyn JavaContext, instance: JavaObjectProxy) -> JavaResult<()> {
+        log::trace!("java.lang.StringBuffer::<init>({:#x})", instance.ptr_instance);
+
+        let value = context.instantiate_array("C", 16)?;
+        context.put_field(&instance, "value", value.ptr_instance)?;
+        context.put_field(&instance, "count", 0)?;
 
         Ok(())
     }
 
-    async fn append(_: &mut dyn JavaContext, _instance: JavaObjectProxy, _a1: JavaObjectProxy) -> JavaResult<JavaObjectProxy> {
-        log::warn!("stub java.lang.StringBuffer::append()");
+    async fn append_string(context: &mut dyn JavaContext, instance: JavaObjectProxy, string: JavaObjectProxy) -> JavaResult<JavaObjectProxy> {
+        log::trace!(
+            "stub java.lang.StringBuffer::append({:#x}, {:#x})",
+            instance.ptr_instance,
+            string.ptr_instance
+        );
+        let current_count = context.get_field(&instance, "count")?;
 
-        Ok(JavaObjectProxy::new(0))
+        let ptr_value_to_add = JavaObjectProxy::new(context.get_field(&string, "value")?);
+        let count_to_add = context.get_field(&string, "length")?;
+        let value_to_add = context.load_array(&ptr_value_to_add, 0, count_to_add)?;
+
+        StringBuffer::ensure_capacity(context, &instance, current_count + count_to_add)?;
+
+        let value = JavaObjectProxy::new(context.get_field(&instance, "value")?);
+        context.store_array(&value, current_count, &value_to_add)?;
+
+        Ok(instance)
+    }
+
+    fn ensure_capacity(context: &mut dyn JavaContext, instance: &JavaObjectProxy, capacity: u32) -> JavaResult<()> {
+        let value = JavaObjectProxy::new(context.get_field(instance, "value")?);
+        let current_capacity = context.array_length(&value)?;
+
+        if current_capacity < capacity {
+            let old_values = context.load_array(&value, 0, current_capacity)?;
+            let new_capacity = capacity * 2;
+
+            let new_value = context.instantiate_array("C", new_capacity)?;
+            // TODO free existing array
+            context.put_field(instance, "value", new_value.ptr_instance)?;
+            context.store_array(&new_value, 0, &old_values)?;
+        }
+
+        Ok(())
     }
 }
