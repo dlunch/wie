d148a661338ac8042dd6ab9add1dca790912e4c0
Inseok Lee <git@dlun.ch>
2023-08-22 19:09:09 +0900

    Implement stub getResourceAsStream

--- crates/wie_wipi_java/src/impl/java/io/input_stream.rs
+++ crates/wie_wipi_java/src/impl/java/io/input_stream.rs
@@ -1,6 +1,9 @@
 use alloc::vec;
 
-use crate::base::JavaClassProto;
+use crate::{
+    base::{JavaClassProto, JavaMethodProto},
+    JavaContext, JavaMethodAccessFlag, JavaObjectProxy, JavaResult,
+};
 
 // class java.io.InputStream
 pub struct InputStream {}
@@ -8,8 +11,14 @@ pub struct InputStream {}
 impl InputStream {
     pub fn as_proto() -> JavaClassProto {
         JavaClassProto {
-            methods: vec![],
+            methods: vec![JavaMethodProto::new("<init>", "()V", Self::init, JavaMethodAccessFlag::NONE)],
             fields: vec![],
         }
+    }
+
+    async fn init(_: &mut dyn JavaContext, this: JavaObjectProxy<InputStream>) -> JavaResult<()> {
+        log::warn!("stub java.lang.InputStream::<init>({:#x})", this.ptr_instance);
+
+        Ok(())
     }
 }
