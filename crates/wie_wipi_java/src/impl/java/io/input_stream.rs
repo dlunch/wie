c666b0d083d568daf4a5d227d936bf24b4bdf753
Inseok Lee <git@dlun.ch>
2023-08-22 19:28:29 +0900

    Pass resource to InputStream

--- crates/wie_wipi_java/src/impl/java/io/input_stream.rs
+++ crates/wie_wipi_java/src/impl/java/io/input_stream.rs
@@ -1,23 +1,26 @@
 use alloc::vec;
 
 use crate::{
-    base::{JavaClassProto, JavaMethodProto},
-    JavaContext, JavaMethodAccessFlag, JavaObjectProxy, JavaResult,
+    base::{JavaClassProto, JavaFieldProto, JavaMethodProto},
+    Array, JavaContext, JavaFieldAccessFlag, JavaMethodAccessFlag, JavaObjectProxy, JavaResult,
 };
 
 // class java.io.InputStream
 pub struct InputStream {}
 
+// TODO create ByteArrayInputStream
 impl InputStream {
     pub fn as_proto() -> JavaClassProto {
         JavaClassProto {
-            methods: vec![JavaMethodProto::new("<init>", "()V", Self::init, JavaMethodAccessFlag::NONE)],
-            fields: vec![],
+            methods: vec![JavaMethodProto::new("<init>", "([B)V", Self::init, JavaMethodAccessFlag::NONE)],
+            fields: vec![JavaFieldProto::new("buf", "[B", JavaFieldAccessFlag::NONE)],
         }
     }
 
-    async fn init(_: &mut dyn JavaContext, this: JavaObjectProxy<InputStream>) -> JavaResult<()> {
-        log::warn!("stub java.lang.InputStream::<init>({:#x})", this.ptr_instance);
+    async fn init(context: &mut dyn JavaContext, this: JavaObjectProxy<InputStream>, data: JavaObjectProxy<Array>) -> JavaResult<()> {
+        log::warn!("stub java.lang.InputStream::<init>({:#x}, {:#x})", this.ptr_instance, data.ptr_instance);
+
+        context.put_field(&this.cast(), "buf", data.ptr_instance)?;
 
         Ok(())
     }
