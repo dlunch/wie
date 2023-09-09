d1483db032f6a284754ec1d4bd760871f3c1a2da
Inseok Lee <git@dlun.ch>
2023-09-09 14:21:20 +0900

    Fix clip constructor type

--- crates/wie_wipi_java/src/impl/org/kwis/msp/media/clip.rs
+++ crates/wie_wipi_java/src/impl/org/kwis/msp/media/clip.rs
@@ -3,6 +3,7 @@ use alloc::vec;
 use crate::{
     base::{JavaClassProto, JavaContext, JavaMethodFlag, JavaMethodProto, JavaResult},
     proxy::JavaObjectProxy,
+    r#impl::java::lang::String,
 };
 
 // class org.kwis.msp.media.Clip
@@ -13,13 +14,28 @@ impl Clip {
         JavaClassProto {
             parent_class: Some("java/lang/Object"),
             interfaces: vec![],
-            methods: vec![JavaMethodProto::new("<init>", "(I)V", Self::init, JavaMethodFlag::NONE)],
+            methods: vec![JavaMethodProto::new(
+                "<init>",
+                "(Ljava/lang/String;Ljava/lang/String;)V",
+                Self::init,
+                JavaMethodFlag::NONE,
+            )],
             fields: vec![],
         }
     }
 
-    async fn init(_: &mut dyn JavaContext, this: JavaObjectProxy<Clip>, a0: u32) -> JavaResult<()> {
-        log::warn!("stub org.kwis.msp.media.Clip::<init>({:#x}, {})", this.ptr_instance, a0);
+    async fn init(
+        _: &mut dyn JavaContext,
+        this: JavaObjectProxy<Clip>,
+        r#type: JavaObjectProxy<String>,
+        resource_name: JavaObjectProxy<String>,
+    ) -> JavaResult<()> {
+        log::warn!(
+            "stub org.kwis.msp.media.Clip::<init>({:#x}, {:#x}, {:#x})",
+            this.ptr_instance,
+            r#type.ptr_instance,
+            resource_name.ptr_instance
+        );
 
         Ok(())
     }
