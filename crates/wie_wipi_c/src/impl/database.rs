da5cd91c9a66d57aa69296c6a86d7ab47c14fa73
Jungmin Park <pjm0616@gmail.com>
2023-09-16 22:15:23 +0900

    wipi_c/database: Name stubs

--- crates/wie_wipi_c/src/impl/database.rs
+++ crates/wie_wipi_c/src/impl/database.rs
@@ -5,27 +5,27 @@ use crate::{
     method::MethodImpl,
 };
 
-fn gen_stub(id: u32) -> CMethodBody {
-    let body = move |_: &mut dyn CContext| async move { Err::<(), _>(anyhow::anyhow!("Unimplemented database{}", id)) };
+fn gen_stub(id: u32, name: &'static str) -> CMethodBody {
+    let body = move |_: &mut dyn CContext| async move { Err::<(), _>(anyhow::anyhow!("Unimplemented database{}: {}", id, name)) };
 
     body.into_body()
 }
 
 pub fn get_database_method_table() -> Vec<CMethodBody> {
     vec![
-        gen_stub(0),
-        gen_stub(1),
-        gen_stub(2),
-        gen_stub(3),
-        gen_stub(4),
-        gen_stub(5),
-        gen_stub(6),
-        gen_stub(7),
-        gen_stub(8),
-        gen_stub(9),
-        gen_stub(10),
-        gen_stub(11),
-        gen_stub(12),
-        gen_stub(13),
+        gen_stub(0, "MC_dbOpenDataBase"),
+        gen_stub(1, "MC_dbCloseDataBase"),
+        gen_stub(2, "MC_dbDeleteDataBase"),
+        gen_stub(3, "MC_dbInsertRecord"),
+        gen_stub(4, "MC_dbSelectRecord"),
+        gen_stub(5, "MC_dbUpdateRecord"),
+        gen_stub(6, "MC_dbDeleteRecord"),
+        gen_stub(7, "MC_dbListRecords"),
+        gen_stub(8, "MC_dbSortRecords"),
+        gen_stub(9, "MC_dbGetAccessMode"),
+        gen_stub(10, "MC_dbGetNumberOfRecords"),
+        gen_stub(11, "MC_dbGetRecordSize"),
+        gen_stub(12, "MC_dbListDataBases"),
+        gen_stub(13, ""),
     ]
 }
