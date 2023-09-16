c20ad8268ec77728e2cf032dc7f0444370d99f8a
Inseok Lee <git@dlun.ch>
2023-09-16 13:40:43 +0900

    Add random ctor

--- crates/wie_wipi_java/src/impl/java/util/random.rs
+++ crates/wie_wipi_java/src/impl/java/util/random.rs
@@ -1,6 +1,9 @@
 use alloc::vec;
 
-use crate::base::JavaClassProto;
+use crate::{
+    base::{JavaClassProto, JavaMethodProto},
+    JavaContext, JavaMethodFlag, JavaObjectProxy, JavaResult,
+};
 
 // class java.util.Random
 pub struct Random {}
@@ -10,8 +13,14 @@ impl Random {
         JavaClassProto {
             parent_class: Some("java/lang/Object"),
             interfaces: vec![],
-            methods: vec![],
+            methods: vec![JavaMethodProto::new("<init>", "()V", Self::init, JavaMethodFlag::NONE)],
             fields: vec![],
         }
+    }
+
+    async fn init(_: &mut dyn JavaContext, this: JavaObjectProxy<Random>) -> JavaResult<()> {
+        tracing::warn!("stub java.util.Random::<init>({:#x})", this.ptr_instance);
+
+        Ok(())
     }
 }
