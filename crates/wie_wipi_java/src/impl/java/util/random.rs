3e34b8875ba979fb154a2c55d8fa476c5676745f
Inseok Lee <git@dlun.ch>
2023-09-16 13:39:31 +0900

    Typo

--- crates/wie_wipi_java/src/impl/java/util/random.rs
+++ crates/wie_wipi_java/src/impl/java/util/random.rs
@@ -8,7 +8,7 @@ pub struct Random {}
 impl Random {
     pub fn as_proto() -> JavaClassProto {
         JavaClassProto {
-            parent_class: Some("java/util/Random"),
+            parent_class: Some("java/lang/Object"),
             interfaces: vec![],
             methods: vec![],
             fields: vec![],
