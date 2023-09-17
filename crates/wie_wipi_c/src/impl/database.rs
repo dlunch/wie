82d102f411cc668810f037983025ba1a1d3f1693
Park, Jeongmin <pjm0616@gmail.com>
2023-09-17 13:34:20 +0900

    wipi_c 그래픽 관련 메소드 추가 (#19)
    * wipi_c/graphics: Name stubs
    
    * wipi_c/media: Name stubs
    
    * wipi_c/kernel: Name stubs
    
    * wipi_c/database: Name stubs
    
    * wipi_c: Implement some stub methods
    
    * wipi_c/graphics: Implement fill_rect etc
    
    * cargo fmt
    
    * wipi_c/graphics: minor fix

--- crates/wie_wipi_c/src/impl/database.rs
+++ crates/wie_wipi_c/src/impl/database.rs
@@ -1,31 +1,48 @@
+use alloc::string::String;
 use alloc::{vec, vec::Vec};
 
 use crate::{
     base::{CContext, CMethodBody},
     method::MethodImpl,
+    CResult,
 };
 
-fn gen_stub(id: u32) -> CMethodBody {
-    let body = move |_: &mut dyn CContext| async move { Err::<(), _>(anyhow::anyhow!("Unimplemented database{}", id)) };
+fn gen_stub(id: u32, name: &'static str) -> CMethodBody {
+    let body = move |_: &mut dyn CContext| async move { Err::<(), _>(anyhow::anyhow!("Unimplemented database{}: {}", id, name)) };
 
     body.into_body()
 }
 
+async fn open_database(_context: &mut dyn CContext, name: String, record_size: u32, create: u32, mode: u32) -> CResult<u32> {
+    tracing::warn!("stub MC_dbOpenDataBase({}, {}, {}, {})", name, record_size, create, mode);
+    Ok(0)
+}
+
+async fn close_database(_context: &mut dyn CContext, db_id: u32) -> CResult<u32> {
+    tracing::warn!("stub MC_dbCloseDataBase({})", db_id);
+    Ok(0)
+}
+
+async fn insert_record(_context: &mut dyn CContext, db_id: u32, buf_ptr: u32, buf_len: u32) -> CResult<u32> {
+    tracing::warn!("stub MC_dbInsertRecord({}, {:#x}, {})", db_id, buf_ptr, buf_len);
+    Ok(0)
+}
+
 pub fn get_database_method_table() -> Vec<CMethodBody> {
     vec![
-        gen_stub(0),
-        gen_stub(1),
-        gen_stub(2),
-        gen_stub(3),
-        gen_stub(4),
-        gen_stub(5),
-        gen_stub(6),
-        gen_stub(7),
-        gen_stub(8),
-        gen_stub(9),
-        gen_stub(10),
-        gen_stub(11),
-        gen_stub(12),
-        gen_stub(13),
+        open_database.into_body(),
+        close_database.into_body(),
+        gen_stub(2, "MC_dbDeleteDataBase"),
+        insert_record.into_body(),
+        gen_stub(4, "MC_dbSelectRecord"),
+        gen_stub(5, "MC_dbUpdateRecord"),
+        gen_stub(6, "MC_dbDeleteRecord"),
+        gen_stub(7, "MC_dbListRecords"),
+        gen_stub(8, "MC_dbSortRecords"),
+        gen_stub(9, "MC_dbGetAccessMode"),
+        gen_stub(10, "MC_dbGetNumberOfRecords"),
+        gen_stub(11, "MC_dbGetRecordSize"),
+        gen_stub(12, "MC_dbListDataBases"),
+        gen_stub(13, ""),
     ]
 }
--- crates/wie_wipi_c/src/impl/graphics.rs
+++ crates/wie_wipi_c/src/impl/graphics.rs
@@ -1,21 +1,25 @@
 mod framebuffer;
+mod grp_context;
 mod image;
 
 use alloc::{vec, vec::Vec};
+use bytemuck::Zeroable;
 use core::mem::size_of;
 
-use wie_backend::Image;
+use wie_backend::{Color, Image};
 use wie_base::util::{read_generic, write_generic};
 
 use crate::{
     base::{CContext, CMemoryId, CMethodBody, CResult},
     method::MethodImpl,
+    r#impl::graphics::framebuffer::WIPICDisplayInfo,
+    r#impl::graphics::grp_context::{WIPICGraphicsContext, WIPICGraphicsContextIdx},
 };
 
 use self::{framebuffer::WIPICFramebuffer, image::WIPICImage};
 
-fn gen_stub(id: u32) -> CMethodBody {
-    let body = move |_: &mut dyn CContext| async move { Err::<(), _>(anyhow::anyhow!("Unimplemented graphics{}", id)) };
+fn gen_stub(id: u32, name: &'static str) -> CMethodBody {
+    let body = move |_: &mut dyn CContext| async move { Err::<(), _>(anyhow::anyhow!("Unimplemented graphics{}: {}", id, name)) };
 
     body.into_body()
 }
@@ -29,6 +33,77 @@ async fn get_screen_framebuffer(context: &mut dyn CContext, a0: u32) -> CResult<
     write_generic(context, context.data_ptr(memory)?, framebuffer)?;
 
     Ok(memory)
+}
+
+async fn init_context(context: &mut dyn CContext, p_grp_ctx: u32) -> CResult<()> {
+    tracing::trace!("MC_grpInitContext({:#x})", p_grp_ctx);
+    let grp_ctx: WIPICGraphicsContext = WIPICGraphicsContext::zeroed();
+    write_generic(context, p_grp_ctx, grp_ctx)?;
+    Ok(())
+}
+
+async fn set_context(context: &mut dyn CContext, p_grp_ctx: u32, op: WIPICGraphicsContextIdx, pv: u32) -> CResult<()> {
+    tracing::trace!("MC_grpSetContext({:#x}, {:?}, {:#x})", p_grp_ctx, op, pv);
+    let mut grp_ctx: WIPICGraphicsContext = read_generic(context, p_grp_ctx)?;
+    match op {
+        WIPICGraphicsContextIdx::ClipIdx => {
+            grp_ctx.clip = read_generic(context, pv)?;
+        }
+        WIPICGraphicsContextIdx::FgPixelIdx => {
+            grp_ctx.fgpxl = pv;
+        }
+        WIPICGraphicsContextIdx::BgPixelIdx => {
+            grp_ctx.bgpxl = pv;
+        }
+        WIPICGraphicsContextIdx::TransPixelIdx => {
+            grp_ctx.transpxl = pv;
+        }
+        WIPICGraphicsContextIdx::AlphaIdx => {
+            grp_ctx.alpha = pv;
+            // grp_ctx.pixel_op_func_ptr = todo!();
+            // grp_ctx.param1 = todo!();
+        }
+        WIPICGraphicsContextIdx::PixelopIdx => {
+            grp_ctx.pixel_op_func_ptr = pv;
+        }
+        WIPICGraphicsContextIdx::PixelParam1Idx => {
+            grp_ctx.param1 = pv;
+        }
+        WIPICGraphicsContextIdx::FontIdx => {
+            grp_ctx.font = pv;
+        }
+        WIPICGraphicsContextIdx::StyleIdx => {
+            grp_ctx.style = pv;
+        }
+        WIPICGraphicsContextIdx::OffsetIdx => {
+            grp_ctx.offset = read_generic(context, pv)?;
+        }
+        _ => {
+            tracing::warn!("MC_grpSetContext({:#x}, {:?}, {:#x}): ignoring invalid op", p_grp_ctx, op, pv);
+        }
+    }
+    write_generic(context, p_grp_ctx, grp_ctx)?;
+
+    Ok(())
+}
+
+async fn put_pixel(context: &mut dyn CContext, dst_fb: CMemoryId, x: u32, y: u32, p_gctx: u32) -> CResult<()> {
+    tracing::trace!("MC_grpPutPixel({:#x}, {:#x}, {:#x}, {:?})", dst_fb.0, x, y, p_gctx);
+    let framebuffer: WIPICFramebuffer = read_generic(context, context.data_ptr(dst_fb)?)?;
+    let gctx: WIPICGraphicsContext = read_generic(context, p_gctx)?;
+
+    let mut canvas = framebuffer.canvas(context)?;
+    canvas.put_pixel(x, y, Color::from_argb32(gctx.fgpxl));
+    Ok(())
+}
+
+async fn fill_rect(context: &mut dyn CContext, dst_fb: CMemoryId, x: u32, y: u32, w: u32, h: u32, p_gctx: u32) -> CResult<()> {
+    tracing::trace!("MC_grpFillRect({:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x})", dst_fb.0, x, y, w, h, p_gctx);
+    let framebuffer: WIPICFramebuffer = read_generic(context, context.data_ptr(dst_fb)?)?;
+    let gctx: WIPICGraphicsContext = read_generic(context, p_gctx)?;
+    let mut canvas = framebuffer.canvas(context)?;
+    canvas.draw_rect(x, y, w, h, Color::from_argb32(gctx.fgpxl));
+    Ok(())
 }
 
 async fn create_image(context: &mut dyn CContext, ptr_image: u32, image_data: CMemoryId, offset: u32, len: u32) -> CResult<u32> {
@@ -102,87 +177,119 @@ async fn flush(context: &mut dyn CContext, a0: u32, framebuffer: CMemoryId, a2: 
     Ok(0)
 }
 
+async fn get_pixel_from_rgb(_context: &mut dyn CContext, r: u32, g: u32, b: u32) -> CResult<u32> {
+    tracing::trace!("MC_grpGetPixelFromRGB({:#x}, {:#x}, {:#x})", r, g, b);
+    if (r > 0xff) || (g > 0xff) | (b > 0xff) {
+        tracing::debug!("MC_grpGetPixelFromRGB({:#x}, {:#x}, {:#x}): value clipped to 8 bits", r, g, b);
+    }
+
+    let rgb32 = Color::new(r as u8, g as u8, b as u8, 0).to_argb32();
+    Ok(rgb32)
+}
+
+async fn get_display_info(context: &mut dyn CContext, reserved: u32, out_ptr: u32) -> CResult<u32> {
+    tracing::debug!("MC_grpGetDisplayInfo({:#x}, {:#x})", reserved, out_ptr);
+
+    assert_eq!(reserved, 0);
+    let canvas = context.backend().screen_canvas();
+    assert_eq!(canvas.bytes_per_pixel(), 4);
+    let info = WIPICDisplayInfo {
+        bpp: canvas.bytes_per_pixel() * 8,
+        depth: 8,
+        width: canvas.width(),
+        height: canvas.height(),
+        bpl: canvas.bytes_per_pixel() * canvas.width(),
+        color_type: 1, // 1==MC_GRP_DIRECT_COLOR_TYPE
+        red_mask: 0xff0000,
+        blue_mask: 0xff00,
+        green_mask: 0xff,
+    };
+    drop(canvas);
+    write_generic(context, out_ptr, info)?;
+    Ok(1)
+}
+
 pub fn get_graphics_method_table() -> Vec<CMethodBody> {
     vec![
-        gen_stub(0),
-        gen_stub(1),
+        gen_stub(0, "MC_grpGetImageProperty"),
+        gen_stub(1, "MC_grpGetImageFrameBuffer"),
         get_screen_framebuffer.into_body(),
-        gen_stub(3),
-        gen_stub(4),
-        gen_stub(5),
-        gen_stub(6),
-        gen_stub(7),
-        gen_stub(8),
-        gen_stub(9),
-        gen_stub(10),
-        gen_stub(11),
-        gen_stub(12),
+        gen_stub(3, "MC_grpDestroyOffScreenFrameBuffer"),
+        gen_stub(4, "MC_grpCreateOffScreenFrameBuffer"),
+        init_context.into_body(),
+        set_context.into_body(),
+        gen_stub(7, "MC_grpGetContext"),
+        put_pixel.into_body(),
+        gen_stub(9, "MC_grpDrawLine"),
+        gen_stub(10, "MC_grpDrawRect"),
+        fill_rect.into_body(),
+        gen_stub(12, "MC_grpCopyFrameBuffer"),
         draw_image.into_body(),
-        gen_stub(14),
-        gen_stub(15),
-        gen_stub(16),
-        gen_stub(17),
-        gen_stub(18),
-        gen_stub(19),
-        gen_stub(20),
+        gen_stub(14, "MC_grpCopyArea"),
+        gen_stub(15, "MC_grpDrawArc"),
+        gen_stub(16, "MC_grpFillArc"),
+        gen_stub(17, "MC_grpDrawString"),
+        gen_stub(18, "MC_grpDrawUnicodeString"),
+        gen_stub(19, "MC_grpGetRGBPixels"),
+        gen_stub(20, "MC_grpSetRGBPixels"),
         flush.into_body(),
-        gen_stub(22),
-        gen_stub(23),
-        gen_stub(24),
-        gen_stub(25),
-        gen_stub(26),
-        gen_stub(27),
-        gen_stub(28),
-        gen_stub(29),
-        gen_stub(30),
-        gen_stub(31),
+        get_pixel_from_rgb.into_body(),
+        gen_stub(23, "MC_grpGetRGBFromPixel"),
+        get_display_info.into_body(),
+        gen_stub(25, "MC_grpRepaint"),
+        gen_stub(26, "MC_grpGetFont"),
+        gen_stub(27, "MC_grpGetFontHeight"),
+        gen_stub(28, "MC_grpGetFontAscent"),
+        gen_stub(29, "MC_grpGetFontDescent"),
+        gen_stub(30, "MC_grpGetStringWidth"),
+        gen_stub(31, "MC_grpGetUnicodeStringWidth"),
         create_image.into_body(),
-        gen_stub(33),
-        gen_stub(34),
-        gen_stub(35),
-        gen_stub(36),
-        gen_stub(37),
-        gen_stub(38),
-        gen_stub(39),
-        gen_stub(40),
-        gen_stub(41),
-        gen_stub(42),
-        gen_stub(43),
-        gen_stub(44),
-        gen_stub(45),
-        gen_stub(46),
-        gen_stub(47),
-        gen_stub(48),
-        gen_stub(49),
-        gen_stub(50),
-        gen_stub(51),
-        gen_stub(52),
-        gen_stub(53),
-        gen_stub(54),
-        gen_stub(55),
-        gen_stub(56),
-        gen_stub(57),
-        gen_stub(58),
-        gen_stub(59),
-        gen_stub(60),
-        gen_stub(61),
-        gen_stub(62),
-        gen_stub(63),
-        gen_stub(64),
-        gen_stub(65),
-        gen_stub(66),
-        gen_stub(67),
-        gen_stub(68),
-        gen_stub(69),
-        gen_stub(70),
-        gen_stub(71),
-        gen_stub(72),
-        gen_stub(73),
-        gen_stub(74),
-        gen_stub(75),
-        gen_stub(76),
-        gen_stub(77),
-        gen_stub(78),
-        gen_stub(79),
+        gen_stub(33, "MC_grpDestroyImage"),
+        gen_stub(34, "MC_grpDecodeNextImage"),
+        gen_stub(35, "MC_grpEncodeImage"),
+        gen_stub(36, "MC_grpPostEvent"),
+        gen_stub(37, "MC_imHandleInput"),
+        gen_stub(38, "MC_imSetCurrentMode"),
+        gen_stub(39, "MC_imGetCurrentMode"),
+        gen_stub(40, "MC_imGetSurpportModeCount"),
+        gen_stub(41, "MC_imGetSupportedModes"),
+        gen_stub(42, "MC_grpFillPolygon"),
+        gen_stub(43, "MC_grpDrawPolygon"),
+        gen_stub(44, "OEMC_grpShowAnnunciator"),
+        gen_stub(45, "OEMC_grpGetAnnunciatorInfo"),
+        gen_stub(46, "OEMC_grpSetAnnunciatorIcon"),
+        gen_stub(47, "OEMC_grpGetIdleHelpLineInfo"),
+        gen_stub(48, "OEMC_grpShowHelpLine"),
+        gen_stub(49, "OEMC_grpGetCharGlyph"),
+        gen_stub(50, "OEMC_grpCreateImageEx"),
+        gen_stub(51, "OEMC_grpHideHelpLine"),
+        gen_stub(52, "OEMC_grpSetCloneScreenFrameBuffer"),
+        gen_stub(53, "OEMC_grpGetFontEx"),
+        gen_stub(54, "OEMC_grpGetFontLists"),
+        gen_stub(55, "OEMC_grpGetFontInfo"),
+        gen_stub(56, "OEMC_grpSetFontHelpLine"),
+        gen_stub(57, "OEMC_grpGetFontHelpLine"),
+        gen_stub(58, "OEMC_grpEncodeImageEx"),
+        gen_stub(59, "OEMC_grpGetImageInfo"),
+        gen_stub(60, ""),
+        gen_stub(61, ""),
+        gen_stub(62, ""),
+        gen_stub(63, ""),
+        gen_stub(64, ""),
+        gen_stub(65, ""),
+        gen_stub(66, ""),
+        gen_stub(67, ""),
+        gen_stub(68, ""),
+        gen_stub(69, ""),
+        gen_stub(70, ""),
+        gen_stub(71, ""),
+        gen_stub(72, ""),
+        gen_stub(73, ""),
+        gen_stub(74, ""),
+        gen_stub(75, ""),
+        gen_stub(76, ""),
+        gen_stub(77, ""),
+        gen_stub(78, ""),
+        gen_stub(79, ""),
     ]
 }
