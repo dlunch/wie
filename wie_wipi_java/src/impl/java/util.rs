0608da128c407b7d8519642ab9902b06c3418796
Inseok Lee <git@dlun.ch>
2023-09-27 16:33:44 +0900

    Add TimerTask

--- wie_wipi_java/src/impl/java/util.rs
+++ wie_wipi_java/src/impl/java/util.rs
@@ -1,5 +1,6 @@
 mod hashtable;
 mod random;
+mod timer_task;
 mod vector;
 
-pub use self::{hashtable::Hashtable, random::Random, vector::Vector};
+pub use self::{hashtable::Hashtable, random::Random, timer_task::TimerTask, vector::Vector};
---
+++ wie_wipi_java/src/impl/java/util/timer_task.rs
@@ -0,0 +1,17 @@
+use alloc::vec;
+
+use crate::base::JavaClassProto;
+
+// class java.util.TimerTask
+pub struct TimerTask {}
+
+impl TimerTask {
+    pub fn as_proto() -> JavaClassProto {
+        JavaClassProto {
+            parent_class: Some("java/lang/Object"),
+            interfaces: vec![],
+            methods: vec![],
+            fields: vec![],
+        }
+    }
+}
