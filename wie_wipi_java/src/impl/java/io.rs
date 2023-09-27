99719317f4800e5b226c7f7718e64d55e9ecbb22
Inseok Lee <git@dlun.ch>
2023-09-27 16:31:26 +0900

    Add EOFException

--- wie_wipi_java/src/impl/java/io.rs
+++ wie_wipi_java/src/impl/java/io.rs
@@ -1,5 +1,6 @@
 mod byte_array_input_stream;
+mod eof_exception;
 mod input_stream;
 mod io_exception;
 
-pub use self::{byte_array_input_stream::ByteArrayInputStream, input_stream::InputStream, io_exception::IOException};
+pub use self::{byte_array_input_stream::ByteArrayInputStream, eof_exception::EOFException, input_stream::InputStream, io_exception::IOException};
