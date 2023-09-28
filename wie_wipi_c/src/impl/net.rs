d2a1699455860c7d9005c650407b82382bc007c6
Inseok Lee <git@dlun.ch>
2023-09-28 11:14:27 +0900

    Add net, util interface

---
+++ wie_wipi_c/src/impl/net.rs
@@ -0,0 +1,53 @@
+use alloc::{vec, vec::Vec};
+
+use crate::{
+    base::{CContext, CMethodBody, CResult, CWord},
+    method::MethodImpl,
+};
+
+fn gen_stub(id: CWord, name: &'static str) -> CMethodBody {
+    let body = move |_: &mut dyn CContext| async move { Err::<(), _>(anyhow::anyhow!("Unimplemented graphics{}: {}", id, name)) };
+
+    body.into_body()
+}
+
+async fn connect(_context: &mut dyn CContext, cb: CWord, param: CWord) -> CResult<i32> {
+    tracing::warn!("stub MC_netConnect({:#x}, {:#x})", cb, param);
+
+    Ok(-1) // M_E_ERROR
+}
+
+pub fn get_net_method_table() -> Vec<CMethodBody> {
+    vec![
+        connect.into_body(),
+        gen_stub(1, "MC_netClose"),
+        gen_stub(2, "MC_netSocket"),
+        gen_stub(3, "MC_netSocketConnect"),
+        gen_stub(4, "MC_netSocketWrite"),
+        gen_stub(5, "MC_netSocketRead"),
+        gen_stub(6, "MC_netSocketClose"),
+        gen_stub(7, "MC_netSocketBind"),
+        gen_stub(8, "MC_netGetMaxPacketLength"),
+        gen_stub(9, "MC_netSocketSendTo"),
+        gen_stub(10, "MC_netSocketRcvFrom"),
+        gen_stub(11, "MC_netGetHostAddr"),
+        gen_stub(12, "MC_netSocketAccept"),
+        gen_stub(13, "MC_netSetReadCB"),
+        gen_stub(14, "MC_netSetWriteCB"),
+        gen_stub(15, "MC_netHttpOpen"),
+        gen_stub(16, "MC_netHttpConnect"),
+        gen_stub(17, "MC_netHttpSetRequestMethod"),
+        gen_stub(18, "MC_netHttpGetRequestMethod"),
+        gen_stub(19, "MC_netHttpSetRequestProperty"),
+        gen_stub(20, "MC_netHttpGetRequestProperty"),
+        gen_stub(21, "MC_netHttpSetProxy"),
+        gen_stub(22, "MC_netHttpGetProxy"),
+        gen_stub(23, "MC_netHttpGetResponseCode"),
+        gen_stub(24, "MC_netHttpGetResponseMessage"),
+        gen_stub(25, "MC_netHttpGetHeaderField"),
+        gen_stub(26, "MC_netHttpGetLength"),
+        gen_stub(27, "MC_netHttpGetType"),
+        gen_stub(28, "MC_netHttpGetEncoding"),
+        gen_stub(29, "MC_netHttpClose"),
+    ]
+}
