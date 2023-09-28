93ba03ce90acd5397f271a0fd2cb60eb541c1dd5
Inseok Lee <git@dlun.ch>
2023-09-28 10:00:27 +0900

    Accept raw type on methods macro

--- wie_base/src/methods.rs
+++ wie_base/src/methods.rs
@@ -1,6 +1,6 @@
 #[macro_export]
 macro_rules! __impl_fn_helper {
-    ($context: ident, $($arg: ident),*) => {
+    ($context: ident, $raw_type: ty, $($arg: ident),*) => {
         impl<'a, E, R, F, Fut, $($arg),*> FnHelper<'a, E, R, ($($arg,)*)> for F
         where
             F: Fn(&'a mut dyn $context, $($arg),*) -> Fut,
@@ -9,7 +9,7 @@ macro_rules! __impl_fn_helper {
         {
             type Output = Fut;
             #[allow(unused_assignments, non_snake_case, unused_mut)]
-            fn do_call(&self, context: &'a mut dyn $context, args: &[u32]) -> Fut {
+            fn do_call(&self, context: &'a mut dyn $context, args: &[$raw_type]) -> Fut {
                 let mut index = 0;
                 $(
                     let $arg = $arg::to_rust(context, args[index]);
@@ -23,14 +23,14 @@ macro_rules! __impl_fn_helper {
 
 #[macro_export]
 macro_rules! __impl_method_body {
-    ($context: ident, $($arg: ident),*) => {
+    ($context: ident, $raw_type: ty, $($arg: ident),*) => {
         #[async_trait::async_trait(?Send)]
         impl<F, R, E, $($arg),*> MethodBody<E> for MethodHolder<F, R, ($($arg,)*)>
         where
             F: for<'a> FnHelper<'a, E, R, ($($arg,)*)>,
             R: TypeConverter<R>,
         {
-            async fn call(&self, context: &mut dyn $context, args: &[u32]) -> Result<u32, E> {
+            async fn call(&self, context: &mut dyn $context, args: &[$raw_type]) -> Result<$raw_type, E> {
                 let result = self.0.do_call(context, args).await?;
 
                 Ok(R::from_rust(context, result))
@@ -57,46 +57,46 @@ macro_rules! __impl_method_impl {
 
 #[macro_export]
 macro_rules! __generate {
-    ($context: ident, $($arg: ident),*) => {
-        $crate::__impl_fn_helper!($context, $($arg),*);
-        $crate::__impl_method_body!($context, $($arg),*);
+    ($context: ident, $raw_type: ty, $($arg: ident),*) => {
+        $crate::__impl_fn_helper!($context, $raw_type, $($arg),*);
+        $crate::__impl_method_body!($context, $raw_type, $($arg),*);
         $crate::__impl_method_impl!($($arg),*);
     };
 }
 
 #[macro_export]
 macro_rules! methods {
-    ($context: ident) => {
+    ($context: ident, $raw_type: ty) => {
         #[async_trait::async_trait(?Send)]
         pub trait MethodBody<E> {
-            async fn call(&self, context: &mut dyn $context, args: &[u32]) -> Result<u32, E>;
+            async fn call(&self, context: &mut dyn $context, args: &[$raw_type]) -> Result<$raw_type, E>;
         }
 
         trait FnHelper<'a, E, R, P> {
             type Output: Future<Output = Result<R, E>> + 'a;
-            fn do_call(&self, context: &'a mut dyn $context, args: &[u32]) -> Self::Output;
+            fn do_call(&self, context: &'a mut dyn $context, args: &[$raw_type]) -> Self::Output;
         }
 
         struct MethodHolder<F, R, P>(pub F, PhantomData<(R, P)>);
 
         pub trait TypeConverter<T> {
-            fn to_rust(context: &mut dyn $context, raw: u32) -> T;
-            fn from_rust(context: &mut dyn $context, rust: T) -> u32;
+            fn to_rust(context: &mut dyn $context, raw: $raw_type) -> T;
+            fn from_rust(context: &mut dyn $context, rust: T) -> $raw_type;
         }
 
         pub trait MethodImpl<F, R, E, P> {
             fn into_body(self) -> Box<dyn MethodBody<E>>;
         }
 
-        $crate::__generate!($context,);
-        $crate::__generate!($context, P0);
-        $crate::__generate!($context, P0, P1);
-        $crate::__generate!($context, P0, P1, P2);
-        $crate::__generate!($context, P0, P1, P2, P3);
-        $crate::__generate!($context, P0, P1, P2, P3, P4);
-        $crate::__generate!($context, P0, P1, P2, P3, P4, P5);
-        $crate::__generate!($context, P0, P1, P2, P3, P4, P5, P6);
-        $crate::__generate!($context, P0, P1, P2, P3, P4, P5, P6, P7);
-        $crate::__generate!($context, P0, P1, P2, P3, P4, P5, P6, P7, P8);
+        $crate::__generate!($context, $raw_type,);
+        $crate::__generate!($context, $raw_type, P0);
+        $crate::__generate!($context, $raw_type, P0, P1);
+        $crate::__generate!($context, $raw_type, P0, P1, P2);
+        $crate::__generate!($context, $raw_type, P0, P1, P2, P3);
+        $crate::__generate!($context, $raw_type, P0, P1, P2, P3, P4);
+        $crate::__generate!($context, $raw_type, P0, P1, P2, P3, P4, P5);
+        $crate::__generate!($context, $raw_type, P0, P1, P2, P3, P4, P5, P6);
+        $crate::__generate!($context, $raw_type, P0, P1, P2, P3, P4, P5, P6, P7);
+        $crate::__generate!($context, $raw_type, P0, P1, P2, P3, P4, P5, P6, P7, P8);
     };
 }
--- wie_wipi_c/src/method.rs
+++ wie_wipi_c/src/method.rs
@@ -5,4 +5,4 @@ use wie_base::methods;
 
 use crate::CContext;
 
-methods!(CContext);
+methods!(CContext, u32);
