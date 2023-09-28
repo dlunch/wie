36eac687b10b5e9e51c8cb418a653937443d9fd4
Inseok Lee <git@dlun.ch>
2023-09-28 10:41:40 +0900

    Add keyboard mapping

--- wie_base/src/lib.rs
+++ wie_base/src/lib.rs
@@ -5,9 +5,32 @@ extern crate alloc;
 pub mod methods;
 pub mod util;
 
+#[repr(i32)]
+#[derive(Debug, Copy, Clone)]
+pub enum WIPIKey {
+    UP = -1,
+    DOWN = -6,
+    LEFT = -2,
+    RIGHT = -5,
+    FIRE = -8, // Ok
+
+    NUM0 = 48,
+    NUM1 = 49,
+    NUM2 = 50,
+    NUM3 = 51,
+    NUM4 = 52,
+    NUM5 = 53,
+    NUM6 = 54,
+    NUM7 = 55,
+    NUM8 = 56,
+    NUM9 = 57,
+    HASH = 35, // #
+    STAR = 42, // *
+}
+
 #[derive(Debug)]
 pub enum Event {
     Redraw,
-    Keydown(u32),
-    Keyup(u32),
+    Keydown(WIPIKey),
+    Keyup(WIPIKey),
 }
