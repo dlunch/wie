e051cd91d47b2c57d1b697d0a0a9ab9fadfdcda0
Inseok Lee <git@dlun.ch>
2023-09-30 13:35:12 +0900

    Make it able to start on web

--- wie_web/src/rust/main.rs
+++ wie_web/src/rust/main.rs
@@ -1,26 +1,29 @@
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tracing_subscriber::{filter::LevelFilter, fmt::time::UtcTime, layer::SubscriberExt, util::SubscriberInitExt, Layer};
 use tracing_web::{performance_layer, MakeConsoleWriter};
-use wasm_bindgen::prelude::*;
+use wasm_bindgen::{prelude::*, JsError};
 
 use wie::Wie;
 use wie_backend::{Window, WindowCallbackEvent};
 use wie_base::Event;
 use wie_vendor_ktf::KtfArchive;
 
-#[wasm_bindgen]
-pub fn start(buf: &[u8]) {
+fn do_start(buf: &[u8]) -> anyhow::Result<()> {
     console_error_panic_hook::set_once();
 
-    let fmt_layer = tracing_subscriber::fmt::layer().with_writer(MakeConsoleWriter);
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .with_ansi(false)
+        .with_timer(UtcTime::rfc_3339())
+        .with_writer(MakeConsoleWriter)
+        .with_filter(LevelFilter::DEBUG);
     let perf_layer = performance_layer().with_details_from_fields(tracing_subscriber::fmt::format::Pretty::default());
 
     tracing_subscriber::registry().with(fmt_layer).with(perf_layer).init();
 
-    let archive = KtfArchive::from_zip(buf).unwrap();
+    let archive = KtfArchive::from_zip(buf)?;
 
-    let window = Window::new(240, 320); // TODO hardcoded size
+    let window = Window::new(240, 320)?; // TODO hardcoded size
 
-    let mut wie = Wie::new(Box::new(archive), window.proxy()).unwrap();
+    let mut wie = Wie::new(Box::new(archive), window.proxy())?;
 
     window.run(move |event| {
         match event {
@@ -31,4 +34,9 @@ pub fn start(buf: &[u8]) {
 
         anyhow::Ok(())
     })
+}
+
+#[wasm_bindgen]
+pub fn start(buf: &[u8]) -> Result<(), JsError> {
+    do_start(buf).map_err(|e| JsError::new(&e.to_string()))
 }
