4fad613ad6d05977d15538761a05c453179d4486
Inseok Lee <git@dlun.ch>
2023-09-30 12:19:57 +0900

    Add error hooks

--- wie_web/src/rust/main.rs
+++ wie_web/src/rust/main.rs
@@ -1,3 +1,5 @@
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tracing_web::{performance_layer, MakeConsoleWriter};
 use wasm_bindgen::prelude::*;
 
 use wie::Wie;
@@ -7,6 +9,13 @@ use wie_vendor_ktf::KtfArchive;
 
 #[wasm_bindgen]
 pub fn start(buf: &[u8]) {
+    console_error_panic_hook::set_once();
+
+    let fmt_layer = tracing_subscriber::fmt::layer().with_writer(MakeConsoleWriter);
+    let perf_layer = performance_layer().with_details_from_fields(tracing_subscriber::fmt::format::Pretty::default());
+
+    tracing_subscriber::registry().with(fmt_layer).with(perf_layer).init();
+
     let archive = KtfArchive::from_zip(buf).unwrap();
 
     let window = Window::new(240, 320); // TODO hardcoded size
