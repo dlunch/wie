867d20760a196268c77b011e06456bdcdcce2969
Inseok Lee <git@dlun.ch>
2023-09-30 14:15:08 +0900

    Split main method

--- wie_web/src/rust/main.rs
+++ wie_web/src/rust/main.rs
@@ -8,17 +8,6 @@ use wie_base::Event;
 use wie_vendor_ktf::KtfArchive;
 
 fn do_start(buf: &[u8]) -> anyhow::Result<()> {
-    console_error_panic_hook::set_once();
-
-    let fmt_layer = tracing_subscriber::fmt::layer()
-        .with_ansi(false)
-        .with_timer(UtcTime::rfc_3339())
-        .with_writer(MakeConsoleWriter)
-        .with_filter(LevelFilter::DEBUG);
-    let perf_layer = performance_layer().with_details_from_fields(tracing_subscriber::fmt::format::Pretty::default());
-
-    tracing_subscriber::registry().with(fmt_layer).with(perf_layer).init();
-
     let archive = KtfArchive::from_zip(buf)?;
 
     let window = Window::new(240, 320)?; // TODO hardcoded size
@@ -39,4 +28,18 @@ fn do_start(buf: &[u8]) -> anyhow::Result<()> {
 #[wasm_bindgen]
 pub fn start(buf: &[u8]) -> Result<(), JsError> {
     do_start(buf).map_err(|e| JsError::new(&e.to_string()))
+}
+
+#[wasm_bindgen(start)]
+pub fn main() {
+    console_error_panic_hook::set_once();
+
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .with_ansi(false)
+        .with_timer(UtcTime::rfc_3339())
+        .with_writer(MakeConsoleWriter)
+        .with_filter(LevelFilter::DEBUG);
+    let perf_layer = performance_layer().with_details_from_fields(tracing_subscriber::fmt::format::Pretty::default());
+
+    tracing_subscriber::registry().with(fmt_layer).with(perf_layer).init();
 }
