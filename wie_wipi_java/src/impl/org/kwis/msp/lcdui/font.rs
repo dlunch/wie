de403a0f11328e8caeb6a2f935b5f5c5f471d68c
Inseok Lee <git@dlun.ch>
2023-10-03 17:53:11 +0900

    Add getDefaultFont stub

--- wie_wipi_java/src/impl/org/kwis/msp/lcdui/font.rs
+++ wie_wipi_java/src/impl/org/kwis/msp/lcdui/font.rs
@@ -16,6 +16,12 @@ impl Font {
             methods: vec![
                 JavaMethodProto::new("<init>", "()V", Self::init, JavaMethodFlag::NONE),
                 JavaMethodProto::new("getHeight", "()I", Self::get_height, JavaMethodFlag::NONE),
+                JavaMethodProto::new(
+                    "getDefaultFont",
+                    "()Lorg/kwis/msp/lcdui/Font;",
+                    Self::get_default_font,
+                    JavaMethodFlag::STATIC,
+                ),
             ],
             fields: vec![],
         }
@@ -31,5 +37,14 @@ impl Font {
         tracing::warn!("stub org.kwis.msp.lcdui.Font::get_height");
 
         Ok(12) // TODO: hardcoded
+    }
+
+    async fn get_default_font(context: &mut dyn JavaContext) -> JavaResult<JavaObjectProxy<Font>> {
+        tracing::warn!("stub org.kwis.msp.lcdui.Font::get_default_font");
+
+        let instance = context.instantiate("Lorg/kwis/msp/lcdui/Font;").await?;
+        context.call_method(&instance, "<init>", "()V", &[]).await?;
+
+        Ok(instance.cast())
     }
 }
