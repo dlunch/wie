4faf45c8722514df259ef06256571a2776d632f0
Inseok Lee <git@dlun.ch>
2023-10-03 18:01:31 +0900

    Add file::init_with_flag

--- wie_wipi_java/src/impl/org/kwis/msp/io/file.rs
+++ wie_wipi_java/src/impl/org/kwis/msp/io/file.rs
@@ -14,7 +14,10 @@ impl File {
         JavaClassProto {
             parent_class: Some("java/lang/Object"),
             interfaces: vec![],
-            methods: vec![JavaMethodProto::new("<init>", "(Ljava/lang/String;I)V", Self::init, JavaMethodFlag::NONE)],
+            methods: vec![
+                JavaMethodProto::new("<init>", "(Ljava/lang/String;I)V", Self::init, JavaMethodFlag::NONE),
+                JavaMethodProto::new("<init>", "(Ljava/lang/String;II)V", Self::init_with_flag, JavaMethodFlag::NONE),
+            ],
             fields: vec![],
         }
     }
@@ -25,6 +28,27 @@ impl File {
             this.ptr_instance,
             filename.ptr_instance,
             mode
+        );
+
+        let filename = String::to_rust_string(context, &filename)?;
+        tracing::debug!("filename: {}", filename);
+
+        Ok(())
+    }
+
+    pub async fn init_with_flag(
+        context: &mut dyn JavaContext,
+        this: JavaObjectProxy<File>,
+        filename: JavaObjectProxy<String>,
+        mode: i32,
+        flag: i32,
+    ) -> JavaResult<()> {
+        tracing::warn!(
+            "stub org.kwis.msp.io.File::<init>({:#x}, {:#x}, {:#x}, {:#x})",
+            this.ptr_instance,
+            filename.ptr_instance,
+            mode,
+            flag
         );
 
         let filename = String::to_rust_string(context, &filename)?;
