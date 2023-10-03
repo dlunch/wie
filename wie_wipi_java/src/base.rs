26310522fc46060785d296af71d5fe94028f5a93
Inseok Lee <git@dlun.ch>
2023-10-03 14:12:19 +0900

    Add file stub

--- wie_wipi_java/src/base.rs
+++ wie_wipi_java/src/base.rs
@@ -159,6 +159,7 @@ pub fn get_class_proto(name: &str) -> Option<JavaClassProto> {
         "org/kwis/msp/db/DataBaseRecordException" => crate::r#impl::org::kwis::msp::db::DataBaseRecordException::as_proto(),
         "org/kwis/msp/handset/BackLight" => crate::r#impl::org::kwis::msp::handset::BackLight::as_proto(),
         "org/kwis/msp/handset/HandsetProperty" => crate::r#impl::org::kwis::msp::handset::HandsetProperty::as_proto(),
+        "org/kwis/msp/io/File" => crate::r#impl::org::kwis::msp::io::File::as_proto(),
         "org/kwis/msp/lcdui/Card" => crate::r#impl::org::kwis::msp::lcdui::Card::as_proto(),
         "org/kwis/msp/lcdui/Display" => crate::r#impl::org::kwis::msp::lcdui::Display::as_proto(),
         "org/kwis/msp/lcdui/EventQueue" => crate::r#impl::org::kwis::msp::lcdui::EventQueue::as_proto(),
--- wie_wipi_java/src/impl/org/kwis/msp.rs
+++ wie_wipi_java/src/impl/org/kwis/msp.rs
@@ -1,5 +1,6 @@
 pub mod db;
 pub mod handset;
+pub mod io;
 pub mod lcdui;
 pub mod lwc;
 pub mod media;
