02dcfa685e9520be7e86d6809b51e247777249d0
Inseok Lee <git@dlun.ch>
2023-10-03 17:57:32 +0900

    Add some stub methods

--- wie_wipi_java/src/impl/org/kwis/msp/lwc/component.rs
+++ wie_wipi_java/src/impl/org/kwis/msp/lwc/component.rs
@@ -1,6 +1,9 @@
 use alloc::vec;
 
-use crate::base::JavaClassProto;
+use crate::{
+    base::{JavaClassProto, JavaContext, JavaMethodFlag, JavaMethodProto, JavaResult},
+    proxy::JavaObjectProxy,
+};
 
 // class org.kwis.msp.lwc.Component
 pub struct Component {}
@@ -10,8 +13,19 @@ impl Component {
         JavaClassProto {
             parent_class: Some("java/lang/Object"),
             interfaces: vec![],
-            methods: vec![],
+            methods: vec![JavaMethodProto::new("keyNotify", "(II)Z", Self::key_notify, JavaMethodFlag::NONE)],
             fields: vec![],
         }
+    }
+
+    async fn key_notify(_: &mut dyn JavaContext, this: JavaObjectProxy<Component>, r#type: i32, chr: i32) -> JavaResult<i32> {
+        tracing::warn!(
+            "stub org.kwis.msp.lwc.Component::key_notify({:#x}, {:#x}, {:#x})",
+            this.ptr_instance,
+            r#type,
+            chr
+        );
+
+        Ok(1)
     }
 }
--- wie_wipi_java/src/impl/org/kwis/msp/lwc/text_field_component.rs
+++ wie_wipi_java/src/impl/org/kwis/msp/lwc/text_field_component.rs
@@ -1,6 +1,10 @@
 use alloc::vec;
 
-use crate::base::JavaClassProto;
+use crate::{
+    base::{JavaClassProto, JavaContext, JavaMethodFlag, JavaMethodProto, JavaResult},
+    proxy::JavaObjectProxy,
+    r#impl::java::lang::String,
+};
 
 // class org.kwis.msp.lwc.TextFieldComponent
 pub struct TextFieldComponent {}
@@ -10,8 +14,24 @@ impl TextFieldComponent {
         JavaClassProto {
             parent_class: Some("org/kwis/msp/lwc/TextComponent"),
             interfaces: vec![],
-            methods: vec![],
+            methods: vec![JavaMethodProto::new("<init>", "(Ljava/lang/String;I)V", Self::init, JavaMethodFlag::NONE)],
             fields: vec![],
         }
+    }
+
+    async fn init(
+        _: &mut dyn JavaContext,
+        this: JavaObjectProxy<TextFieldComponent>,
+        data: JavaObjectProxy<String>,
+        constraint: i32,
+    ) -> JavaResult<()> {
+        tracing::warn!(
+            "stub org.kwis.msp.lwc.TextFieldComponent::<init>({:#x}, {:#x}, {:#x})",
+            this.ptr_instance,
+            data.ptr_instance,
+            constraint
+        );
+
+        Ok(())
     }
 }
