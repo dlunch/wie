733c56ec1c10c0a0f7d03e09c5f13a7ee23e33c6
Inseok Lee <git@dlun.ch>
2023-10-11 19:56:17 +0900

    Make load to accept address

--- wie_vendor_ktf/src/app.rs
+++ wie_vendor_ktf/src/app.rs
@@ -8,10 +8,11 @@ use wie_wipi_java::r#impl::org::kwis::msp::lcdui::Jlet;
 
 use crate::runtime::KtfJavaContext;
 
+const IMAGE_BASE: u32 = 0x100000;
+
 pub struct KtfWipiApp {
     core: ArmCore,
     backend: Backend,
-    base_address: u32,
     bss_size: u32,
     main_class_name: String,
 }
@@ -26,22 +27,22 @@ impl KtfWipiApp {
         let filename = resource.files().find(|x| x.starts_with("client.bin")).context("Invalid archive")?;
         let data = resource.data(resource.id(filename).context("Resource not found")?);
 
-        let (base_address, bss_size) = Self::load(&mut core, data, filename)?;
+        let bss_size = Self::load(&mut core, data, filename)?;
 
         let main_class_name = main_class_name.replace('.', "/");
 
         Ok(Self {
             core,
             backend: backend.clone(),
-            base_address,
+
             bss_size,
             main_class_name,
         })
     }
 
     #[tracing::instrument(name = "start", skip_all)]
-    async fn do_start(core: &mut ArmCore, backend: &mut Backend, base_address: u32, bss_size: u32, main_class_name: String) -> anyhow::Result<()> {
-        let wipi_exe = crate::runtime::start(core, base_address, bss_size).await?;
+    async fn do_start(core: &mut ArmCore, backend: &mut Backend, bss_size: u32, main_class_name: String) -> anyhow::Result<()> {
+        let wipi_exe = crate::runtime::start(core, IMAGE_BASE, bss_size).await?;
         tracing::debug!("Got wipi_exe {:#x}", wipi_exe);
 
         let fn_init = crate::runtime::init(core, backend, wipi_exe).await?;
@@ -57,15 +58,15 @@ impl KtfWipiApp {
         Ok(())
     }
 
-    fn load(core: &mut ArmCore, data: &[u8], filename: &str) -> anyhow::Result<(u32, u32)> {
+    fn load(core: &mut ArmCore, data: &[u8], filename: &str) -> anyhow::Result<u32> {
         let bss_start = filename.find("client.bin").context("Incorrect filename")? + 10;
         let bss_size = filename[bss_start..].parse::<u32>()?;
 
-        let base_address = core.load(data, data.len() + bss_size as usize)?;
+        core.load(data, IMAGE_BASE, data.len() + bss_size as usize)?;
 
-        tracing::debug!("Loaded at {:#x}, size {:#x}, bss {:#x}", base_address, data.len(), bss_size);
+        tracing::debug!("Loaded at {:#x}, size {:#x}, bss {:#x}", IMAGE_BASE, data.len(), bss_size);
 
-        Ok((base_address, bss_size))
+        Ok(bss_size)
     }
 }
 
@@ -73,17 +74,17 @@ impl App for KtfWipiApp {
     fn start(&mut self) -> anyhow::Result<()> {
         let mut core = self.core.clone();
         let mut backend = self.backend.clone();
-        let base_address = self.base_address;
+
         let bss_size = self.bss_size;
         let main_class_name = self.main_class_name.clone();
 
         self.core
-            .spawn(move || async move { Self::do_start(&mut core, &mut backend, base_address, bss_size, main_class_name).await });
+            .spawn(move || async move { Self::do_start(&mut core, &mut backend, bss_size, main_class_name).await });
 
         Ok(())
     }
 
     fn crash_dump(&self) -> String {
-        self.core.dump_reg_stack()
+        self.core.dump_reg_stack(IMAGE_BASE)
     }
 }
