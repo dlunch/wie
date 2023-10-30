e6e0f152dcb7201ffbf8be7c77058701f7853a39
Inseok Lee <git@dlun.ch>
2023-10-30 18:06:57 +0900

    Simplify

--- wie/src/main.rs
+++ wie/src/main.rs
@@ -1,26 +1,20 @@
-#[cfg(not(target_arch = "wasm32"))]
+use std::io::stderr;
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    filename: String,
+}
 
 fn main() -> anyhow::Result<()> {
-    use std::io::stderr;
-
-    use clap::Parser;
-
-    use wie::start;
-
-    #[derive(Parser)]
-    struct Args {
-        filename: String,
-    }
-
     tracing_subscriber::fmt()
         .with_writer(stderr)
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .init();
 
-    let args = Args::parse();
-
-    start(&args.filename)
+    #[cfg(target_arch = "wasm32")]
+    unimplemented!();
+    #[cfg(not(target_arch = "wasm32"))]
+    wie::start(&Args::parse().filename)
 }
-
-#[cfg(target_arch = "wasm32")]
-fn main() {}
