2336730effe82c49f5c4694f3fda668feef86eaa
Inseok Lee <git@dlun.ch>
2023-11-03 09:40:46 +0900

    Make web database backend to do nothing instead of panic

--- wie_backend/src/backend/database/web.rs
+++ wie_backend/src/backend/database/web.rs
@@ -1,46 +1,51 @@
-#![allow(unused_variables)]
+#![allow(dead_code)]
+
 use super::RecordId;
 
-pub struct DatabaseRepository {}
+pub struct DatabaseRepository {
+    app_id: String,
+}
 
 impl DatabaseRepository {
     pub fn new(app_id: &str) -> Self {
-        unimplemented!()
+        Self { app_id: app_id.to_owned() }
     }
 
     pub fn open(&mut self, name: &str) -> anyhow::Result<Database> {
-        unimplemented!()
+        Database::new(name)
     }
 
-    pub fn delete(&mut self, name: &str) -> anyhow::Result<()> {
-        unimplemented!()
+    pub fn delete(&mut self, _name: &str) -> anyhow::Result<()> {
+        Ok(())
     }
 }
 
-pub struct Database {}
+pub struct Database {
+    name: String,
+}
 
 impl Database {
-    pub fn new() -> anyhow::Result<Self> {
-        unimplemented!()
+    pub fn new(name: &str) -> anyhow::Result<Self> {
+        Ok(Self { name: name.to_owned() })
     }
 
-    pub fn add(&mut self, data: &[u8]) -> anyhow::Result<RecordId> {
-        unimplemented!()
+    pub fn add(&mut self, _data: &[u8]) -> anyhow::Result<RecordId> {
+        Ok(0)
     }
 
-    pub fn get(&mut self, id: RecordId) -> anyhow::Result<Vec<u8>> {
-        unimplemented!()
+    pub fn get(&mut self, _id: RecordId) -> anyhow::Result<Vec<u8>> {
+        Ok(Vec::new())
     }
 
-    pub fn delete(&mut self, id: RecordId) -> anyhow::Result<()> {
-        unimplemented!()
+    pub fn delete(&mut self, _id: RecordId) -> anyhow::Result<()> {
+        Ok(())
     }
 
-    pub fn set(&mut self, id: RecordId, data: &[u8]) -> anyhow::Result<()> {
-        unimplemented!()
+    pub fn set(&mut self, _id: RecordId, _data: &[u8]) -> anyhow::Result<()> {
+        Ok(())
     }
 
     pub fn count(&self) -> anyhow::Result<usize> {
-        unimplemented!()
+        Ok(0)
     }
 }
