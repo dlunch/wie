cbdcab87b096d8e12477195a3fad78121ccb00bb
Inseok Lee <git@dlun.ch>
2023-11-11 16:03:55 +0900

    Add Card::repaint

--- wie_impl_java/src/impl/org/kwis/msp/lcdui/card.rs
+++ wie_impl_java/src/impl/org/kwis/msp/lcdui/card.rs
@@ -19,7 +19,8 @@ impl Card {
                 JavaMethodProto::new("<init>", "(I)V", Self::init_1, JavaMethodFlag::NONE),
                 JavaMethodProto::new("getWidth", "()I", Self::get_width, JavaMethodFlag::NONE),
                 JavaMethodProto::new("getHeight", "()I", Self::get_height, JavaMethodFlag::NONE),
-                JavaMethodProto::new("repaint", "(IIII)V", Self::repaint, JavaMethodFlag::NONE),
+                JavaMethodProto::new("repaint", "(IIII)V", Self::repaint_with_area, JavaMethodFlag::NONE),
+                JavaMethodProto::new("repaint", "()V", Self::repaint, JavaMethodFlag::NONE),
                 JavaMethodProto::new("serviceRepaints", "()V", Self::service_repaints, JavaMethodFlag::NONE),
             ],
             fields: vec![JavaFieldProto::new("display", "Lorg/kwis/msp/lcdui/Display;", JavaFieldAccessFlag::NONE)],
@@ -54,7 +55,15 @@ impl Card {
         Ok(screen_canvas.height() as _)
     }
 
-    async fn repaint(context: &mut dyn JavaContext, this: JavaObjectProxy<Card>, a0: i32, a1: i32, a2: i32, a3: i32) -> JavaResult<()> {
+    async fn repaint(context: &mut dyn JavaContext, this: JavaObjectProxy<Card>) -> JavaResult<()> {
+        tracing::debug!("org.kwis.msp.lcdui.Card::repaint({:#x})", this.ptr_instance);
+
+        context.backend().window().request_redraw()?;
+
+        Ok(())
+    }
+
+    async fn repaint_with_area(context: &mut dyn JavaContext, this: JavaObjectProxy<Card>, a0: i32, a1: i32, a2: i32, a3: i32) -> JavaResult<()> {
         tracing::warn!(
             "stub org.kwis.msp.lcdui.Card::repaint({:#x}, {}, {}, {}, {})",
             this.ptr_instance,
