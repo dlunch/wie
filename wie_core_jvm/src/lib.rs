e3ee175554761236ca9e48fc81b0d866b2f4879c
Inseok Lee <git@dlun.ch>
2023-12-03 16:15:10 +0900

    Update packages

--- wie_core_jvm/src/lib.rs
+++ wie_core_jvm/src/lib.rs
@@ -1,23 +1,24 @@
 #![no_std]
 extern crate alloc;
 
-use alloc::{fmt::Debug, format, rc::Rc};
+use alloc::{boxed::Box, fmt::Debug, format, rc::Rc};
 use core::cell::RefCell;
 
-use jvm::{Class, ClassLoader, Jvm};
+use jvm::{ArrayClass, Class, ClassLoader, Jvm, JvmResult};
+use jvm_impl::{ClassImpl, ThreadContextProviderImpl};
 
 use wie_backend::{task, AsyncCallable, Backend};
 
 pub type JvmCoreResult<T> = anyhow::Result<T>;
 
-#[derive(Clone, Default)]
+#[derive(Clone)]
 pub struct JvmCore {
     jvm: Rc<RefCell<Jvm>>,
 }
 
 impl JvmCore {
     pub fn new(backend: &Backend) -> Self {
-        let mut jvm = Jvm::new();
+        let mut jvm = Jvm::new(&ThreadContextProviderImpl {});
         jvm.add_class_loader(JvmCoreClassLoader { backend: backend.clone() });
 
         Self {
@@ -37,7 +38,9 @@ impl JvmCore {
     pub fn invoke_static_method(&mut self, main_class_name: &str, method_name: &str, descriptor: &str) -> JvmCoreResult<()> {
         let mut jvm = self.jvm.borrow_mut();
 
-        jvm.invoke_static_method(main_class_name, method_name, descriptor)
+        jvm.invoke_static_method(main_class_name, method_name, descriptor, &[])?;
+
+        Ok(())
     }
 }
 
@@ -46,13 +49,17 @@ struct JvmCoreClassLoader {
 }
 
 impl ClassLoader for JvmCoreClassLoader {
-    fn load(&mut self, class_name: &str) -> jvm::JvmResult<Option<jvm::Class>> {
+    fn load(&mut self, class_name: &str) -> JvmResult<Option<Box<dyn Class>>> {
         let path = format!("{}.class", class_name.replace('.', "/"));
 
         let resource = self.backend.resource();
         let resource_id = resource.id(&path).unwrap();
         let class_data = resource.data(resource_id);
 
-        Some(Class::from_classfile(class_data)).transpose()
+        Ok(Some(Box::new(ClassImpl::from_classfile(class_data)?)))
+    }
+
+    fn load_array_class(&mut self, _element_type_name: &str) -> JvmResult<Option<Box<dyn ArrayClass>>> {
+        Ok(None)
     }
 }
