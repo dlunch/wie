e7832a90bbda0290af5a144807a38c18822b56b0
Inseok Lee <git@dlun.ch>
2023-12-22 16:22:13 +0900

    Update packages

--- wie_core_jvm/src/lib.rs
+++ wie_core_jvm/src/lib.rs
@@ -4,7 +4,7 @@ extern crate alloc;
 use alloc::{boxed::Box, fmt::Debug, format, rc::Rc};
 use core::cell::RefCell;
 
-use jvm::{ArrayClass, Class, ClassLoader, Jvm, JvmResult};
+use jvm::{ArrayClass, Class, ClassLoader, ClassRegistry, Jvm, JvmDetail, JvmResult};
 use jvm_impl::{ArrayClassImpl, ClassImpl, ThreadContextProviderImpl};
 
 use wie_backend::{task, AsyncCallable, Backend};
@@ -18,7 +18,10 @@ pub struct JvmCore {
 
 impl JvmCore {
     pub fn new(backend: &Backend) -> Self {
-        let jvm = Jvm::new(JvmCoreClassLoader { backend: backend.clone() }, &ThreadContextProviderImpl {});
+        let jvm = Jvm::new(JvmDetailImpl {
+            class_loader: JvmCoreClassLoader { backend: backend.clone() },
+            thread_context_provider: ThreadContextProviderImpl {},
+        });
 
         Self {
             jvm: Rc::new(RefCell::new(jvm)),
@@ -34,12 +37,32 @@ impl JvmCore {
         task::spawn(callable)
     }
 
-    pub fn invoke_static_method(&mut self, main_class_name: &str, method_name: &str, descriptor: &str) -> JvmCoreResult<()> {
+    #[allow(clippy::await_holding_refcell_ref)]
+    pub async fn invoke_static_method(&mut self, main_class_name: &str, method_name: &str, descriptor: &str) -> JvmCoreResult<()> {
         let mut jvm = self.jvm.borrow_mut();
 
-        jvm.invoke_static_method(main_class_name, method_name, descriptor, &[])?;
+        jvm.invoke_static_method(main_class_name, method_name, descriptor, &[]).await?;
 
         Ok(())
+    }
+}
+
+struct JvmDetailImpl {
+    class_loader: JvmCoreClassLoader,
+    thread_context_provider: ThreadContextProviderImpl,
+}
+
+impl JvmDetail for JvmDetailImpl {
+    fn class_loader(&mut self) -> &mut dyn ClassLoader {
+        &mut self.class_loader
+    }
+
+    fn class_registry(&mut self) -> &mut dyn ClassRegistry {
+        todo!()
+    }
+
+    fn thread_context_provider(&self) -> &dyn jvm::ThreadContextProvider {
+        &self.thread_context_provider
     }
 }
 
