28f27b599650755cb865d01c75b6c2fbea89a672
Inseok Lee <git@dlun.ch>
2023-12-29 13:50:17 +0900

    Use shortcut

--- wie_ktf/src/runtime/java/context/method.rs
+++ wie_ktf/src/runtime/java/context/method.rs
@@ -221,13 +221,10 @@ impl Method for JavaMethod {
 
     async fn run(&self, _jvm: &mut Jvm, args: Box<[JavaValue]>) -> JvmResult<JavaValue> {
         let result = self.run(args).await?;
-        let return_type = if let JavaType::Method(_, x) = JavaType::parse(&self.descriptor()) {
-            x
-        } else {
-            panic!("Should be method type")
-        };
+        let r#type = JavaType::parse(&self.descriptor());
+        let (_, return_type) = r#type.as_method();
 
-        Ok(JavaValue::from_raw(result, &return_type, &self.core))
+        Ok(JavaValue::from_raw(result, return_type, &self.core))
     }
 }
 
