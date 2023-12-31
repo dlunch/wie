3eaf8b0b66298a0ea4efff618f3ef10e9ee98eea
Inseok Lee <git@dlun.ch>
2023-12-31 11:03:13 +0900

    Change window to winit

--- wie_cli/src/main.rs
+++ wie_cli/src/main.rs
@@ -1,6 +1,21 @@
-use std::io::stderr;
+extern crate alloc;
+
+mod window;
+
+use std::{
+    fs::File,
+    io::{stderr, Read},
+};
 
 use clap::Parser;
+
+use wie_backend::{extract_zip, Archive, Backend, Executor};
+use wie_base::{Event, KeyCode};
+use wie_ktf::KtfArchive;
+use wie_lgt::LgtArchive;
+use wie_skt::SktArchive;
+
+use self::window::{WindowCallbackEvent, WindowImpl};
 
 #[derive(Parser)]
 struct Args {
@@ -13,8 +28,78 @@ fn main() -> anyhow::Result<()> {
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .init();
 
-    #[cfg(target_arch = "wasm32")]
-    unimplemented!();
-    #[cfg(not(target_arch = "wasm32"))]
-    wie_cli::start(&Args::parse().filename)
+    start(&Args::parse().filename)
+}
+
+pub fn start(filename: &str) -> anyhow::Result<()> {
+    let mut file = File::open(filename)?;
+
+    let mut buf = Vec::new();
+    file.read_to_end(&mut buf)?;
+
+    let files = extract_zip(&buf)?;
+
+    let archive: Box<dyn Archive> = if KtfArchive::is_ktf_archive(&files) {
+        Box::new(KtfArchive::from_zip(files)?)
+    } else if LgtArchive::is_lgt_archive(&files) {
+        Box::new(LgtArchive::from_zip(files)?)
+    } else if SktArchive::is_skt_archive(&files) {
+        Box::new(SktArchive::from_zip(files)?)
+    } else {
+        anyhow::bail!("Unknown archive format");
+    };
+
+    let window = WindowImpl::new(240, 320)?; // TODO hardcoded size
+
+    let window_proxy = window.proxy();
+
+    let mut backend = Backend::new(&archive.id(), Box::new(window_proxy));
+    let mut app = archive.load_app(&mut backend)?;
+
+    let mut executor = Executor::new();
+    app.start()?;
+
+    window.run(move |event| {
+        match event {
+            WindowCallbackEvent::Update => executor
+                .tick(&backend.time())
+                .map_err(|x| anyhow::anyhow!("{}\n{}", x, app.crash_dump()))?,
+            WindowCallbackEvent::Redraw => backend.push_event(Event::Redraw),
+            WindowCallbackEvent::Keydown(x) => {
+                if let Some(keycode) = convert_key(x) {
+                    backend.push_event(Event::Keydown(keycode));
+                }
+            }
+            WindowCallbackEvent::Keyup(x) => {
+                if let Some(keycode) = convert_key(x) {
+                    backend.push_event(Event::Keyup(keycode));
+                }
+            }
+        }
+
+        anyhow::Ok(())
+    })
+}
+
+fn convert_key(key_code: u32) -> Option<KeyCode> {
+    match key_code {
+        2 => Some(KeyCode::NUM1),
+        3 => Some(KeyCode::NUM2),
+        4 => Some(KeyCode::NUM3),
+        16 => Some(KeyCode::NUM4), // Q
+        17 => Some(KeyCode::NUM5), // W
+        18 => Some(KeyCode::NUM6), // E
+        30 => Some(KeyCode::NUM7), // A
+        31 => Some(KeyCode::NUM8), // S
+        32 => Some(KeyCode::NUM9), // D
+        44 => Some(KeyCode::STAR), // Z
+        45 => Some(KeyCode::NUM0), // X
+        46 => Some(KeyCode::HASH), // C
+        57 => Some(KeyCode::OK),   // Space
+        103 => Some(KeyCode::UP),
+        108 => Some(KeyCode::DOWN),
+        105 => Some(KeyCode::LEFT),
+        106 => Some(KeyCode::RIGHT),
+        _ => None,
+    }
 }
