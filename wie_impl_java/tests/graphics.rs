d81bfe35e49f9027224070f83b0fb7b166357bf5
Inseok Lee <git@dlun.ch>
2024-01-07 13:45:15 +0900

    Move java test to unit test

--- wie_impl_java/tests/graphics.rs
+++
@@ -1,9 +0,0 @@
-use test_utils::test_jvm_core;
-
-#[futures_test::test]
-async fn test_graphics() -> anyhow::Result<()> {
-    let mut core = test_jvm_core();
-    let mut _jvm = core.jvm();
-
-    Ok(()) // TODO
-}
