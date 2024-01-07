f3c1c4e85e2f9a0fc4e697161e1aad9108d2b7d9
Inseok Lee <git@dlun.ch>
2024-01-07 15:07:36 +0900

    Resolve cyclic dependency on test util

--- test_utils/src/lib.rs
+++ test_utils/src/lib.rs
@@ -2,7 +2,7 @@
 extern crate alloc;
 
 mod arm_core;
-mod jvm_core;
+mod jvm;
 mod platform;
 
-pub use self::{arm_core::test_arm_core, jvm_core::test_jvm_core, platform::TestPlatform};
+pub use self::{arm_core::test_arm_core, jvm::test_jvm, platform::TestPlatform};
