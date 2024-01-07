be9fa5101588777b926eba99190ddcfcc3292024
Inseok Lee <git@dlun.ch>
2024-01-07 14:49:42 +0900

    Rename wipic module name

--- wie_impl_wipi_c/src/lib.rs
+++ wie_impl_wipi_c/src/lib.rs
@@ -1,8 +1,8 @@
 #![no_std]
 extern crate alloc;
 
+pub mod api;
 mod base;
-pub mod r#impl;
 mod method;
 
 pub use self::base::{WIPICContext, WIPICError, WIPICMemoryId, WIPICMethodBody, WIPICResult, WIPICWord};
