f7fa9e866b9c40bb3bfb654942d89c768c66b1e5
Inseok Lee <git@dlun.ch>
2024-01-07 15:13:42 +0900

    Move arm allocator to unit test

--- test_utils/src/arm_core.rs
+++
@@ -1,9 +0,0 @@
-use alloc::boxed::Box;
-
-use wie_core_arm::ArmCore;
-
-use crate::TestPlatform;
-
-pub fn test_arm_core() -> ArmCore {
-    ArmCore::new(wie_backend::System::new(Box::new(TestPlatform)).handle()).unwrap()
-}
