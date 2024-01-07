1c1dd022eb234bd930049c798e111c471062a9b0
Inseok Lee <git@dlun.ch>
2024-01-07 15:07:36 +0900

    Resolve cyclic dependency on test util

---
+++ test_utils/src/jvm.rs
@@ -0,0 +1,26 @@
+use core::ops::{Deref, DerefMut};
+
+use alloc::boxed::Box;
+
+use java_runtime_base::JavaClassProto;
+use jvm::{Class, Jvm, JvmResult};
+
+use jvm_impl::{ClassImpl, JvmDetailImpl};
+
+fn get_class_loader<T, C, Context>(get_class_proto: T, context: Context) -> impl Fn(&str) -> JvmResult<Option<Box<dyn Class>>>
+where
+    T: Fn(&str) -> Option<JavaClassProto<C>>,
+    C: ?Sized + 'static,
+    Context: DerefMut + Deref<Target = C> + Clone + 'static,
+{
+    move |class_name| Ok(get_class_proto(class_name).map(|x| Box::new(ClassImpl::from_class_proto(class_name, x, context.clone())) as _))
+}
+
+pub fn test_jvm<T, C, Context>(get_class_proto: T, context: Context) -> Jvm
+where
+    T: Fn(&str) -> Option<JavaClassProto<C>> + 'static,
+    C: ?Sized + 'static,
+    Context: DerefMut + Deref<Target = C> + Clone + 'static,
+{
+    Jvm::new(JvmDetailImpl::new(get_class_loader(get_class_proto, context)))
+}
