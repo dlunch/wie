d2a1b1fcaeac30e3c2cf61fd21bffd73e8000d9a
Inseok Lee <git@dlun.ch>
2024-01-07 13:45:15 +0900

    Move java test to unit test

--- wie_impl_java/src/classes/org/kwis/msp/lcdui/graphics.rs
+++ wie_impl_java/src/classes/org/kwis/msp/lcdui/graphics.rs
@@ -464,3 +464,16 @@ impl Graphics {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use test_utils::test_jvm_core;
+
+    #[futures_test::test]
+    async fn test_graphics() -> anyhow::Result<()> {
+        let mut core = test_jvm_core();
+        let mut _jvm = core.jvm();
+
+        Ok(()) // TODO
+    }
+}
