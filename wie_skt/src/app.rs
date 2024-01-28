dd7774640e3af96280eca8978be5b27ac5a0bc24
Inseok Lee <git@dlun.ch>
2024-01-28 19:18:11 +0900

    Properly load jar file

--- wie_skt/src/app.rs
+++ wie_skt/src/app.rs
@@ -1,4 +1,7 @@
-use alloc::string::{String, ToString};
+use alloc::{
+    string::{String, ToString},
+    vec::Vec,
+};
 
 use wie_backend::{App, System, SystemHandle};
 use wie_common::Event;
@@ -6,22 +9,25 @@ use wie_core_jvm::JvmCore;
 
 pub struct SktApp {
     system: System,
+    jar: Vec<u8>,
     main_class_name: String,
 }
 
 impl SktApp {
-    pub fn new(main_class_name: &str, system: System) -> anyhow::Result<Self> {
+    pub fn new(main_class_name: &str, jar: Vec<u8>, system: System) -> anyhow::Result<Self> {
         Ok(Self {
             system,
+            jar,
             main_class_name: main_class_name.to_string(),
         })
     }
 
     #[tracing::instrument(name = "start", skip_all)]
-    async fn do_start(system: &mut SystemHandle, main_class_name: String) -> anyhow::Result<()> {
+    async fn do_start(system: &mut SystemHandle, jar: Vec<u8>, main_class_name: String) -> anyhow::Result<()> {
         let core = JvmCore::new(system).await?;
-        let normalized_class_name = main_class_name.replace('.', "/");
+        core.add_jar(&jar).await?;
 
+        let normalized_class_name = main_class_name.replace('.', "/");
         let main_class = core.jvm().new_class(&normalized_class_name, "()V", []).await?;
 
         let result: anyhow::Result<()> = core
@@ -45,11 +51,12 @@ impl App for SktApp {
     fn start(&mut self) -> anyhow::Result<()> {
         let mut system_handle = self.system.handle();
 
+        let jar = self.jar.clone();
         let main_class_name = self.main_class_name.clone();
 
         self.system
             .handle()
-            .spawn(move || async move { Self::do_start(&mut system_handle, main_class_name).await });
+            .spawn(move || async move { Self::do_start(&mut system_handle, jar, main_class_name).await });
 
         Ok(())
     }
