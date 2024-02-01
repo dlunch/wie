8e487fc5508131b646990846e4d1bf428677f19e
Inseok Lee <git@dlun.ch>
2024-02-01 16:45:28 +0900

    Move canvas test to unit test

--- wie_backend/src/canvas.rs
+++ wie_backend/src/canvas.rs
@@ -336,3 +336,28 @@ pub fn decode_image(data: &[u8]) -> anyhow::Result<Box<dyn Image>> {
         pod_collect_to_vec(&data),
     )) as Box<_>)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{ArgbPixel, Canvas, Color, Image, ImageBuffer, VecImageBuffer};
+
+    #[test]
+    fn test_canvas() -> anyhow::Result<()> {
+        let mut image_buffer = VecImageBuffer::<ArgbPixel>::new(10, 10);
+        let mut canvas = Canvas::new(&mut image_buffer as &mut dyn ImageBuffer);
+
+        canvas.fill_rect(0, 0, 10, 10, Color { r: 0, g: 0, b: 0, a: 255 });
+
+        let raw = image_buffer.raw();
+
+        assert_eq!(raw.len(), 10 * 10 * 4);
+        for i in 0..10 * 10 {
+            assert_eq!(raw[i * 4], 0);
+            assert_eq!(raw[i * 4 + 1], 0);
+            assert_eq!(raw[i * 4 + 2], 0);
+            assert_eq!(raw[i * 4 + 3], 255);
+        }
+
+        Ok(())
+    }
+}
