fa1dc2c23dfb69ad4bd260c89b2a98caa16675c0
Inseok Lee <git@dlun.ch>
2024-03-17 16:50:35 +0900

    Start to add skvm classes

---
+++ wie_skvm/src/classes/com/skt/m/device.rs
@@ -0,0 +1,28 @@
+use alloc::vec;
+
+use java_class_proto::JavaMethodProto;
+use jvm::{ClassInstanceRef, Jvm, Result as JvmResult};
+
+use crate::context::{SKVMJavaClassProto, SKVMJavaContext};
+
+// class com.skt.m.Device
+pub struct Device {}
+
+impl Device {
+    pub fn as_proto() -> SKVMJavaClassProto {
+        SKVMJavaClassProto {
+            parent_class: Some("java/lang/Object"),
+            interfaces: vec![],
+            methods: vec![
+                JavaMethodProto::new("<init>", "()V", Self::init, Default::default()),
+            ],
+            fields: vec![],
+        }
+    }
+
+    async fn init(_jvm: &Jvm, _context: &mut SKVMJavaContext, this: ClassInstanceRef<Self>) -> JvmResult<()> {
+        tracing::debug!("com.skt.m.Device::<init>({:?})", &this);
+
+        Ok(())
+    }
+}
---
+++ wie_skvm/src/lib.rs
@@ -0,0 +1,32 @@
+#![no_std]
+extern crate alloc;
+
+pub mod classes;
+mod context;
+
+use core::future::Future;
+
+use context::SKVMJavaClassProto;
+pub use context::SKVMJavaContextBase;
+
+use alloc::boxed::Box;
+use jvm::{ClassDefinition, Jvm, Result as JvmResult};
+
+pub async fn register<T, F>(jvm: &Jvm, class_creator: T) -> JvmResult<()>
+where
+    T: Fn(&str, SKVMJavaClassProto) -> F,
+    F: Future<Output = Box<dyn ClassDefinition>>,
+{
+    // superclass should come before subclass
+    let classes = [(
+        "com/skt/m/Device", classes::com::skt::m::Device::as_proto(),
+    )];
+
+    for (name, proto) in classes {
+        let class = class_creator(name, proto).await;
+
+        jvm.register_class(class, None).await?;
+    }
+
+    Ok(())
+}
