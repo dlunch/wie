9ef4ea94b32c0161fd90083cf357cfa798dd7e8b
Inseok Lee <git@dlun.ch>
2024-03-30 14:25:28 +0900

    Lift some sync requirement

--- wie_backend/src/platform.rs
+++ wie_backend/src/platform.rs
@@ -1,6 +1,6 @@
 use crate::{audio_sink::AudioSink, database::DatabaseRepository, screen::Screen, time::Instant};
 
-pub trait Platform: Sync + Send {
+pub trait Platform: Send {
     fn screen(&mut self) -> &mut dyn Screen;
     fn now(&self) -> Instant;
     fn database_repository(&self) -> &dyn DatabaseRepository;
