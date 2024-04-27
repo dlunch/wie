2c9c5d69ab7c7dfb67cc29dbd65ab2124b9ba128
Inseok Lee <git@dlun.ch>
2024-04-27 09:49:17 +0900

    Add audio thread on cli

--- test_utils/src/platform.rs
+++ test_utils/src/platform.rs
@@ -28,4 +28,20 @@ impl AudioSink for TestAudioSink {
     fn play_wave(&self, _channel: u8, _sampling_rate: u32, _wave_data: &[i16]) {
         todo!()
     }
+
+    fn midi_note_on(&self, _channel_id: u8, _note: u8, _velocity: u8) {
+        todo!()
+    }
+
+    fn midi_note_off(&self, _channel_id: u8, _note: u8, _velocity: u8) {
+        todo!()
+    }
+
+    fn midi_program_change(&self, _channel_id: u8, _program: u8) {
+        todo!()
+    }
+
+    fn midi_control_change(&self, _channel_id: u8, _control: u8, _value: u8) {
+        todo!()
+    }
 }
--- wie_backend/src/audio_sink.rs
+++ wie_backend/src/audio_sink.rs
@@ -1,3 +1,7 @@
 pub trait AudioSink: Sync + Send {
     fn play_wave(&self, channel: u8, sampling_rate: u32, wave_data: &[i16]);
+    fn midi_note_on(&self, channel_id: u8, note: u8, velocity: u8);
+    fn midi_note_off(&self, channel_id: u8, note: u8, velocity: u8);
+    fn midi_program_change(&self, channel_id: u8, program: u8);
+    fn midi_control_change(&self, channel_id: u8, control: u8, value: u8);
 }
