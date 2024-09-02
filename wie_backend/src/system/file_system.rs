f8ddc7f194570de7eb534f3abdc92c33df108b0c
Inseok Lee <git@dlun.ch>
2024-09-02 21:12:20 +0900

    Remove mount_zip

--- wie_backend/src/system/file_system.rs
+++ wie_backend/src/system/file_system.rs
@@ -1,10 +1,5 @@
 use alloc::string::String;
-use std::{
-    collections::HashMap,
-    io::{Cursor, Read},
-};
-
-use zip::ZipArchive;
+use std::collections::HashMap;
 
 #[derive(Default)]
 pub struct Filesystem {
@@ -34,18 +29,5 @@ impl Filesystem {
 
     pub fn files(&self) -> impl Iterator<Item = (&str, &[u8])> {
         self.virtual_files.iter().map(|(k, v)| (k.as_str(), v.as_slice()))
-    }
-
-    pub fn mount_zip(&mut self, zip: &[u8]) {
-        let mut archive = ZipArchive::new(Cursor::new(zip)).unwrap();
-
-        for i in 0..archive.len() {
-            let mut file = archive.by_index(i).unwrap();
-
-            let mut data = Vec::new();
-            file.read_to_end(&mut data).unwrap();
-
-            self.add(file.name(), data);
-        }
     }
 }
