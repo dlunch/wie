03aa38cecb417ce991768e254d9c2c24d3cd7fb0
Inseok Lee <git@dlun.ch>
2024-09-16 18:54:55 +0900

    Reorganize test

--- wie_wipi_c/src/api/kernel.rs
+++ wie_wipi_c/src/api/kernel.rs
@@ -320,3 +320,37 @@ where
         gen_stub(64, "OEMC_knlGetToken"),
     ]
 }
+
+#[cfg(test)]
+mod test {
+    use alloc::boxed::Box;
+
+    use wie_util::{read_null_terminated_string, write_null_terminated_string, Result, WieError};
+
+    use crate::{context::test::TestContext, WIPICContext};
+
+    use super::get_kernel_method_table;
+
+    #[futures_test::test]
+    async fn test_sprintk() -> Result<()> {
+        let mut context = TestContext::new();
+
+        let kernel_methods = get_kernel_method_table(|_: &mut dyn WIPICContext| async { Ok::<_, WieError>(()) });
+
+        let format = context.alloc_raw(10).unwrap();
+        write_null_terminated_string(&mut context, format, "%d").unwrap();
+
+        let dest = context.alloc_raw(10).unwrap();
+
+        kernel_methods[1]
+            .call(&mut context, Box::new([dest, format, 1234, 0, 0, 0, 0]))
+            .await
+            .unwrap();
+
+        let result = read_null_terminated_string(&context, dest).unwrap();
+
+        assert_eq!(result, "1234");
+
+        Ok(())
+    }
+}
