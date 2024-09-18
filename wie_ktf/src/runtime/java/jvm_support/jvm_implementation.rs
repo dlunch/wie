fb4f064b9260c628d5c68524bdd8854f47bce9b2
Inseok Lee <git@dlun.ch>
2024-09-18 10:17:20 +0900

    Use generic to define rust class

--- wie_ktf/src/runtime/java/jvm_support/jvm_implementation.rs
+++ wie_ktf/src/runtime/java/jvm_support/jvm_implementation.rs
@@ -1,10 +1,11 @@
 use alloc::{boxed::Box, format};
+use core::ops::{Deref, DerefMut};
 
-use java_runtime::{Runtime, RuntimeClassProto};
+use java_class_proto::JavaClassProto;
 use jvm::{ClassDefinition, Jvm, Result as JvmResult};
 
 use wie_core_arm::ArmCore;
-use wie_jvm_support::{JvmImplementation, WieJavaClassProto, WieJvmContext};
+use wie_jvm_support::JvmImplementation;
 
 use super::{JavaArrayClassDefinition, JavaClassDefinition};
 
@@ -21,23 +22,21 @@ impl KtfJvmImplementation {
 
 #[async_trait::async_trait]
 impl JvmImplementation for KtfJvmImplementation {
-    async fn define_class_wie(&self, jvm: &Jvm, proto: WieJavaClassProto, context: Box<WieJvmContext>) -> JvmResult<Box<dyn ClassDefinition>> {
+    async fn define_class_rust<C, Context>(&self, jvm: &Jvm, proto: JavaClassProto<C>, context: Context) -> JvmResult<Box<dyn ClassDefinition>>
+    where
+        C: ?Sized + 'static + Send,
+        Context: Sync + Send + DerefMut + Deref<Target = C> + Clone + 'static,
+    {
         Ok(Box::new(
             JavaClassDefinition::new(&mut self.core.clone(), jvm, proto, context).await.unwrap(),
         ))
     }
 
-    async fn define_class_rust(&self, jvm: &Jvm, proto: RuntimeClassProto, runtime: Box<dyn Runtime>) -> jvm::Result<Box<dyn ClassDefinition>> {
-        Ok(Box::new(
-            JavaClassDefinition::new(&mut self.core.clone(), jvm, proto, runtime).await.unwrap(),
-        ))
-    }
-
-    async fn define_class_java(&self, _jvm: &Jvm, _data: &[u8]) -> jvm::Result<Box<dyn ClassDefinition>> {
+    async fn define_class_java(&self, _jvm: &Jvm, _data: &[u8]) -> JvmResult<Box<dyn ClassDefinition>> {
         unimplemented!()
     }
 
-    async fn define_array_class(&self, jvm: &Jvm, element_type_name: &str) -> jvm::Result<Box<dyn ClassDefinition>> {
+    async fn define_array_class(&self, jvm: &Jvm, element_type_name: &str) -> JvmResult<Box<dyn ClassDefinition>> {
         let class_name = format!("[{}", element_type_name);
         let class = JavaArrayClassDefinition::new(&mut self.core.clone(), jvm, &class_name).await.unwrap();
 
