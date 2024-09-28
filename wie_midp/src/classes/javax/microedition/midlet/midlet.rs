87806e3ffb2948ce4cb74b173e6f9a2977d3ea64
Inseok Lee <git@dlun.ch>
2024-09-28 16:25:57 +0900

    Add stub getAppProperty

--- wie_midp/src/classes/javax/microedition/midlet/midlet.rs
+++ wie_midp/src/classes/javax/microedition/midlet/midlet.rs
@@ -1,6 +1,7 @@
 use alloc::vec;
 
 use java_class_proto::JavaMethodProto;
+use java_runtime::classes::java::lang::String;
 use jvm::{ClassInstanceRef, Jvm, Result as JvmResult};
 
 use wie_jvm_support::{WieJavaClassProto, WieJvmContext};
@@ -16,6 +17,12 @@ impl MIDlet {
             interfaces: vec![],
             methods: vec![
                 JavaMethodProto::new("<init>", "()V", Self::init, Default::default()),
+                JavaMethodProto::new(
+                    "getAppProperty",
+                    "(Ljava/lang/String;)Ljava/lang/String;",
+                    Self::get_app_property,
+                    Default::default(),
+                ),
                 JavaMethodProto::new_abstract("startApp", "([Ljava/lang/String;)V", Default::default()),
             ],
             fields: vec![],
@@ -28,5 +35,16 @@ impl MIDlet {
         let _: () = jvm.invoke_special(&this, "java/lang/Object", "<init>", "()V", ()).await?;
 
         Ok(())
+    }
+
+    async fn get_app_property(
+        _jvm: &Jvm,
+        _context: &mut WieJvmContext,
+        this: ClassInstanceRef<Self>,
+        key: ClassInstanceRef<String>,
+    ) -> JvmResult<ClassInstanceRef<String>> {
+        tracing::warn!("stub javax.microedition.midlet.MIDlet::getAppProperty({:?}, {:?})", &this, key);
+
+        Ok(None.into())
     }
 }
