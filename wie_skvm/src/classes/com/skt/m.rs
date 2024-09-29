349b0ff7a8b9dbc7e64966c5b74694e11b627315
blluv <biluv@protonmail.com>
2024-09-30 00:41:38 +0900

    Add com.skt.m.ProgressBar stub

--- wie_skvm/src/classes/com/skt/m.rs
+++ wie_skvm/src/classes/com/skt/m.rs
@@ -4,5 +4,6 @@ mod back_light;
 mod device;
 mod graphics_2d;
 mod vibration;
+mod progress_bar;
 
-pub use {audio_clip::AudioClip, audio_system::AudioSystem, back_light::BackLight, device::Device, graphics_2d::Graphics2D, vibration::Vibration};
+pub use {audio_clip::AudioClip, audio_system::AudioSystem, back_light::BackLight, device::Device, graphics_2d::Graphics2D, vibration::Vibration, progress_bar::ProgressBar};
---
+++ wie_skvm/src/classes/com/skt/m/progress_bar.rs
@@ -0,0 +1,44 @@
+use alloc::vec;
+
+use java_class_proto::JavaMethodProto;
+use java_runtime::classes::java::lang::String;
+use jvm::{ClassInstanceRef, Jvm, Result as JvmResult};
+
+use wie_jvm_support::{WieJavaClassProto, WieJvmContext};
+
+// interface com.skt.m.ProgressBar
+pub struct ProgressBar;
+
+impl ProgressBar {
+    pub fn as_proto() -> WieJavaClassProto {
+        WieJavaClassProto {
+            name: "com/skt/m/ProgressBar",
+            parent_class: None,
+            interfaces: vec![],
+            methods: vec![
+                JavaMethodProto::new("<init>", "(Ljava/lang/String;)V", Self::init, Default::default()),
+                JavaMethodProto::new("setMaxValue", "(I)V",Self::set_max_value, Default::default()),
+                JavaMethodProto::new("setValue", "(I)V",Self::set_value, Default::default()),
+            ],
+            fields: vec![],
+        }
+    }
+
+    async fn init(_jvm: &Jvm, _context: &mut WieJvmContext, this: ClassInstanceRef<Self>, name: ClassInstanceRef<String>) -> JvmResult<()> {
+        tracing::debug!("com.skt.m.ProgressBar::<init>({:?}, {:?})", &this, name);
+
+        Ok(())
+    }
+
+    async fn set_max_value(_jvm: &Jvm, _context: &mut WieJvmContext, this: ClassInstanceRef<Self>, value: i32) -> JvmResult<()> {
+        tracing::debug!("com.skt.m.ProgressBar::setMaxValue({:?}, {:?})", &this, value);
+
+        Ok(())
+    }
+
+    async fn set_value(_jvm: &Jvm, _context: &mut WieJvmContext, this: ClassInstanceRef<Self>, value: i32) -> JvmResult<()> {
+        tracing::debug!("com.skt.m.ProgressBar::setValue({:?}, {:?})", &this, value);
+
+        Ok(())
+    }
+}
