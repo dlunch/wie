edc779c2aff4d141bae75b119e9b847bf9a60279
Inseok Lee <git@dlun.ch>
2024-10-03 14:53:37 +0900

    Implement mathfp

--- wie_skvm/src/classes/com/skt/m.rs
+++ wie_skvm/src/classes/com/skt/m.rs
@@ -3,10 +3,11 @@ mod audio_system;
 mod back_light;
 mod device;
 mod graphics_2d;
+mod math_fp;
 mod progress_bar;
 mod vibration;
 
 pub use {
-    audio_clip::AudioClip, audio_system::AudioSystem, back_light::BackLight, device::Device, graphics_2d::Graphics2D, progress_bar::ProgressBar,
-    vibration::Vibration,
+    audio_clip::AudioClip, audio_system::AudioSystem, back_light::BackLight, device::Device, graphics_2d::Graphics2D, math_fp::MathFP,
+    progress_bar::ProgressBar, vibration::Vibration,
 };
