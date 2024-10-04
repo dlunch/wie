883e83d0656208c84619ead3d675411583b732ff
Inseok Lee <git@dlun.ch>
2024-10-04 10:45:17 +0900

    Add TextBoxComponent

--- wie_wipi_java/src/classes/org/kwis/msp/lwc.rs
+++ wie_wipi_java/src/classes/org/kwis/msp/lwc.rs
@@ -2,10 +2,11 @@ mod annunciator_component;
 mod component;
 mod container_component;
 mod shell_component;
+mod text_box_component;
 mod text_component;
 mod text_field_component;
 
 pub use self::{
     annunciator_component::AnnunciatorComponent, component::Component, container_component::ContainerComponent, shell_component::ShellComponent,
-    text_component::TextComponent, text_field_component::TextFieldComponent,
+    text_box_component::TextBoxComponent, text_component::TextComponent, text_field_component::TextFieldComponent,
 };
