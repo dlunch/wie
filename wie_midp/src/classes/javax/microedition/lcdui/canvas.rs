f313b207e144f23f6e52526069733ce5609fc1e2
Inseok Lee <git@dlun.ch>
2024-10-05 13:56:13 +0900

    getWidth, getHeight is displayable's method

--- wie_midp/src/classes/javax/microedition/lcdui/canvas.rs
+++ wie_midp/src/classes/javax/microedition/lcdui/canvas.rs
@@ -16,8 +16,6 @@ impl Canvas {
             interfaces: vec![],
             methods: vec![
                 JavaMethodProto::new("<init>", "()V", Self::init, Default::default()),
-                JavaMethodProto::new("getWidth", "()I", Self::get_width, Default::default()),
-                JavaMethodProto::new("getHeight", "()I", Self::get_height, Default::default()),
                 JavaMethodProto::new("repaint", "()V", Self::repaint, Default::default()),
                 JavaMethodProto::new("repaint", "(IIII)V", Self::repaint_with_area, Default::default()),
                 JavaMethodProto::new("serviceRepaints", "()V", Self::service_repaints, Default::default()),
@@ -35,24 +33,6 @@ impl Canvas {
             .await?;
 
         Ok(())
-    }
-
-    async fn get_width(jvm: &Jvm, _context: &mut WieJvmContext, this: ClassInstanceRef<Self>) -> JvmResult<i32> {
-        tracing::debug!("javax.microedition.lcdui.Canvas::getWidth({:?})", &this);
-
-        let display = jvm.get_field(&this, "currentDisplay", "Ljavax/microedition/lcdui/Display;").await?;
-        let width = jvm.invoke_virtual(&display, "getWidth", "()I", ()).await?;
-
-        Ok(width)
-    }
-
-    async fn get_height(jvm: &Jvm, _context: &mut WieJvmContext, this: ClassInstanceRef<Self>) -> JvmResult<i32> {
-        tracing::debug!("javax.microedition.lcdui.Canvas::getHeight({:?})", &this);
-
-        let display = jvm.get_field(&this, "currentDisplay", "Ljavax/microedition/lcdui/Display;").await?;
-        let height = jvm.invoke_virtual(&display, "getHeight", "()I", ()).await?;
-
-        Ok(height)
     }
 
     async fn repaint(jvm: &Jvm, _: &mut WieJvmContext, this: ClassInstanceRef<Self>) -> JvmResult<()> {
--- wie_midp/src/classes/javax/microedition/lcdui/displayable.rs
+++ wie_midp/src/classes/javax/microedition/lcdui/displayable.rs
@@ -18,6 +18,8 @@ impl Displayable {
             interfaces: vec![],
             methods: vec![
                 JavaMethodProto::new("<init>", "()V", Self::init, Default::default()),
+                JavaMethodProto::new("getWidth", "()I", Self::get_width, Default::default()),
+                JavaMethodProto::new("getHeight", "()I", Self::get_height, Default::default()),
                 // wie private methods...
                 JavaMethodProto::new(
                     "setDisplay",
@@ -56,5 +58,23 @@ impl Displayable {
             .await?;
 
         Ok(())
+    }
+
+    async fn get_width(jvm: &Jvm, _context: &mut WieJvmContext, this: ClassInstanceRef<Self>) -> JvmResult<i32> {
+        tracing::debug!("javax.microedition.lcdui.Canvas::getWidth({:?})", &this);
+
+        let display = jvm.get_field(&this, "currentDisplay", "Ljavax/microedition/lcdui/Display;").await?;
+        let width = jvm.invoke_virtual(&display, "getWidth", "()I", ()).await?;
+
+        Ok(width)
+    }
+
+    async fn get_height(jvm: &Jvm, _context: &mut WieJvmContext, this: ClassInstanceRef<Self>) -> JvmResult<i32> {
+        tracing::debug!("javax.microedition.lcdui.Canvas::getHeight({:?})", &this);
+
+        let display = jvm.get_field(&this, "currentDisplay", "Ljavax/microedition/lcdui/Display;").await?;
+        let height = jvm.invoke_virtual(&display, "getHeight", "()I", ()).await?;
+
+        Ok(height)
     }
 }
