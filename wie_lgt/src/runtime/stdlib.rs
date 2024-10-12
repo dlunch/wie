374ba0639da90c229752892d284274eec822e0bf
Inseok Lee <git@dlun.ch>
2024-10-12 20:33:27 +0900

    strncpy

--- wie_lgt/src/runtime/stdlib.rs
+++ wie_lgt/src/runtime/stdlib.rs
@@ -1,4 +1,5 @@
 use alloc::{format, string::String, vec};
+use core::cmp::min;
 
 use wie_core_arm::ArmCore;
 use wie_util::{write_null_terminated_string, ByteRead, ByteWrite, Result, WieError};
@@ -8,6 +9,7 @@ pub fn get_stdlib_method(core: &mut ArmCore, function_index: u32) -> Result<u32>
         0x3f6 => core.register_function(unk2, &())?,
         0x3fb => core.register_function(atoi, &())?,
         0x405 => core.register_function(strcpy, &())?,
+        0x406 => core.register_function(strncpy, &())?,
         0x409 => core.register_function(strcmp, &())?,
         0x411 => core.register_function(strlen, &())?,
         0x414 => core.register_function(memcpy, &())?,
@@ -21,6 +23,17 @@ async fn strcpy(core: &mut ArmCore, _: &mut (), dst: u32, src: String) -> Result
     tracing::debug!("strcpy({:#x}, {})", dst, src);
 
     write_null_terminated_string(core, dst, &src)?;
+
+    Ok(())
+}
+
+async fn strncpy(core: &mut ArmCore, _: &mut (), dst: u32, src: String, size: u32) -> Result<()> {
+    tracing::debug!("strncpy({:#x}, {}, {:#x})", dst, src, size);
+
+    let size_to_copy = min(size, src.len() as u32);
+    let bytes = &src.as_bytes()[..size_to_copy as usize];
+
+    core.write_bytes(dst, bytes)?;
 
     Ok(())
 }
