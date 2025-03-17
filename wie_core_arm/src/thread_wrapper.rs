610233c2c9b3f812b4b93563f0cc818b7552ceb1
Inseok Lee <git@dlun.ch>
2025-03-17 09:36:26 +0900

    Save thread state in core

--- wie_core_arm/src/thread_wrapper.rs
+++ wie_core_arm/src/thread_wrapper.rs
@@ -5,56 +5,25 @@ use core::{
 };
 use wie_util::Result;
 
-use crate::{Allocator, ArmCore, context::ArmCoreContext};
-
-const STACK_SIZE: u32 = 0x1000;
+use crate::ArmCore;
 
 pub struct ArmCoreThreadWrapper {
     core: ArmCore,
-    context: ArmCoreContext,
-    stack_base: u32,
+    thread_id: u32,
     future: Pin<Box<dyn Future<Output = Result<()>> + Send>>,
 }
 
 impl ArmCoreThreadWrapper {
-    pub fn new<F, Fut>(mut core: ArmCore, entry: F) -> Result<Self>
+    pub fn new<F, Fut>(core: ArmCore, thread_id: u32, entry: F) -> Result<Self>
     where
         F: FnOnce() -> Fut,
         Fut: Future<Output = Result<()>> + Send + 'static,
     {
-        let stack_base = Allocator::alloc(&mut core, STACK_SIZE)?;
-        let context = ArmCoreContext {
-            r0: 0,
-            r1: 0,
-            r2: 0,
-            r3: 0,
-            r4: 0,
-            r5: 0,
-            r6: 0,
-            r7: 0,
-            r8: 0,
-            sb: 0,
-            sl: 0,
-            fp: 0,
-            ip: 0,
-            sp: stack_base + STACK_SIZE,
-            lr: 0,
-            pc: 0,
-            cpsr: 0x10, // USR32
-        };
-
         Ok(Self {
             core,
-            context,
-            stack_base,
+            thread_id,
             future: Box::pin(entry()),
         })
-    }
-}
-
-impl Drop for ArmCoreThreadWrapper {
-    fn drop(&mut self) {
-        Allocator::free(&mut self.core, self.stack_base, STACK_SIZE).unwrap()
     }
 }
 
@@ -62,12 +31,9 @@ impl Future for ArmCoreThreadWrapper {
     type Output = Result<()>;
 
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let context = self.context.clone(); // for borrow checker
-        self.core.restore_context(&context);
+        let _ = self.core.enter_thread_context(self.thread_id);
 
         let result = self.future.as_mut().poll(cx);
-
-        self.context = self.core.save_context();
 
         result
     }
