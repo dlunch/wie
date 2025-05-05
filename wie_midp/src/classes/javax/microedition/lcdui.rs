bc4971da7b43298fed27f9b6a2b1c618374d53f5
Inseok Lee <git@dlun.ch>
2025-05-05 16:50:22 +0900

    Add javax.microedition.lcdui.Command

--- wie_midp/src/classes/javax/microedition/lcdui.rs
+++ wie_midp/src/classes/javax/microedition/lcdui.rs
@@ -1,8 +1,9 @@
 mod canvas;
+mod command;
 mod display;
 mod displayable;
 mod font;
 mod graphics;
 mod image;
 
-pub use {canvas::Canvas, display::Display, displayable::Displayable, font::Font, graphics::Graphics, image::Image};
+pub use {canvas::Canvas, command::Command, display::Display, displayable::Displayable, font::Font, graphics::Graphics, image::Image};
