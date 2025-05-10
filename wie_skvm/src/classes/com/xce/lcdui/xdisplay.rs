af20408def155edc60b319b9031a4452c43bc984
Inseok Lee <git@dlun.ch>
2025-05-10 13:06:35 +0900

    Implement XDisplay::copyLCD

--- wie_skvm/src/classes/com/xce/lcdui/xdisplay.rs
+++ wie_skvm/src/classes/com/xce/lcdui/xdisplay.rs
@@ -2,9 +2,10 @@ use alloc::vec;
 
 use java_class_proto::{JavaFieldProto, JavaMethodProto};
 use java_constants::{FieldAccessFlags, MethodAccessFlags};
-use jvm::{Jvm, Result as JvmResult};
+use jvm::{ClassInstanceRef, Jvm, Result as JvmResult};
 
 use wie_jvm_support::{WieJavaClassProto, WieJvmContext};
+use wie_midp::classes::javax::microedition::lcdui::{Graphics, Image};
 
 // class com.xce.lcdui.XDisplay
 pub struct XDisplay;
@@ -18,6 +19,12 @@ impl XDisplay {
             methods: vec![
                 JavaMethodProto::new("<clinit>", "()V", Self::cl_init, MethodAccessFlags::STATIC),
                 JavaMethodProto::new("refresh", "(IIII)V", Self::refresh, MethodAccessFlags::STATIC),
+                JavaMethodProto::new(
+                    "copyLCD",
+                    "(Ljavax/microedition/lcdui/Graphics;Ljavax/microedition/lcdui/Image;IIII)V",
+                    Self::copy_lcd,
+                    MethodAccessFlags::STATIC,
+                ),
             ],
             fields: vec![
                 JavaFieldProto::new("width", "I", FieldAccessFlags::STATIC),
@@ -39,11 +46,27 @@ impl XDisplay {
     }
 
     async fn refresh(_jvm: &Jvm, context: &mut WieJvmContext, x: i32, y: i32, width: i32, height: i32) -> JvmResult<()> {
-        tracing::warn!("stub com.xce.lcdui.XDisplay::refresh({}, {}, {}, {})", x, y, width, height);
+        tracing::warn!("stub com.xce.lcdui.XDisplay::refresh({x}, {y}, {width}, {height})");
 
         let mut platform = context.system().platform();
         let screen = platform.screen();
         screen.request_redraw().unwrap();
+
+        Ok(())
+    }
+
+    #[allow(clippy::too_many_arguments)]
+    async fn copy_lcd(
+        _jvm: &Jvm,
+        _context: &mut WieJvmContext,
+        graphics: ClassInstanceRef<Graphics>,
+        image: ClassInstanceRef<Image>,
+        x: i32,
+        y: i32,
+        width: i32,
+        height: i32,
+    ) -> JvmResult<()> {
+        tracing::warn!("stub com.xce.lcdui.XDisplay::copyLCD({graphics:?}, {image:?}, {x}, {y}, {width}, {height})",);
 
         Ok(())
     }
