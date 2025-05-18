9db3cd7ffa88119633602adf72a76940b32f2f2d
Inseok Lee <git@dlun.ch>
2025-05-18 10:44:42 +0900

    Defer wipi initialization after classloader

--- wie_ktf/src/runtime/java/jvm_support.rs
+++ wie_ktf/src/runtime/java/jvm_support.rs
@@ -10,19 +10,24 @@ mod name;
 mod value;
 mod vtable_builder;
 
-use alloc::boxed::Box;
+use alloc::{boxed::Box, format};
 use core::mem::size_of;
 use jvm_implementation::KtfJvmImplementation;
 
-use bytemuck::{Pod, Zeroable};
+use bytemuck::{Pod, Zeroable, cast_slice};
 
 use java_runtime::classes::java::util::{Enumeration, jar::JarEntry};
-use jvm::{ClassDefinition, ClassInstance, ClassInstanceRef, Jvm, runtime::JavaLangString};
+use jvm::{
+    ClassDefinition, ClassInstance, ClassInstanceRef, Jvm,
+    runtime::{JavaIoInputStream, JavaLangString},
+};
 
 use wie_backend::System;
 use wie_core_arm::{Allocator, ArmCore};
 use wie_jvm_support::JvmSupport;
-use wie_util::{Result, read_generic, read_null_terminated_table, write_generic};
+use wie_util::{Result, WieError, read_generic, read_null_terminated_table, write_generic};
+
+use crate::runtime::init::load_native;
 
 use self::{
     array_class_instance::JavaArrayClassInstance,
@@ -94,56 +99,86 @@ impl KtfJvmSupport {
         let protos = [wie_wipi_java::get_protos().into(), wie_midp::get_protos().into()];
         let jvm = JvmSupport::new_jvm(system, jar_name, Box::new(protos), &[], KtfJvmImplementation::new(core.clone())).await?;
 
-        let client_bin = if let Some(x) = jar_name {
-            // find client.bin
-            let jar_name_java = JavaLangString::from_rust_string(&jvm, x).await.unwrap();
-            let jar_file = jvm
-                .new_class("java/util/jar/JarFile", "(Ljava/lang/String;)V", (jar_name_java,))
-                .await
-                .unwrap();
-            let entries: ClassInstanceRef<Enumeration> = jvm.invoke_virtual(&jar_file, "entries", "()Ljava/util/Enumeration;", []).await.unwrap();
-
-            loop {
-                let has_more_elements: bool = jvm.invoke_virtual(&entries, "hasMoreElements", "()Z", []).await.unwrap();
-                if !has_more_elements {
-                    break None;
-                }
-
-                let entry: ClassInstanceRef<JarEntry> = jvm.invoke_virtual(&entries, "nextElement", "()Ljava/lang/Object;", []).await.unwrap();
-                let name = jvm.invoke_virtual(&entry, "getName", "()Ljava/lang/String;", []).await.unwrap();
-                let name_rust = JavaLangString::to_rust_string(&jvm, &name).await.unwrap();
-
-                if name_rust.contains("client.bin") {
-                    break Some(name);
-                }
-            }
-        } else {
-            None
-        };
-
-        let class_loader_class = JavaClassDefinition::new(
-            core,
-            &jvm,
-            KtfClassLoader::as_proto(),
-            Box::new(ClassLoaderContext {
-                core: core.clone(),
-                system: system.clone(),
-            }) as Box<_>,
-        )
-        .await?;
-
-        jvm.register_class(Box::new(class_loader_class), None).await.unwrap();
-
         let system_class_loader: Box<dyn ClassInstance> = jvm
             .invoke_static("java/lang/ClassLoader", "getSystemClassLoader", "()Ljava/lang/ClassLoader;", [])
             .await
             .unwrap();
 
+        // used in tests
+        if jar_name.is_none() {
+            let context_data = KtfJvmSupportContext {
+                ptr_vtables_base: ptr_jvm_context + 12,
+                class_loader: KtfJvmSupport::class_instance_raw(&system_class_loader),
+                ptr_jvm_exception_context,
+            };
+            write_generic(core, SUPPORT_CONTEXT_BASE, context_data)?;
+            return Ok((jvm, system_class_loader));
+        }
+
+        // find client.bin
+        let jar_name_java = JavaLangString::from_rust_string(&jvm, jar_name.unwrap()).await.unwrap();
+        let jar_file = jvm
+            .new_class("java/util/jar/JarFile", "(Ljava/lang/String;)V", (jar_name_java,))
+            .await
+            .unwrap();
+        let entries: ClassInstanceRef<Enumeration> = jvm.invoke_virtual(&jar_file, "entries", "()Ljava/util/Enumeration;", []).await.unwrap();
+
+        let (name, name_rust) = loop {
+            let has_more_elements: bool = jvm.invoke_virtual(&entries, "hasMoreElements", "()Z", []).await.unwrap();
+            if !has_more_elements {
+                return Err(WieError::FatalError("client.bin not found".into()));
+            }
+
+            let entry: ClassInstanceRef<JarEntry> = jvm.invoke_virtual(&entries, "nextElement", "()Ljava/lang/Object;", []).await.unwrap();
+            let name = jvm.invoke_virtual(&entry, "getName", "()Ljava/lang/String;", []).await.unwrap();
+            let name_rust = JavaLangString::to_rust_string(&jvm, &name).await.unwrap();
+
+            if name_rust.contains("client.bin") {
+                break (name, name_rust);
+            }
+        };
+
+        // load client.bin
+        let data_stream = jvm
+            .invoke_virtual(
+                &system_class_loader,
+                "getResourceAsStream",
+                "(Ljava/lang/String;)Ljava/io/InputStream;",
+                (name,),
+            )
+            .await
+            .unwrap();
+
+        let data = JavaIoInputStream::read_until_end(&jvm, &data_stream).await.unwrap();
+
+        // call global initializer
+        let (native_functions, wipi_exe) = load_native(
+            core,
+            system,
+            &jvm,
+            &name_rust,
+            cast_slice(&data),
+            ptr_jvm_context as _,
+            ptr_jvm_exception_context as _,
+        )
+        .await
+        .unwrap();
+
+        let class_loader_class = JavaClassDefinition::new(
+            core,
+            &jvm,
+            KtfClassLoader::as_proto(),
+            Box::new(ClassLoaderContext { core: core.clone() }) as Box<_>,
+        )
+        .await?;
+
+        jvm.register_class(Box::new(class_loader_class), None).await.unwrap();
+
         let class_loader = jvm
             .new_class(
                 "net/wie/KtfClassLoader",
-                "(Ljava/lang/ClassLoader;Ljava/lang/String;II)V",
-                (system_class_loader, client_bin, ptr_jvm_context as i32, ptr_jvm_exception_context as i32),
+                "(Ljava/lang/ClassLoader;I)V",
+                (system_class_loader, native_functions.fn_get_class as i32),
             )
             .await
             .unwrap();
@@ -154,6 +189,12 @@ impl KtfJvmSupport {
             ptr_jvm_exception_context,
         };
         write_generic(core, SUPPORT_CONTEXT_BASE, context_data)?;
+
+        // call init with our class loader loaded
+        let result = core.run_function::<u32>(wipi_exe.fn_init, &[]).await?;
+        if result != 0 {
+            return Err(WieError::FatalError(format!("wipi init failed with code {result:#x}")));
+        }
 
         Ok((jvm, class_loader))
     }
