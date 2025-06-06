74a0ffb2d45842a5f08239c840447adc36071db6
Inseok Lee <git@dlun.ch>
2025-06-06 16:25:39 +0900

    Add lgt bpp getter

--- wie_lgt/src/runtime/wipi_c.rs
+++ wie_lgt/src/runtime/wipi_c.rs
@@ -26,6 +26,7 @@ pub fn get_wipi_c_method(core: &mut ArmCore, system: &mut System, jvm: &Jvm, fun
         0x32 => graphics::get_framebuffer_pointer.into_body(),
         0x33 => graphics::get_framebuffer_width.into_body(),
         0x34 => graphics::get_framebuffer_height.into_body(),
+        0x36 => graphics::get_framebuffer_bpp.into_body(),
         0x64 => kernel::printk.into_body(),
         0x65 => kernel::sprintk.into_body(),
         0x6a => unk1.into_body(),
