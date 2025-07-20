7c610c1da8e0b462e95643fcd3268c7456570c8c
Inseok Lee <git@dlun.ch>
2025-07-20 15:07:25 +0900

    fmt

--- test_utils/src/lib.rs
+++ test_utils/src/lib.rs
@@ -4,4 +4,7 @@ extern crate alloc;
 mod jvm;
 mod platform;
 
-pub use self::{jvm::run_jvm_test, platform::{TestPlatformEvent, TestPlatform}};
+pub use self::{
+    jvm::run_jvm_test,
+    platform::{TestPlatform, TestPlatformEvent},
+};
