0f2ac7e5df9d20886bfea57bfe62a23ff1b4dfb2
Inseok Lee <git@dlun.ch>
2025-08-17 22:55:58 +0900

    Call network callback with error

--- wie_wipi_c/src/api/net.rs
+++ wie_wipi_c/src/api/net.rs
@@ -1,9 +1,30 @@
-use wie_util::Result;
+use alloc::{boxed::Box, vec::Vec};
 
-use crate::{WIPICWord, context::WIPICContext};
+use wie_util::{Result, WieError};
 
-pub async fn connect(_context: &mut dyn WIPICContext, cb: WIPICWord, param: WIPICWord) -> Result<i32> {
+use crate::{WIPICResult, WIPICWord, context::WIPICContext, method::MethodBody};
+
+pub async fn connect(context: &mut dyn WIPICContext, cb: WIPICWord, param: WIPICWord) -> Result<i32> {
     tracing::warn!("stub MC_netConnect({:#x}, {:#x})", cb, param);
+
+    struct ConnectCallback {
+        cb: WIPICWord,
+        param: WIPICWord,
+    }
+
+    #[async_trait::async_trait]
+    impl MethodBody<WieError> for ConnectCallback {
+        #[tracing::instrument(name = "timer", skip_all)]
+        async fn call(&self, context: &mut dyn WIPICContext, _: Box<[WIPICWord]>) -> Result<WIPICResult> {
+            context.system().sleep(1).await; // simulate some delay
+
+            context.call_function(self.cb, &[u32::MAX, self.param]).await?; // callback with M_E_ERROR
+
+            Ok(WIPICResult { results: Vec::new() })
+        }
+    }
+
+    context.spawn(Box::new(ConnectCallback { cb, param }))?;
 
     Ok(-1) // M_E_ERROR
 }
