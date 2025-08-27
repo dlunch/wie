70da03d91f2f3d4021938eba240934f8bd7b55e0
Inseok Lee <git@dlun.ch>
2025-08-27 17:03:25 +0900

    Set j2me property

--- wie_j2me/src/emulator.rs
+++ wie_j2me/src/emulator.rs
@@ -2,13 +2,13 @@ use alloc::{
     borrow::ToOwned,
     boxed::Box,
     collections::btree_map::BTreeMap,
-    str,
+    format, str,
     string::{String, ToString},
     vec::Vec,
 };
 
 use jvm::{
-    Result as JvmResult,
+    ClassInstance, Result as JvmResult,
     runtime::{JavaIoInputStream, JavaLangString},
 };
 
@@ -25,13 +25,20 @@ impl J2MEEmulator {
         let descriptor = J2MEDescriptor::parse(&jad);
 
         let files = [(jar_filename.to_owned(), jar)].into_iter().collect();
-        Self::load(platform, &jar_filename, &descriptor.name, Some(descriptor.main_class_name), &files)
+        Self::load(
+            platform,
+            &jar_filename,
+            &descriptor.name,
+            Some(descriptor.main_class_name),
+            descriptor.properties,
+            &files,
+        )
     }
 
     pub fn from_jar(platform: Box<dyn Platform>, jar_filename: &str, jar: Vec<u8>) -> Result<Self> {
         let files = [(jar_filename.to_owned(), jar)].into_iter().collect();
 
-        Self::load(platform, jar_filename, jar_filename, None, &files)
+        Self::load(platform, jar_filename, jar_filename, None, BTreeMap::new(), &files)
     }
 
     fn load(
@@ -39,6 +46,7 @@ impl J2MEEmulator {
         jar_filename: &str,
         id: &str,
         main_class_name: Option<String>,
+        properties: BTreeMap<String, String>,
         files: &BTreeMap<String, Vec<u8>>,
     ) -> Result<Self> {
         let system = System::new(platform, id, id, DefaultTaskRunner);
@@ -50,15 +58,26 @@ impl J2MEEmulator {
         let mut system_clone = system.clone();
         let jar_filename = jar_filename.to_owned();
 
-        system.spawn(async move || Self::do_start(&mut system_clone, jar_filename, main_class_name).await);
+        system.spawn(async move || Self::do_start(&mut system_clone, jar_filename, properties, main_class_name).await);
 
         Ok(J2MEEmulator { system })
     }
 
     #[tracing::instrument(name = "start", skip_all)]
-    async fn do_start(system: &mut System, jar_filename: String, main_class_name: Option<String>) -> Result<()> {
+    async fn do_start(
+        system: &mut System,
+        jar_filename: String,
+        properties: BTreeMap<String, String>,
+        main_class_name: Option<String>,
+    ) -> Result<()> {
+        let properties = properties
+            .into_iter()
+            .map(|(k, v)| (format!("wie.appProperty.{k}"), v))
+            .collect::<Vec<_>>();
+        let properties = properties.iter().map(|(k, v)| (k.as_ref(), v.as_ref())).collect::<Vec<_>>();
+
         let protos = [wie_midp::get_protos().into()];
-        let jvm = JvmSupport::new_jvm(system, Some(&jar_filename), Box::new(protos), &[], RustJavaJvmImplementation).await?;
+        let jvm = JvmSupport::new_jvm(system, Some(&jar_filename), Box::new(protos), &properties, RustJavaJvmImplementation).await?;
 
         let main_class_name = if let Some(x) = main_class_name {
             x.replace('.', "/")
@@ -82,6 +101,23 @@ impl J2MEEmulator {
             let data = JavaIoInputStream::read_until_end(&jvm, &resource_stream).await.unwrap();
 
             let descriptor = J2MEDescriptor::parse(&data);
+
+            for (k, v) in descriptor.properties {
+                let property_key = format!("wie.appProperty.{k}");
+                let property_key = JavaLangString::from_rust_string(&jvm, &property_key).await.unwrap();
+                let property_value = JavaLangString::from_rust_string(&jvm, &v).await.unwrap();
+
+                let _: Option<Box<dyn ClassInstance>> = jvm
+                    .invoke_static(
+                        "java/lang/System",
+                        "setProperty",
+                        "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/Object;",
+                        (property_key, property_value),
+                    )
+                    .await
+                    .unwrap();
+            }
+
             if descriptor.main_class_name.is_empty() {
                 return Err(WieError::FatalError("Main class not found".into()));
             }
@@ -115,6 +151,7 @@ impl Emulator for J2MEEmulator {
 struct J2MEDescriptor {
     name: String,
     main_class_name: String,
+    properties: BTreeMap<String, String>,
 }
 
 impl J2MEDescriptor {
@@ -123,6 +160,7 @@ impl J2MEDescriptor {
 
         let mut name = String::new();
         let mut main_class_name = String::new();
+        let mut properties = BTreeMap::new();
 
         for line in lines {
             let line = str::from_utf8(line).unwrap().trim();
@@ -135,6 +173,7 @@ impl J2MEDescriptor {
 
             let key = parts.next().unwrap().trim();
             let value = parts.next().unwrap().trim();
+            properties.insert(key.trim().to_string(), value.trim().to_string());
 
             match key {
                 "MIDlet-Name" => name = value.to_string(),
@@ -143,6 +182,10 @@ impl J2MEDescriptor {
             }
         }
 
-        Self { name, main_class_name }
+        Self {
+            name,
+            main_class_name,
+            properties,
+        }
     }
 }
