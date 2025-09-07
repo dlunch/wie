ec4a5bfcf3605141423b4bb727baffa1408e79be
Inseok Lee <git@dlun.ch>
2025-09-07 09:19:47 +0900

    Add com/skt/m/Vibration::stop, isSupported

--- wie_skvm/src/classes/com/skt/m/vibration.rs
+++ wie_skvm/src/classes/com/skt/m/vibration.rs
@@ -17,6 +17,8 @@ impl Vibration {
             methods: vec![
                 JavaMethodProto::new("getLevelNum", "()I", Self::get_level_num, MethodAccessFlags::STATIC),
                 JavaMethodProto::new("start", "(II)V", Self::start, MethodAccessFlags::STATIC),
+                JavaMethodProto::new("stop", "()V", Self::stop, MethodAccessFlags::STATIC),
+                JavaMethodProto::new("isSupported", "()Z", Self::is_supported, MethodAccessFlags::STATIC),
             ],
             fields: vec![],
         }
@@ -32,5 +34,17 @@ impl Vibration {
         tracing::warn!("stub com.skt.m.Vibration::start({}, {})", level, timeout);
 
         Ok(())
+    }
+
+    async fn stop(_jvm: &Jvm, _context: &mut WieJvmContext) -> JvmResult<()> {
+        tracing::warn!("stub com.skt.m.Vibration::stop()");
+
+        Ok(())
+    }
+
+    async fn is_supported(_jvm: &Jvm, _context: &mut WieJvmContext) -> JvmResult<bool> {
+        tracing::warn!("stub com.skt.m.Vibration::isSupported()");
+
+        Ok(true)
     }
 }
