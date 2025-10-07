77c086c40c2ce636d288f8dbb24e1686ec577733
Inseok Lee <git@dlun.ch>
2025-10-07 17:27:55 +0900

    Add org.kwis.msf.io.SchemeNotFoundException

---
+++ wie_wipi_java/src/classes/org/kwis/msf/io/scheme_not_found_exception.rs
@@ -0,0 +1,48 @@
+use alloc::vec;
+
+use java_class_proto::JavaMethodProto;
+use java_runtime::classes::java::lang::String;
+use jvm::{ClassInstanceRef, Jvm, Result};
+
+use wie_jvm_support::{WieJavaClassProto, WieJvmContext};
+
+// class org.kwis.msf.io.SchemeNotFoundException
+pub struct SchemeNotFoundException;
+
+impl SchemeNotFoundException {
+    pub fn as_proto() -> WieJavaClassProto {
+        WieJavaClassProto {
+            name: "org/kwis/msf/io/SchemeNotFoundException",
+            parent_class: Some("java/io/IOException"),
+            interfaces: vec![],
+            methods: vec![
+                JavaMethodProto::new("<init>", "()V", Self::init, Default::default()),
+                JavaMethodProto::new("<init>", "(Ljava/lang/String;)V", Self::init_with_message, Default::default()),
+            ],
+            fields: vec![],
+        }
+    }
+
+    async fn init(jvm: &Jvm, _: &mut WieJvmContext, this: ClassInstanceRef<SchemeNotFoundException>) -> Result<()> {
+        tracing::debug!("org.kwis.msf.io.SchemeNotFoundException::<init>({this:?})");
+
+        let _: () = jvm.invoke_special(&this, "java/io/IOException", "<init>", "()V", ()).await?;
+
+        Ok(())
+    }
+
+    async fn init_with_message(
+        jvm: &Jvm,
+        _: &mut WieJvmContext,
+        this: ClassInstanceRef<SchemeNotFoundException>,
+        message: ClassInstanceRef<String>,
+    ) -> Result<()> {
+        tracing::debug!("org.kwis.msf.io.SchemeNotFoundException::<init>({this:?}, {message:?})");
+
+        let _: () = jvm
+            .invoke_special(&this, "java/io/IOException", "<init>", "(Ljava/lang/String;)V", (message,))
+            .await?;
+
+        Ok(())
+    }
+}
