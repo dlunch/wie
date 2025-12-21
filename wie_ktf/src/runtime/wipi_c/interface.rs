11eab26613c44fec1b907720c533d96f75f35ee9
Inseok Lee <git@dlun.ch>
2025-12-21 11:13:20 +0900

    Use graphcis interface on wipi_type

--- wie_ktf/src/runtime/wipi_c/interface.rs
+++ wie_ktf/src/runtime/wipi_c/interface.rs
@@ -1,37 +1,20 @@
 use alloc::vec::Vec;
 use core::mem::{size_of, size_of_val};
-use jvm::Jvm;
 
-use bytemuck::{Pod, Zeroable};
+use bytemuck::Pod;
+
+use jvm::Jvm;
+use wipi_types::ktf::wipic::WIPICInterface;
 
 use wie_backend::System;
 use wie_core_arm::{Allocator, ArmCore};
 use wie_util::{Result, write_generic};
 use wie_wipi_c::{WIPICContext, WIPICMethodBody};
 
-use crate::runtime::wipi_c::{context::KtfWIPICContext, method_table};
-
-#[repr(C)]
-#[derive(Clone, Copy, Pod, Zeroable)]
-struct WIPICInterface {
-    interface_0: u32,
-    interface_1: u32,
-    interface_2: u32,
-    interface_3: u32,
-    interface_4: u32,
-    interface_5: u32,
-    interface_6: u32,
-    interface_7: u32,
-    interface_8: u32,
-    interface_9: u32,
-    interface_10: u32,
-    interface_11: u32,
-    interface_12: u32,
-    interface_13: u32,
-    interface_14: u32,
-    interface_15: u32,
-    interface_16: u32,
-}
+use crate::runtime::wipi_c::{
+    context::KtfWIPICContext,
+    method_table::{self, get_graphics_interface},
+};
 
 fn write_methods(context: &mut dyn WIPICContext, methods: Vec<WIPICMethodBody>) -> Result<u32> {
     let address = context.alloc_raw((methods.len() * 4) as u32)?;
@@ -57,20 +40,29 @@ pub fn get_wipic_knl_interface(core: &mut ArmCore, system: &mut System, jvm: &Jv
     Ok(address)
 }
 
+fn write_interface<T: Pod>(context: &mut dyn WIPICContext, interface: T) -> Result<u32> {
+    let address = context.alloc_raw(size_of_val(&interface) as u32)?;
+    write_generic(context, address, interface)?;
+
+    Ok(address)
+}
+
 pub async fn get_wipic_interfaces(context: &mut dyn WIPICContext) -> Result<u32> {
     tracing::trace!("get_wipic_interfaces");
 
-    let interface_0 = write_methods(context, method_table::get_util_method_table())?;
-    let interface_1 = write_methods(context, method_table::get_misc_method_table())?;
-    let interface_2 = write_methods(context, method_table::get_graphics_method_table())?;
+    let graphics_interface = get_graphics_interface(context)?;
+
+    let util_interface = write_methods(context, method_table::get_util_method_table())?;
+    let misc_interface = write_methods(context, method_table::get_misc_method_table())?;
+    let graphics_interface = write_interface(context, graphics_interface)?;
     let interface_3 = write_methods(context, method_table::get_unk3_method_table())?;
     let interface_4 = write_methods(context, method_table::get_stub_method_table(4))?;
     let interface_5 = write_methods(context, method_table::get_stub_method_table(5))?;
-    let interface_6 = write_methods(context, method_table::get_database_method_table())?;
+    let database_interface = write_methods(context, method_table::get_database_method_table())?;
     let interface_7 = write_methods(context, method_table::get_stub_method_table(7))?;
-    let interface_8 = write_methods(context, method_table::get_uic_method_table())?;
-    let interface_9 = write_methods(context, method_table::get_media_method_table())?;
-    let interface_10 = write_methods(context, method_table::get_net_method_table())?;
+    let uic_interface = write_methods(context, method_table::get_uic_method_table())?;
+    let media_interface = write_methods(context, method_table::get_media_method_table())?;
+    let net_interface = write_methods(context, method_table::get_net_method_table())?;
     let interface_11 = write_methods(context, method_table::get_stub_method_table(11))?;
     let interface_12 = write_methods(context, method_table::get_unk12_method_table())?;
     let interface_13 = write_methods(context, method_table::get_stub_method_table(13))?;
@@ -79,17 +71,17 @@ pub async fn get_wipic_interfaces(context: &mut dyn WIPICContext) -> Result<u32>
     let interface_16 = write_methods(context, method_table::get_stub_method_table(16))?;
 
     let interface = WIPICInterface {
-        interface_0,
-        interface_1,
-        interface_2,
+        util_interface,
+        misc_interface,
+        graphics_interface,
         interface_3,
         interface_4,
         interface_5,
-        interface_6,
+        database_interface,
         interface_7,
-        interface_8,
-        interface_9,
-        interface_10,
+        uic_interface,
+        media_interface,
+        net_interface,
         interface_11,
         interface_12,
         interface_13,
