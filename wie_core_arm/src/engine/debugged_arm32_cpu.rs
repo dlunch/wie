4a8fcd69d72deb2c05805c2deb9b8610581eb2de
Inseok Lee <git@dlun.ch>
2026-03-24 19:45:28 +0900

    Improve GDB server control flow

--- wie_core_arm/src/engine/debugged_arm32_cpu.rs
+++ wie_core_arm/src/engine/debugged_arm32_cpu.rs
@@ -1,88 +1,314 @@
-extern crate std;
-
-use alloc::sync::Arc;
+use alloc::{collections::BTreeSet, sync::Arc, vec::Vec};
 use core::ops::Range;
-use wie_util::WieError;
 
 use crossbeam::channel;
-use gdbstub::{common::Signal, stub::MultiThreadStopReason};
+use gdbstub::{
+    common::{Signal, Tid},
+    stub::MultiThreadStopReason,
+};
+use spin::Mutex;
+use wie_util::WieError;
 
-use super::{Arm32CpuEngine, ArmEngine};
+use crate::{ThreadId, context::ArmCoreContext};
+
+use super::{Arm32CpuEngine, ArmEngine, ArmRegister, MemoryPermission};
+
+#[derive(Copy, Clone)]
+enum ResumeMode {
+    Continue,
+    Step,
+}
+
+pub(crate) struct DebugInner {
+    cpu: Mutex<Arm32CpuEngine>,
+    stop_event_tx: channel::Sender<MultiThreadStopReason<u32>>,
+    pub(crate) stop_event_rx: channel::Receiver<MultiThreadStopReason<u32>>,
+    resume_tx: channel::Sender<ResumeMode>,
+    resume_rx: channel::Receiver<ResumeMode>,
+    interrupt_pending: Mutex<bool>,
+    breakpoints: Mutex<BTreeSet<u32>>,
+    active_threads: Mutex<Vec<ThreadId>>,
+    thread_ready_tx: channel::Sender<()>,
+    pub(crate) thread_ready_rx: channel::Receiver<()>,
+}
+
+impl DebugInner {
+    fn new() -> Arc<Self> {
+        let (stop_event_tx, stop_event_rx) = channel::unbounded();
+        let (resume_tx, resume_rx) = channel::unbounded();
+        let (thread_ready_tx, thread_ready_rx) = channel::unbounded();
+
+        Arc::new(Self {
+            cpu: Mutex::new(Arm32CpuEngine::new()),
+            stop_event_tx,
+            stop_event_rx,
+            resume_tx,
+            resume_rx,
+            interrupt_pending: Mutex::new(false),
+            breakpoints: Mutex::new(BTreeSet::new()),
+            active_threads: Mutex::new(Vec::new()),
+            thread_ready_tx,
+            thread_ready_rx,
+        })
+    }
+
+    pub(crate) fn read_registers(&self) -> [u32; 17] {
+        let cpu = self.cpu.lock();
+
+        [
+            cpu.reg_read(ArmRegister::R0),
+            cpu.reg_read(ArmRegister::R1),
+            cpu.reg_read(ArmRegister::R2),
+            cpu.reg_read(ArmRegister::R3),
+            cpu.reg_read(ArmRegister::R4),
+            cpu.reg_read(ArmRegister::R5),
+            cpu.reg_read(ArmRegister::R6),
+            cpu.reg_read(ArmRegister::R7),
+            cpu.reg_read(ArmRegister::R8),
+            cpu.reg_read(ArmRegister::SB),
+            cpu.reg_read(ArmRegister::SL),
+            cpu.reg_read(ArmRegister::FP),
+            cpu.reg_read(ArmRegister::IP),
+            cpu.reg_read(ArmRegister::SP),
+            cpu.reg_read(ArmRegister::LR),
+            cpu.reg_read(ArmRegister::PC),
+            cpu.reg_read(ArmRegister::Cpsr),
+        ]
+    }
+
+    pub(crate) fn write_registers(&self, regs: [u32; 17]) {
+        let mut cpu = self.cpu.lock();
+
+        cpu.reg_write(ArmRegister::R0, regs[0]);
+        cpu.reg_write(ArmRegister::R1, regs[1]);
+        cpu.reg_write(ArmRegister::R2, regs[2]);
+        cpu.reg_write(ArmRegister::R3, regs[3]);
+        cpu.reg_write(ArmRegister::R4, regs[4]);
+        cpu.reg_write(ArmRegister::R5, regs[5]);
+        cpu.reg_write(ArmRegister::R6, regs[6]);
+        cpu.reg_write(ArmRegister::R7, regs[7]);
+        cpu.reg_write(ArmRegister::R8, regs[8]);
+        cpu.reg_write(ArmRegister::SB, regs[9]);
+        cpu.reg_write(ArmRegister::SL, regs[10]);
+        cpu.reg_write(ArmRegister::FP, regs[11]);
+        cpu.reg_write(ArmRegister::IP, regs[12]);
+        cpu.reg_write(ArmRegister::SP, regs[13]);
+        cpu.reg_write(ArmRegister::LR, regs[14]);
+        cpu.reg_write(ArmRegister::PC, regs[15]);
+        cpu.reg_write(ArmRegister::Cpsr, regs[16]);
+    }
+
+    pub(crate) fn read_memory(&self, start_addr: u32, data: &mut [u8]) -> wie_util::Result<usize> {
+        self.cpu.lock().mem_read(start_addr, data.len(), data)
+    }
+
+    pub(crate) fn write_memory(&self, start_addr: u32, data: &[u8]) -> wie_util::Result<()> {
+        self.cpu.lock().mem_write(start_addr, data)
+    }
+
+    pub(crate) fn resume_continue(&self) {
+        self.resume_tx.send(ResumeMode::Continue).unwrap();
+    }
+
+    pub(crate) fn resume_step(&self) {
+        self.resume_tx.send(ResumeMode::Step).unwrap();
+    }
+
+    pub(crate) fn interrupt(&self) {
+        *self.interrupt_pending.lock() = true;
+    }
+
+    pub(crate) fn add_breakpoint(&self, addr: u32) {
+        self.breakpoints.lock().insert(Self::normalize_addr(addr));
+    }
+
+    pub(crate) fn remove_breakpoint(&self, addr: u32) {
+        self.breakpoints.lock().remove(&Self::normalize_addr(addr));
+    }
+
+    pub(crate) fn active_threads(&self) -> Vec<ThreadId> {
+        self.active_threads.lock().clone()
+    }
+
+    pub(crate) fn on_thread_created(&self, thread_id: ThreadId, context: &ArmCoreContext) {
+        let mut active_threads = self.active_threads.lock();
+        if !active_threads.contains(&thread_id) {
+            active_threads.push(thread_id);
+        }
+        drop(active_threads);
+
+        self.write_registers([
+            context.r0,
+            context.r1,
+            context.r2,
+            context.r3,
+            context.r4,
+            context.r5,
+            context.r6,
+            context.r7,
+            context.r8,
+            context.sb,
+            context.sl,
+            context.fp,
+            context.ip,
+            context.sp,
+            context.lr,
+            context.pc,
+            context.cpsr,
+        ]);
+
+        let _ = self.thread_ready_tx.send(());
+    }
+
+    pub(crate) fn on_thread_deleted(&self, thread_id: ThreadId) {
+        self.active_threads.lock().retain(|&x| x != thread_id);
+    }
+
+    fn normalize_addr(addr: u32) -> u32 {
+        if addr & 1 == 1 { addr - 1 } else { addr }
+    }
+
+    fn take_interrupt(&self) -> bool {
+        let mut interrupt_pending = self.interrupt_pending.lock();
+        let result = *interrupt_pending;
+        *interrupt_pending = false;
+
+        result
+    }
+
+    fn wait_for_resume(&self) -> ResumeMode {
+        loop {
+            if let Ok(mode) = self.resume_rx.recv() {
+                return mode;
+            }
+        }
+    }
+
+    pub(crate) fn enqueue_stop_reason(&self, reason: MultiThreadStopReason<u32>) {
+        self.stop_event_tx.send(reason).unwrap();
+    }
+
+    fn map_stop_reason(error: WieError) -> MultiThreadStopReason<u32> {
+        match error {
+            WieError::AllocationFailure => MultiThreadStopReason::Signal(Signal::SIGKILL),
+            WieError::InvalidMemoryAccess(_) => MultiThreadStopReason::Signal(Signal::SIGSEGV),
+            WieError::Unimplemented(_) => MultiThreadStopReason::Signal(Signal::SIGSYS),
+            WieError::JavaException(_) => MultiThreadStopReason::Signal(Signal::SIGTRAP),
+            WieError::JavaExceptionUnwind { .. } => MultiThreadStopReason::Signal(Signal::SIGTRAP),
+            WieError::FatalError(_) => MultiThreadStopReason::Signal(Signal::SIGABRT),
+        }
+    }
+}
 
 pub struct DebuggedArm32CpuEngine {
-    arm32_cpu_engine: Arm32CpuEngine,
-    pub(crate) stop_event_tx: Arc<channel::Sender<MultiThreadStopReason<u32>>>,
-    pub(crate) stop_event_rx: Arc<channel::Receiver<MultiThreadStopReason<u32>>>,
-    pub(crate) resume_event_tx: Arc<channel::Sender<()>>,
-    pub(crate) resume_event_rx: Arc<channel::Receiver<()>>,
+    debug: Arc<DebugInner>,
+    current_resume_mode: Option<ResumeMode>,
+    stopped_on_breakpoint: Option<u32>,
 }
 
 impl DebuggedArm32CpuEngine {
     pub fn new() -> Self {
-        let (stop_event_tx, stop_event_rx) = channel::unbounded();
-        let (resume_event_tx, resume_event_rx) = channel::unbounded();
+        let debug = DebugInner::new();
 
         Self {
-            arm32_cpu_engine: Arm32CpuEngine::new(),
-            stop_event_tx: Arc::new(stop_event_tx),
-            stop_event_rx: Arc::new(stop_event_rx),
-            resume_event_tx: Arc::new(resume_event_tx),
-            resume_event_rx: Arc::new(resume_event_rx),
+            debug,
+            current_resume_mode: None,
+            stopped_on_breakpoint: None,
         }
+    }
+
+    pub(crate) fn debug_inner(&self) -> Arc<DebugInner> {
+        self.debug.clone()
     }
 }
 
 impl ArmEngine for DebuggedArm32CpuEngine {
     fn run(&mut self, end: u32, hook: &Range<u32>, count: u32) -> wie_util::Result<u32> {
         loop {
-            let result = self.arm32_cpu_engine.run(end, hook, count);
+            if self.debug.take_interrupt() {
+                self.debug.enqueue_stop_reason(MultiThreadStopReason::SwBreak(Tid::new(1).unwrap()));
+                self.current_resume_mode = None;
+                continue;
+            }
 
-            if let Err(x) = result {
-                let reason = match x {
-                    WieError::AllocationFailure => MultiThreadStopReason::Signal(Signal::SIGKILL),
-                    WieError::InvalidMemoryAccess(_) => MultiThreadStopReason::Signal(Signal::SIGSEGV),
-                    WieError::Unimplemented(_) => MultiThreadStopReason::Signal(Signal::SIGSYS),
-                    WieError::JavaException(_) => MultiThreadStopReason::Signal(Signal::SIGTRAP),
-                    WieError::JavaExceptionUnwind { .. } => MultiThreadStopReason::Signal(Signal::SIGTRAP),
-                    WieError::FatalError(_) => MultiThreadStopReason::Signal(Signal::SIGABRT),
-                };
-
-                self.stop_event_tx.send(reason).unwrap();
-
-                // wait until resume is called
-                // TODO this should be async wait
-                loop {
-                    if self.resume_event_rx.recv().is_ok() {
-                        break;
-                    }
+            let resume_mode = match self.current_resume_mode {
+                Some(mode) => mode,
+                None => {
+                    let mode = self.debug.wait_for_resume();
+                    self.current_resume_mode = Some(mode);
+                    mode
                 }
-            } else {
-                return result;
+            };
+
+            let current_pc = {
+                let cpu = self.debug.cpu.lock();
+                DebugInner::normalize_addr(cpu.reg_read(ArmRegister::PC))
+            };
+
+            if self.stopped_on_breakpoint == Some(current_pc) {
+                self.stopped_on_breakpoint = None;
+            } else if self.debug.breakpoints.lock().contains(&current_pc) {
+                self.stopped_on_breakpoint = Some(current_pc);
+                self.debug.enqueue_stop_reason(MultiThreadStopReason::Signal(Signal::SIGTRAP));
+                self.current_resume_mode = None;
+                continue;
+            }
+
+            let run_count = match resume_mode {
+                ResumeMode::Continue => count,
+                ResumeMode::Step => 1,
+            };
+
+            let result = {
+                let mut cpu = self.debug.cpu.lock();
+                cpu.run(end, hook, run_count)
+            };
+
+            match result {
+                Ok(pc) => match resume_mode {
+                    ResumeMode::Continue => return Ok(pc),
+                    ResumeMode::Step => {
+                        self.debug.enqueue_stop_reason(MultiThreadStopReason::Signal(Signal::SIGTRAP));
+                        self.current_resume_mode = None;
+                    }
+                },
+                Err(error) => {
+                    self.debug.enqueue_stop_reason(DebugInner::map_stop_reason(error));
+                    self.current_resume_mode = None;
+                }
             }
         }
     }
 
-    fn reg_write(&mut self, reg: super::ArmRegister, value: u32) {
-        self.arm32_cpu_engine.reg_write(reg, value)
+    fn reg_write(&mut self, reg: ArmRegister, value: u32) {
+        self.debug.cpu.lock().reg_write(reg, value)
     }
 
-    fn reg_read(&self, reg: super::ArmRegister) -> u32 {
-        self.arm32_cpu_engine.reg_read(reg)
+    fn reg_read(&self, reg: ArmRegister) -> u32 {
+        self.debug.cpu.lock().reg_read(reg)
     }
 
-    fn mem_map(&mut self, address: u32, size: usize, permission: super::MemoryPermission) {
-        self.arm32_cpu_engine.mem_map(address, size, permission)
+    fn mem_map(&mut self, address: u32, size: usize, permission: MemoryPermission) {
+        self.debug.cpu.lock().mem_map(address, size, permission)
     }
 
     fn mem_write(&mut self, address: u32, data: &[u8]) -> wie_util::Result<()> {
-        self.arm32_cpu_engine.mem_write(address, data)
+        self.debug.cpu.lock().mem_write(address, data)
     }
 
     fn mem_read(&mut self, address: u32, size: usize, result: &mut [u8]) -> wie_util::Result<usize> {
-        self.arm32_cpu_engine.mem_read(address, size, result)
+        self.debug.cpu.lock().mem_read(address, size, result)
     }
 
     fn is_mapped(&self, address: u32, size: usize) -> bool {
-        self.arm32_cpu_engine.is_mapped(address, size)
+        self.debug.cpu.lock().is_mapped(address, size)
+    }
+
+    fn on_thread_created(&mut self, thread_id: ThreadId, context: &ArmCoreContext) {
+        self.debug.on_thread_created(thread_id, context);
+    }
+
+    fn on_thread_deleted(&mut self, thread_id: ThreadId) {
+        self.debug.on_thread_deleted(thread_id);
     }
 }
--- wie_core_arm/src/gdb.rs
+++ wie_core_arm/src/gdb.rs
@@ -1,5 +1,6 @@
 extern crate std; // we need thread
 
+use alloc::{format, sync::Arc};
 use std::{
     io,
     net::{TcpListener, TcpStream},
@@ -9,48 +10,121 @@ use std::{
 
 use crossbeam::channel;
 use gdbstub::{
-    common::{Signal, Tid},
+    common::{Pid, Signal, Tid},
     conn::ConnectionExt,
     stub::{
-        GdbStub, MultiThreadStopReason,
+        DisconnectReason, GdbStub, MultiThreadStopReason,
         run_blocking::{BlockingEventLoop, Event, WaitForStopReasonError},
     },
     target::{
         Target, TargetResult,
         ext::base::{
             BaseOps,
-            multithread::{MultiThreadBase, MultiThreadResume},
+            multithread::{MultiThreadBase, MultiThreadResume, MultiThreadSingleStep, MultiThreadSingleStepOps},
         },
+        ext::breakpoints::{Breakpoints, BreakpointsOps, SwBreakpoint, SwBreakpointOps},
+        ext::extended_mode::{AttachKind, CurrentActivePid, CurrentActivePidOps, ExtendedMode, ExtendedModeOps, ShouldTerminate},
     },
 };
 use gdbstub_arch::arm::{Armv4t, reg::ArmCoreRegs};
 
-use wie_util::{ByteRead, ByteWrite};
-
-use crate::{ArmCore, context::ArmCoreContext, engine::DebuggedArm32CpuEngine};
+use crate::{ArmCore, engine::DebugInner};
 
 type GdbTargetError = &'static str;
 
 #[allow(dead_code)]
 pub struct GdbTarget {
     core: ArmCore,
+    debug: Arc<DebugInner>,
+    do_step: bool,
+    startup_tx: Option<channel::Sender<io::Result<()>>>,
 }
 
 impl GdbTarget {
-    pub fn start(core: ArmCore) {
-        let this = GdbTarget { core };
-        thread::spawn(|| this.run_gdb_server());
+    pub fn start(core: ArmCore) -> wie_util::Result<()> {
+        let debug = {
+            let inner = core.inner.lock();
+
+            inner
+                .engine
+                .as_any()
+                .downcast_ref::<crate::engine::DebuggedArm32CpuEngine>()
+                .unwrap()
+                .debug_inner()
+        };
+
+        let (startup_tx, startup_rx) = channel::bounded(1);
+        let this = GdbTarget {
+            core,
+            debug,
+            do_step: false,
+            startup_tx: Some(startup_tx),
+        };
+
+        thread::spawn(|| {
+            let _ = this.run_gdb_server();
+        });
+
+        match startup_rx.recv() {
+            Ok(Ok(())) => Ok(()),
+            Ok(Err(err)) => Err(wie_util::WieError::FatalError(format!("Failed to start GDB server: {err}"))),
+            Err(err) => Err(wie_util::WieError::FatalError(format!("Failed to wait for debugger run request: {err}"))),
+        }
     }
 
     fn run_gdb_server(mut self) -> io::Result<()> {
         let sock = TcpListener::bind("127.0.0.1:2159")?;
         println!("GDB server listening on {}", sock.local_addr()?);
 
-        let (stream, _addr) = sock.accept()?;
+        loop {
+            let (stream, addr) = match sock.accept() {
+                Ok(result) => result,
+                Err(err) => {
+                    if let Some(startup_tx) = self.startup_tx.take() {
+                        let _ = startup_tx.send(Err(io::Error::new(err.kind(), format!("{err}"))));
+                    }
+                    return Err(err);
+                }
+            };
 
-        let gdb = GdbStub::new(stream);
-        gdb.run_blocking::<GdbBlockingEventLoop>(&mut self).unwrap();
+            println!("GDB client attached from {addr}");
 
+            let gdb = GdbStub::new(stream);
+
+            match gdb.run_blocking::<GdbBlockingEventLoop>(&mut self) {
+                Ok(DisconnectReason::Disconnect) => {
+                    println!("GDB client requested detach");
+                    println!("GDB client detached");
+                }
+                Ok(DisconnectReason::TargetExited(code)) => {
+                    println!("GDB session ended: target exited with code {code}");
+                    return Ok(());
+                }
+                Ok(DisconnectReason::TargetTerminated(sig)) => {
+                    println!("GDB session ended: target terminated with signal {sig:?}");
+                    return Ok(());
+                }
+                Ok(DisconnectReason::Kill) => {
+                    println!("GDB session ended: kill requested");
+                    return Ok(());
+                }
+                Err(err) if err.is_connection_error() => {
+                    if let Some((conn_err, kind)) = err.into_connection_error() {
+                        println!("GDB client disconnected");
+                        println!("GDB connection closed ({kind:?}): {conn_err}");
+                    }
+                }
+                Err(err) if err.is_target_error() => {
+                    return Err(io::Error::other(format!("GDB target error: {}", err.into_target_error().unwrap())));
+                }
+                Err(err) => {
+                    return Err(io::Error::other(format!("GDB server error: {err}")));
+                }
+            }
+            println!("GDB server waiting for next client");
+        }
+
+        #[allow(unreachable_code)]
         Ok(())
     }
 }
@@ -69,76 +143,75 @@ impl Target for GdbTarget {
     fn guard_rail_implicit_sw_breakpoints(&self) -> bool {
         true
     }
+
+    #[inline(always)]
+    fn support_breakpoints(&mut self) -> Option<BreakpointsOps<'_, Self>> {
+        Some(self)
+    }
+
+    #[inline(always)]
+    fn support_extended_mode(&mut self) -> Option<ExtendedModeOps<'_, Self>> {
+        Some(self)
+    }
 }
 
 impl MultiThreadBase for GdbTarget {
-    #[inline(always)]
-    fn read_registers(&mut self, regs: &mut ArmCoreRegs, tid: Tid) -> TargetResult<(), Self> {
-        let thread_context = self.core.read_thread_context(tid.into()).unwrap();
+    fn support_resume(&mut self) -> Option<gdbstub::target::ext::base::multithread::MultiThreadResumeOps<'_, Self>> {
+        Some(self)
+    }
 
-        regs.r[0] = thread_context.r0;
-        regs.r[1] = thread_context.r1;
-        regs.r[2] = thread_context.r2;
-        regs.r[3] = thread_context.r3;
-        regs.r[4] = thread_context.r4;
-        regs.r[5] = thread_context.r5;
-        regs.r[6] = thread_context.r6;
-        regs.r[7] = thread_context.r7;
-        regs.r[8] = thread_context.r8;
-        regs.r[9] = thread_context.sb;
-        regs.r[10] = thread_context.sl;
-        regs.r[11] = thread_context.fp;
-        regs.r[12] = thread_context.ip;
-        regs.sp = thread_context.sp;
-        regs.lr = thread_context.lr;
-        regs.pc = thread_context.pc;
-        regs.cpsr = thread_context.cpsr;
+    #[inline(always)]
+    fn read_registers(&mut self, regs: &mut ArmCoreRegs, _tid: Tid) -> TargetResult<(), Self> {
+        let thread_context = self.debug.read_registers();
+
+        regs.r[0] = thread_context[0];
+        regs.r[1] = thread_context[1];
+        regs.r[2] = thread_context[2];
+        regs.r[3] = thread_context[3];
+        regs.r[4] = thread_context[4];
+        regs.r[5] = thread_context[5];
+        regs.r[6] = thread_context[6];
+        regs.r[7] = thread_context[7];
+        regs.r[8] = thread_context[8];
+        regs.r[9] = thread_context[9];
+        regs.r[10] = thread_context[10];
+        regs.r[11] = thread_context[11];
+        regs.r[12] = thread_context[12];
+        regs.sp = thread_context[13];
+        regs.lr = thread_context[14];
+        regs.pc = thread_context[15];
+        regs.cpsr = thread_context[16];
 
         Ok(())
     }
 
     #[inline(always)]
-    fn write_registers(&mut self, regs: &ArmCoreRegs, tid: Tid) -> TargetResult<(), Self> {
-        let context = ArmCoreContext {
-            r0: regs.r[0],
-            r1: regs.r[1],
-            r2: regs.r[2],
-            r3: regs.r[3],
-            r4: regs.r[4],
-            r5: regs.r[5],
-            r6: regs.r[6],
-            r7: regs.r[7],
-            r8: regs.r[8],
-            sb: regs.r[9],
-            sl: regs.r[10],
-            fp: regs.r[11],
-            ip: regs.r[12],
-            sp: regs.sp,
-            lr: regs.lr,
-            pc: regs.pc,
-            cpsr: regs.cpsr,
-        };
-
-        self.core.write_thread_context(tid.into(), &context);
+    fn write_registers(&mut self, regs: &ArmCoreRegs, _tid: Tid) -> TargetResult<(), Self> {
+        self.debug.write_registers([
+            regs.r[0], regs.r[1], regs.r[2], regs.r[3], regs.r[4], regs.r[5], regs.r[6], regs.r[7], regs.r[8], regs.r[9], regs.r[10], regs.r[11],
+            regs.r[12], regs.sp, regs.lr, regs.pc, regs.cpsr,
+        ]);
 
         Ok(())
     }
 
     #[inline(always)]
     fn read_addrs(&mut self, start_addr: u32, data: &mut [u8], _tid: Tid) -> TargetResult<usize, Self> {
-        Ok(self.core.read_bytes(start_addr, data).unwrap())
+        self.debug
+            .read_memory(start_addr, data)
+            .map_err(|_| gdbstub::target::TargetError::NonFatal)
     }
 
     #[inline(always)]
     fn write_addrs(&mut self, start_addr: u32, data: &[u8], _tid: Tid) -> TargetResult<(), Self> {
-        self.core.write_bytes(start_addr, data).unwrap();
-
-        Ok(())
+        self.debug
+            .write_memory(start_addr, data)
+            .map_err(|_| gdbstub::target::TargetError::NonFatal)
     }
 
     #[inline(always)]
     fn list_active_threads(&mut self, thread_is_active: &mut dyn FnMut(Tid)) -> Result<(), Self::Error> {
-        let thread_ids = self.core.get_thread_ids();
+        let thread_ids = self.debug.active_threads();
 
         for thread_id in thread_ids {
             thread_is_active(Tid::try_from(thread_id).unwrap());
@@ -150,19 +223,92 @@ impl MultiThreadBase for GdbTarget {
 
 impl MultiThreadResume for GdbTarget {
     fn resume(&mut self) -> Result<(), Self::Error> {
-        let engine = &self.core.inner.lock().engine;
-        let debugged_engine = engine.as_any().downcast_ref::<DebuggedArm32CpuEngine>().unwrap();
-        debugged_engine.resume_event_tx.send(()).unwrap();
+        if self.do_step {
+            self.debug.resume_step();
+        } else {
+            self.debug.resume_continue();
+        }
 
         Ok(())
     }
 
     fn clear_resume_actions(&mut self) -> Result<(), Self::Error> {
+        self.do_step = false;
         Ok(())
     }
 
     fn set_resume_action_continue(&mut self, _tid: Tid, _signal: Option<Signal>) -> Result<(), Self::Error> {
+        self.do_step = false;
         Ok(())
+    }
+
+    fn support_single_step(&mut self) -> Option<MultiThreadSingleStepOps<'_, Self>> {
+        Some(self)
+    }
+}
+
+impl MultiThreadSingleStep for GdbTarget {
+    fn set_resume_action_step(&mut self, _tid: Tid, _signal: Option<Signal>) -> Result<(), Self::Error> {
+        self.do_step = true;
+        Ok(())
+    }
+}
+
+impl Breakpoints for GdbTarget {
+    fn support_sw_breakpoint(&mut self) -> Option<SwBreakpointOps<'_, Self>> {
+        Some(self)
+    }
+}
+
+impl SwBreakpoint for GdbTarget {
+    fn add_sw_breakpoint(&mut self, addr: u32, _kind: <Self::Arch as gdbstub::arch::Arch>::BreakpointKind) -> TargetResult<bool, Self> {
+        self.debug.add_breakpoint(addr);
+
+        Ok(true)
+    }
+
+    fn remove_sw_breakpoint(&mut self, addr: u32, _kind: <Self::Arch as gdbstub::arch::Arch>::BreakpointKind) -> TargetResult<bool, Self> {
+        self.debug.remove_breakpoint(addr);
+
+        Ok(true)
+    }
+}
+
+impl ExtendedMode for GdbTarget {
+    fn run(&mut self, _filename: Option<&[u8]>, _args: gdbstub::target::ext::extended_mode::Args<'_, '_>) -> TargetResult<Pid, Self> {
+        if let Some(startup_tx) = self.startup_tx.take() {
+            let _ = startup_tx.send(Ok(()));
+        }
+
+        let _ = self.debug.thread_ready_rx.recv();
+
+        Ok(Pid::new(1).unwrap())
+    }
+
+    fn attach(&mut self, _pid: Pid) -> TargetResult<(), Self> {
+        Ok(())
+    }
+
+    fn query_if_attached(&mut self, _pid: Pid) -> TargetResult<AttachKind, Self> {
+        Ok(AttachKind::Attach)
+    }
+
+    fn kill(&mut self, _pid: Option<Pid>) -> TargetResult<ShouldTerminate, Self> {
+        Ok(ShouldTerminate::No)
+    }
+
+    fn restart(&mut self) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    fn support_current_active_pid(&mut self) -> Option<CurrentActivePidOps<'_, Self>> {
+        Some(self)
+    }
+}
+
+impl CurrentActivePid for GdbTarget {
+    fn current_active_pid(&mut self) -> Result<Pid, Self::Error> {
+        Ok(Pid::new(1).unwrap())
     }
 }
 
@@ -178,15 +324,14 @@ impl BlockingEventLoop for GdbBlockingEventLoop {
         target: &mut GdbTarget,
         conn: &mut Self::Connection,
     ) -> Result<Event<MultiThreadStopReason<u32>>, WaitForStopReasonError<GdbTargetError, io::Error>> {
-        let engine = &target.core.inner.lock().engine;
-        let debugged_engine = engine.as_any().downcast_ref::<DebuggedArm32CpuEngine>().unwrap();
-        let stop_event_rx = debugged_engine.stop_event_rx.clone();
+        let stop_event_rx = target.debug.stop_event_rx.clone();
 
         loop {
             match stop_event_rx.recv_timeout(Duration::from_millis(10)) {
                 Ok(x) => return Ok(Event::TargetStopped(x)),
                 Err(channel::RecvTimeoutError::Timeout) => {
-                    if let Some(x) = conn.peek().unwrap() {
+                    if conn.peek().unwrap().is_some() {
+                        let x = conn.read().unwrap();
                         return Ok(Event::IncomingData(x));
                     }
                 }
@@ -195,9 +340,8 @@ impl BlockingEventLoop for GdbBlockingEventLoop {
         }
     }
 
-    fn on_interrupt(_target: &mut GdbTarget) -> Result<Option<MultiThreadStopReason<u32>>, GdbTargetError> {
-        // TODO handle interrupt
-
-        Ok(Some(MultiThreadStopReason::Signal(Signal::SIGINT)))
+    fn on_interrupt(target: &mut GdbTarget) -> Result<Option<MultiThreadStopReason<u32>>, GdbTargetError> {
+        target.debug.interrupt();
+        Ok(None::<MultiThreadStopReason<u32>>)
     }
 }
