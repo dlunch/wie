3727695383c934d2f0dc346a79e14a5cbcf3b604
Inseok Lee <git@dlun.ch>
2026-07-16 19:16:11 +0900

    Fix ImageObserver interface hierarchy

--- wie_wipi_java/src/classes/org/kwis/msp/lcdui/image_observer.rs
+++ wie_wipi_java/src/classes/org/kwis/msp/lcdui/image_observer.rs
@@ -12,7 +12,7 @@ impl ImageObserver {
     pub fn as_proto() -> WieJavaClassProto {
         WieJavaClassProto {
             name: "org/kwis/msp/lcdui/ImageObserver",
-            parent_class: Some("java/lang/Object"),
+            parent_class: None,
             interfaces: vec![],
             methods: vec![JavaMethodProto::new_abstract(
                 "notify",
