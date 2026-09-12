a5486451fb06e0fb022d4b3053210848476c1d11
Inseok Lee <git@dlun.ch>
2026-09-12 16:52:22 +0900

    Restore function-based Java SVC handlers

--- wie-ktf/src/runtime/java.rs
+++ wie-ktf/src/runtime/java.rs
@@ -2,7 +2,7 @@ use alloc::{boxed::Box, collections::BTreeMap, sync::Arc};
 
 use spin::Mutex;
 
-use wie_core_arm::{ArmCore, EmulatedFunction, EmulatedFunctionParam, RegisteredFunction, SvcId};
+use wie_core_arm::{ArmCore, RegisteredFunction, SvcId};
 use wie_util::{Result, WieError};
 
 use crate::runtime::SVC_CATEGORY_JAVA;
@@ -12,31 +12,18 @@ pub mod jvm_support;
 
 pub type JavaSvcFunctions = Arc<Mutex<BTreeMap<u32, Arc<Box<dyn RegisteredFunction>>>>>;
 
-struct JavaSvcHandler(JavaSvcFunctions);
-
-#[async_trait::async_trait]
-impl EmulatedFunction<(), (), ()> for JavaSvcHandler {
-    async fn call(&self, core: &mut ArmCore, _: &mut ()) -> Result<()> {
-        let id = SvcId::get(core, 0);
-        let function = self
-            .0
-            .lock()
+async fn handle_java_svc(core: &mut ArmCore, svc_functions: &mut JavaSvcFunctions, id: SvcId) -> Result<()> {
+    let function = {
+        let svc_functions = svc_functions.lock();
+        svc_functions
             .get(&id.0)
             .cloned()
-            .ok_or_else(|| WieError::FatalError(alloc::format!("Unknown KTF Java SVC id {:#x}", id.0)))?;
+            .ok_or_else(|| WieError::FatalError(alloc::format!("Unknown KTF Java SVC id {:#x}", id.0)))?
+    };
 
-        function.call(core).await
-    }
-}
-
-impl Drop for JavaSvcHandler {
-    fn drop(&mut self) {
-        // Method proxies retain the JVM, which also owns this table.
-        let functions = core::mem::take(&mut *self.0.lock());
-        drop(functions);
-    }
+    function.call(core).await
 }
 
 pub fn register_java_svc_handler(core: &mut ArmCore, svc_functions: &JavaSvcFunctions) -> Result<()> {
-    core.register_svc_handler(SVC_CATEGORY_JAVA, JavaSvcHandler(svc_functions.clone()), &())
+    core.register_svc_handler(SVC_CATEGORY_JAVA, handle_java_svc, svc_functions)
 }
